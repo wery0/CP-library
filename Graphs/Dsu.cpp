5f2ede44c31fa2298cd389029211b7c6e926b300
wery0 <wery0123@yandex.ru>
2022-11-02 06:11:19 +0300

    Added comment & brace init in ctor

--- Graphs/Dsu.cpp
+++ Graphs/Dsu.cpp
@@ -1,3 +1,4 @@
+//Uncomment commented lines, if you want to maintain set of vertexes for each component.
 struct dsu {
     int a;
     vec<int> pr;
@@ -6,22 +7,21 @@ struct dsu {
 
     dsu() = default;
 
-    dsu(int x) {
-        a = x;
+    dsu(int n): a(n) {
         sz.resize(a, 1);
-        pr.resize(a);
+        pr.resize(a); iota(all(pr), 0);
         //vrt.resize(a); for (int q = 0; q < a; ++q) vrt[q] = {q};
-        iota(all(pr), 0);
     }
 
     void clear() {
         fill(all(sz), 1);
         iota(all(pr), 0);
+        //for (int q = 0; q < a; ++q;) vrt[q] = {q}, vrt[q].shrink_to_fit();
     }
 
     bool in_same_component(int x, int y) {return find(x) == find(y);}
 
-    int find(int x) { return x == pr[x] ? x : pr[x] = find(pr[x]); }
+    int find(int x) {return x == pr[x] ? x : pr[x] = find(pr[x]);}
 
     int unite(int x, int y) {
         int px = find(x);
@@ -30,7 +30,8 @@ struct dsu {
         if (sz[px] > sz[py]) swap(px, py);
         pr[px] = py;
         sz[py] += sz[px];
-        //for(int v : vrt[px]) vrt[py].pb(v); vrt[px].clear(); vrt[px].shrink_to_fit();
+        //for(int v : vrt[px]) vrt[py].pb(v);
+        //vrt[px].clear(); vrt[px].shrink_to_fit();
         return 1;
     }
 };
