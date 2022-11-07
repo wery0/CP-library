4a78edbc2031e92adc0008001b0b47ee52578045
wery0 <wery0123@yandex.ru>
2022-11-07 05:39:25 +0300

    Added description in comments

--- Math & number theory/Gen primes with SoE.cpp
+++ Math & number theory/Gen primes with SoE.cpp
@@ -1,7 +1,9 @@
-vec<int> gen_primes(int n) {
+//Fast Sieve of Eratosthenes with const optimizations.
+//Returns vector of all primes in range [1, n].
+vector<int> gen_primes(int n) {
     if (n <= 2) {if (n == 2) return {2}; return {};}
-    vec<int> o = {2, 3}; o.reserve(n / log(n));
-    vec<bool> is_prime(n + 1, true);
+    vector<int> o = {2, 3}; o.reserve(n / log(n));
+    vector<bool> is_prime(n + 1, true);
     is_prime[0] = is_prime[1] = 0;
     for (int w = 4; w <= n; w += 2) is_prime[w] = 0;
     for (int w = 3; w <= n; w += 6) is_prime[w] = 0;
@@ -19,7 +21,7 @@ vec<int> gen_primes(int n) {
     for (int w = 6; w - 1 <= n; w += 6) {
         for (int df : { -1, 1}) {
             const int q = w + df;
-            if (q <= n && is_prime[q]) o.pb(q);
+            if (q <= n && is_prime[q]) o.push_back(q);
         }
     }
     return o;
