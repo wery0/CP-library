1a157f4f649d231d9dd9c9a2bf54a6e5769d60bf
wery0 <wery0123@yandex.ru>
2022-11-08 05:59:03 +0300

    Removed isz dependence & improved description

--- Other/Meeting simulator.cpp
+++ Other/Meeting simulator.cpp
@@ -1,11 +1,11 @@
 //Simulates arrangement of meetings in rooms.
-//Each meeting is defined by [start_time, duration] (<=> [start_time, end_time))
+//Each meeting is defined by [start_time, duration]
 /*Each meeting wiil be arranged in the room with the smallest number among free rooms
-at the moment max(start_time, (when the first free room appears))*/
-//For every meeting calculates the room, where it will be.
+at the moment max(start_time, (when the first free room appears)) and will lasts duration time units*/
+//This function for every meeting calculates the room, where it will be.
 template<typename T>
 vec<int> meeting_simulator(T rooms, vec<pair<T, T>> meetings) {
-    //chmin(rooms, isz(meetings));           //<- uncomment if it may help
+    //chmin(rooms, meetings.size());           //<- uncomment if it may help
     sort(all(meetings), [&](const auto & l, const auto & r) {
         return l.F < r.F;
     });
@@ -13,12 +13,12 @@ vec<int> meeting_simulator(T rooms, vec<pair<T, T>> meetings) {
     priority_queue<int, vec<int>, greater<int>> free;
     for (int q = 0; q < rooms; ++q) free.push(q);
     ll cur_t = 0;
-    vec<int> where(isz(meetings));
-    for (int i = 0; i < isz(meetings); ++i) {
+    vec<int> where(meetings.size());
+    for (int i = 0; i < meetings.size(); ++i) {
         auto [start_time, duration] = meetings[i];
         chmax(cur_t, start_time);
         if (free.empty()) chmax(cur_t, pq.top().F);
-        for (; isz(pq) && pq.top().F <= cur_t;) {
+        for (; pq.size() && pq.top().F <= cur_t;) {
             free.push(pq.top().S);
             pq.pop();
         }
