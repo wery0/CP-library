a92bae734901ea0f62589a62435c4000790c1ebd
wery0 <wery0123@yandex.ru>
2022-11-10 16:06:34 +0300

    Removed isz dependence and restrictions on input arrays

--- Strings/All left shifts.cpp
+++ Strings/All left shifts.cpp
@@ -1,10 +1,10 @@
 template<typename I>
-vec<int> Z(I first, I last) {
+vector<int> Z(I first, I last, int ban) {
     int a = last - first;
-    vec<int> m(a);
+    vector<int> m(a);
     for (int q = 1, l = 0, r = 0; q < a; ++q) {
         m[q] = max(0, min(r - q, m[q - l]));
-        while (q + m[q] < a && *(first + m[q]) == *(first + m[q] + q)) {
+        while (q + m[q] < a && m[q] != ban && m[q] + q != ban && *(first + m[q]) == *(first + m[q] + q)) {
             ++m[q];
         }
         l = chmax(r, q + m[q]) ? q : l;
@@ -12,17 +12,16 @@ vec<int> Z(I first, I last) {
     return m;
 }
 
-//finds all left shifts of m, which equals to n.
+//Finds all left shifts of m, which equals to n.
 template<typename T_arr>
-vec<int> find_all_left_shifts(T_arr m, T_arr n) {
-    int a = isz(m); assert(isz(n) == a);
-    n.pb(-1);            //must not appear in m or n
-    n.insert(n.end(), all(m));
-    n.insert(n.end(), all(m));
-    vec<int> zf = Z(all(n));
-    vec<int> ans;
+vector<int> find_all_left_shifts(T_arr m, T_arr n) {
+    int a = m.size(); assert(n.size() == a);
+    n.push_back(0);
+    n.insert(n.end(), m.begin(), m.end());
+    n.insert(n.end(), m.begin(), m.end());
+    vector<int> zf = Z(n.begin(), n.end(), a), ans;
     for (int q = a + 1, i = 0; i < a; ++q, ++i) {
-        if (zf[q] == a) ans.pb(i);
+        if (zf[q] == a) ans.push_back(i);
     }
     return ans;
 }
