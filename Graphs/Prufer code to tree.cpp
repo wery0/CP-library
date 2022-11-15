931f2e6fe6cd42561c0376d768a03070286e0fa2
wery0 <wery0123@yandex.ru>
2022-11-16 02:06:06 +0300

    Fixed typo

--- Graphs/Prufer code to tree.cpp
+++ Graphs/Prufer code to tree.cpp
@@ -4,7 +4,7 @@ vector<pair<int, int>> prufer_code_to_tree(vector<int> pcode) {
     vector<int> cnt(n);
     for (int v : pcode) ++cnt[v];
     vector<pair<int, int>> ans(n - 1);
-    int leaf = leaf = find(all(cnt), 0) - cnt.begin();
+    int leaf = find(all(cnt), 0) - cnt.begin();
     for (int q = 0, ptr = leaf + 1; q < n - 2; q++) {
         int p = pcode[q];
         ans[q] = {p, leaf};
