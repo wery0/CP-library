2353bcf4e11e24709472b3e017ad657a9d61da2b
wery0 <wery0123@yandex.ru>
2022-11-19 17:49:51 +0300

    Functional wrapped into structure

--- Strings/EERTREE.cpp
+++ Strings/EERTREE.cpp
@@ -1,50 +1,44 @@
-struct Node {
-    int len = 0;
-    Node *sl = 0;
-    Node *m[26] = {0};
+template<const int ALPHABET = 26>
+struct EERTREE {
 
-    Node() {}
+    struct Node {
+        int len = 0;
+        Node *sl = 0;
+        Node *m[ALPHABET] = {0};
 
-    Node(int d) {
-        len = d;
+        Node() = default;
+        Node(int d): len(d) {}
+    };
+
+    str t;
+    Node *r0 = new Node(0);
+    Node *r1 = new Node(-1);
+    Node *lst = r0;
+
+    Node* go(Node *n) {
+        for (; n != r1; n = n->sl) {
+            int cur = t.size() - 1;
+            int pr = cur - 1 - n->len;
+            if (pr >= 0 && t[pr] == t[cur]) break;
+        }
+        return n;
+    }
+
+    EERTREE() {r0->sl = r1->sl = r1;}
+
+
+    int vertex_count = 0;
+
+    void add_char(char c) {
+        assert('a' <= c && c < 'a' + ALPHABET);
+        c -= 'a';
+        t += c;
+        lst = go(lst);
+        if (!lst->m[c]) {
+            vertex_count++;
+            lst->m[c] = new Node(lst->len + 2);
+            lst->m[c]->sl = lst == r1 ? r0 : go(lst->sl)->m[c];
+        }
+        lst = lst->m[c];
     }
 };
-
-str t;
-int cv = 0, cur = 0;
-Node *r0 = new Node(0);
-Node *r1 = new Node(-1);
-Node *lst = r0;
-
-Node* pizduy(Node *n) {
-    for (; n != r1; n = n->sl) {
-        int pr = cur - 1 - n->len;
-        if (pr >= 0 && t[pr] == t[cur]) break;
-    }
-    return n;
-}
-
-void add_char(char c) {
-    assert('a' <= c && c <= 'z');
-    c -= 'a';
-    lst = pizduy(lst);
-    if (!lst->m[c]) {
-        cv++;
-        lst->m[c] = new Node(lst->len + 2);
-        lst->m[c]->sl = lst == r1 ? r0 : pizduy(lst->sl)->m[c];
-    }
-    lst = lst->m[c];
-}
-
-int main() {
-    fast;
-    r0->sl = r1->sl = r1;
-    cin >> t;
-    vec<int> ans;
-    for (int q = 0; q < t.size(); q++) {
-        add_char(t[q]);
-        ans.pb(cv);
-        cur++;
-    }
-    for (int i : ans) cout << i << " ";
-}
