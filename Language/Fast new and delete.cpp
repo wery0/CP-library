f607ab89e92810a1cbdd324c112c4058dc5b1781
wery0 <wery0123@yandex.ru>
2022-11-19 17:04:52 +0300

    Minor syntax changes

--- Language/Fast new and delete.cpp
+++ Language/Fast new and delete.cpp
@@ -1,10 +1,10 @@
 const int MAX_MEM = 500 * 1024 * 1024;
 int mpos = 0;
 char mem[MAX_MEM];
-inline void * operator new (size_t n) {
+inline void* operator new (size_t n) {
     char *res = mem + mpos;
     mpos += n;
-    assert(mpos <= MAX_MEM);
+    //assert(mpos <= MAX_MEM);
     return (void*)res;
 }
 inline void operator delete (void*) {}
