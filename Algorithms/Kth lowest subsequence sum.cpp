71d36fe454fb8037da037101147640238ea1e286
wery0 <wery0123@yandex.ru>
2022-11-19 16:13:06 +0300

    Added structured binding

--- Algorithms/Kth lowest subsequence sum.cpp
+++ Algorithms/Kth lowest subsequence sum.cpp
@@ -1,5 +1,5 @@
 template<typename T>
-ll kth_lowest_subsequence_sum(vec<T> m, ll k) {
+ll kth_lowest_subsequence(vec<T> m, ll k) {
     ll o = 0;
     for (auto &i : m) if (i < 0) o += i, i *= -1;
     if (k-- == 0) return o;
@@ -7,11 +7,11 @@ ll kth_lowest_subsequence_sum(vec<T> m, ll k) {
     priority_queue<pair<ll, int>, vec<pair<ll, int>>, greater<pair<ll, int>>> pq;
     pq.push({m[0], 0});
     for (; pq.size();) {
-        auto p = pq.top(); pq.pop();
-        if (k-- == 0) return o + p.F;
-        if (p.S + 1 != m.size()) {
-            pq.push({p.F + m[p.S + 1] - m[p.S], p.S + 1});
-            pq.push({p.F + m[p.S + 1], p.S + 1});
+        auto [sm, i] = pq.top(); pq.pop();
+        if (k-- == 0) return o + sm;
+        if (i + 1 != m.size()) {
+            pq.push({sm + m[i + 1] - m[i], i + 1});
+            pq.push({sm + m[i + 1], i + 1});
         }
     }
     assert(0);
