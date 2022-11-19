5ee77ffd781a8c27d211ff09374f63a1e0878ed4
wery0 <wery0123@yandex.ru>
2022-11-19 16:16:57 +0300

    Added comment

--- Algorithms/Kth lowest subsequence sum.cpp
+++ Algorithms/Kth lowest subsequence sum.cpp
@@ -1,10 +1,11 @@
+//Sum of all elements must fit in T
 template<typename T>
-ll kth_lowest_subsequence(vec<T> m, ll k) {
-    ll o = 0;
-    for (auto &i : m) if (i < 0) o += i, i *= -1;
+T kth_lowest_subsequence(vector<T> m, ll k) {
+    T o = 0;
+    for (T &i : m) if (i < 0) o += i, i *= -1;
     if (k-- == 0) return o;
     sort(all(m));
-    priority_queue<pair<ll, int>, vec<pair<ll, int>>, greater<pair<ll, int>>> pq;
+    priority_queue<pair<T, int>, vector<pair<T, int>>, greater<pair<T, int>>> pq;
     pq.push({m[0], 0});
     for (; pq.size();) {
         auto [sm, i] = pq.top(); pq.pop();
