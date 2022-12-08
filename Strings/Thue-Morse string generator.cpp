8f323c056f7fcc0cf1c429e049493bdc5b09e76a
wery0 <wery0123@yandex.ru>
2022-12-09 00:07:05 +0300

    Removed dependency from template

--- Strings/Thue-Morse string generator.cpp
+++ Strings/Thue-Morse string generator.cpp
@@ -1,5 +1,5 @@
-str gen_thue_morse_string(int a) {
-    str s = string(a, 'a');
-    for (int i = 0; i < a; ++i) s[i] += __builtin_parity(i);
+string gen_thue_morse_string(int n) {
+    string s(n, 'a');
+    for (int i = 0; i < n; ++i) s[i] += __builtin_parity(i);
     return s;
 }
