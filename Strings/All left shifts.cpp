7ceeef264e1dfc1dcb91418dae7a066fbdebef67
wery0 <wery0123@yandex.ru>
2022-12-09 00:35:21 +0300

    Merged two functions into one, added example, improved variable naming

--- Strings/All left shifts.cpp
+++ Strings/All left shifts.cpp
@@ -1,27 +1,26 @@
-template<typename I>
-vector<int> Z(I first, I last, int ban) {
-    int a = last - first;
-    vector<int> m(a);
-    for (int q = 1, l = 0, r = 0; q < a; ++q) {
-        m[q] = max(0, min(r - q, m[q - l]));
-        while (q + m[q] < a && m[q] != ban && m[q] + q != ban && *(first + m[q]) == *(first + m[q] + q)) {
-            ++m[q];
-        }
-        l = chmax(r, q + m[q]) ? q : l;
-    }
-    return m;
-}
-
-//Finds all left shifts of m, which equals to n.
+//Finds all left shifts of arr1, which equals to arr2.
+/*
+  arr1 = {1, 2, 1, 2, 1, 2}
+  arr2 = {2, 1, 2, 1, 2, 1}
+  f(arr1, arr2) = {1, 3, 5}
+*/
+//O(n)
 template<typename T_arr>
-vector<int> find_all_left_shifts(T_arr m, T_arr n) {
-    int a = m.size(); assert(n.size() == a);
-    n.push_back(0);
-    n.insert(n.end(), m.begin(), m.end());
-    n.insert(n.end(), m.begin(), m.end());
-    vector<int> zf = Z(n.begin(), n.end(), a), ans;
-    for (int q = a + 1, i = 0; i < a; ++q, ++i) {
-        if (zf[q] == a) ans.push_back(i);
+vector<int> find_all_left_shifts(T_arr arr1, T_arr arr2) {
+    int n = arr1.size(); assert(arr2.size() == n);
+    arr2.push_back(0);
+    arr2.insert(arr2.end(), arr1.begin(), arr1.end());
+    arr2.insert(arr2.end(), arr1.begin(), arr1.end());
+    vector<int> zf(arr2.size()), ans;
+    for (int q = 1, l = 0, r = 0, len = arr2.size(); q < len; ++q) {
+        zf[q] = max(0, min(r - q, zf[q - l]));
+        while (q + zf[q] < len && zf[q] != n && zf[q] + q != n && arr2[zf[q]] == arr2[zf[q] + q]) {
+            ++zf[q];
+        }
+        l = chmax(r, q + zf[q]) ? q : l;
+    }
+    for (int i = 0; i < n; ++i) {
+        if (zf[n + 1 + i] == n) ans.push_back(i);
     }
     return ans;
 }
