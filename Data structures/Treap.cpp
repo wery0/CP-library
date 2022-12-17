ce5ae3462dd968e6fe5e6df95bb01fc89a6a4150
wery0 <wery0123@yandex.ru>
2022-12-17 23:04:26 +0300

    Improved namings, added new functions

--- Data structures/Treap.cpp
+++ Data structures/Treap.cpp
@@ -7,9 +7,9 @@ public:
         Node* l = 0;
         Node* r = 0;
         ll y;
-        ll sz = 0;
-        
-        K x;
+        ll sz;
+
+        K key;
         K mnk;
         K mxk;
         K smk;
@@ -19,14 +19,16 @@ public:
         V mxv;
         V smv;
 
-        Node(K x, V val = UNDEF): x(x), val(val), y(rnd()), sz(1) {
-            mnk = x;
-            mxk = x;
-            smk = x;
+        Node(K k, V v = UNDEF): y(rnd()), sz(1) {
+            key = k;
+            mnk = k;
+            mxk = k;
+            smk = k;
 
-            mnv = val;
-            mxv = val;
-            smv = val;
+            val = v;
+            mnv = v;
+            mxv = v;
+            smv = v;
         }
     };
     Node* root = 0;
@@ -54,9 +56,9 @@ private:
     }
 
     inline void upd(Node* n) {
-        n->mnk = min({gmnk(n->l), n->x, gmnk(n->r)});
-        n->mxk = max({gmxk(n->l), n->x, gmxk(n->r)});
-        n->smk = gsmk(n->l) + n->x + gsmk(n->r);
+        n->mnk = min({gmnk(n->l), n->key, gmnk(n->r)});
+        n->mxk = max({gmxk(n->l), n->key, gmxk(n->r)});
+        n->smk = gsmk(n->l) + n->key + gsmk(n->r);
 
         n->mnv = min({gmnv(n->l), n->val, gmnv(n->r)});
         n->mxv = max({gmxv(n->l), n->val, gmxv(n->r)});
@@ -77,31 +79,31 @@ private:
         return r;
     }
 
-    pair<Node*, Node*> split_size(Node* n, ll k) {
+    pair<Node*, Node*> split_size(Node* n, ll key) {
         if (!n) return {0, 0};
         push(n);
-        if (k <= gsz(n->l)) {
-            pair<Node*, Node*> p = split_size(n->l, k);
+        if (key <= gsz(n->l)) {
+            pair<Node*, Node*> p = split_size(n->l, key);
             n->l = p.S; upd(n);
             p.S = n;
             return p;
         }
-        pair<Node*, Node*> p = split_size(n->r, k - gsz(n->l) - 1);
+        pair<Node*, Node*> p = split_size(n->r, key - gsz(n->l) - 1);
         n->r = p.F; upd(n);
         p.F = n;
         return p;
     }
 
-    pair<Node*, Node*> split_key(Node* n, K k) {
+    pair<Node*, Node*> split_key(Node* n, K key) {
         if (!n) return {0, 0};
         push(n);
-        if (k < n->x) {
-            pair<Node*, Node*> p = split_key(n->l, k);
+        if (key < n->key) {
+            pair<Node*, Node*> p = split_key(n->l, key);
             n->l = p.S; upd(n);
             p.S = n;
             return p;
         }
-        pair<Node*, Node*> p = split_key(n->r, k);
+        pair<Node*, Node*> p = split_key(n->r, key);
         n->r = p.F; upd(n);
         p.F = n;
         return p;
@@ -132,7 +134,7 @@ private:
 
     void update_val_at_pos(Node* n, int pos, V new_val) {
         push(n);
-        if (pos == gsz(n->l)) {n->val = new_val; return;}
+        if (pos == gsz(n->l)) {n->val = new_val; upd(n); return;}
         if (pos < gsz(n->l)) update_val_at_pos(n->l, pos, new_val);
         else update_val_at_pos(n->r, pos - gsz(n->l) - 1, new_val);
         upd(n);
@@ -141,7 +143,7 @@ private:
     Node* erase_pos(Node* n, ll pos) {
         push(n);
         if (gsz(n->l) == pos) {
-            last_erased_key = n->x;
+            last_erased_key = n->key;
             last_erased_val = n->val;
             Node *l = n->l, *r = n->r;
             delete n;
@@ -156,28 +158,28 @@ private:
     Node* insert_node(Node* n, Node* nw) {
         push(n);
         if (!n || nw->y > n->y) {
-            pair<Node*, Node*> p = split_key(n, nw->x);
+            pair<Node*, Node*> p = split_key(n, nw->key);
             nw->l = p.F;
             nw->r = p.S;
             upd(nw);
             return nw;
         }
-        if (nw->x < n->x) n->l = insert_node(n->l, nw);
+        if (nw->key < n->key) n->l = insert_node(n->l, nw);
         else n->r = insert_node(n->r, nw);
         upd(n);
         return n;
     }
 
-    Node* erase_one_key(Node* n, K x) {
-        push(n);
+    Node* erase_one_key(Node* n, K key) {
         if (!n) return 0;
-        if (n->x == x) {
+        push(n);
+        if (n->key == key) {
             Node *l = n->l, *r = n->r;
             delete n;
             return merge(l, r);
         }
-        if (x < n->x) n->l = erase_one_key(n->l, x);
-        else n->r = erase_one_key(n->r, x);
+        if (key < n->key) n->l = erase_one_key(n->l, key);
+        else n->r = erase_one_key(n->r, key);
         upd(n);
         return n;
     }
@@ -185,8 +187,8 @@ private:
     void get_keys_on_subsegment(Node* n, int l, int& len, vector<K>& res) {
         if (!n || len <= 0) return;
         push(n);
-        if (l < gsz(n->l)) get_keys_on_subsegment(n -> l, l, len, res);
-        if (l <= gsz(n->l) && len) res.pb(n->x), --len;
+        if (l < gsz(n->l)) get_keys_on_subsegment(n->l, l, len, res);
+        if (l <= gsz(n->l) && len) res.push_back(n->key), --len;
         get_keys_on_subsegment(n->r, max(0LL, l - gsz(n->l) - 1), len, res);
     }
 
@@ -197,17 +199,14 @@ private:
         for (; n;) {
             push(n);
             if (gmnk(n->l) == mnk) n = n->l;
-            else if (n->x == mnk) return ans + gsz(n->l);
-            else {
-                ans += gsz(n->l) + 1;
-                n = n->r;
-            }
+            else if (n->key == mnk) return ans + gsz(n->l);
+            else ans += gsz(n->l) + 1, n = n->r;
         }
         assert(0);
         return ans;
     }
 
-    void print_keys(Node* n) {if (!n) return; push(n); print_keys(n->l); cout << n->x << ' '; print_keys(n->r);}
+    void print_keys(Node* n) {if (!n) return; push(n); print_keys(n->l); cout << n->key << ' '; print_keys(n->r);}
     void print_vals(Node* n) {if (!n) return; push(n); print_vals(n->l); cout << n->val << ' '; print_vals(n->r);}
 
     void delete_subtree(Node *n) {
@@ -224,34 +223,37 @@ public:
     template<typename I1, typename I2> treap(I1 f_key, I1 l_key, I2 f_val, I2 l_val) {assert(l_key - f_key == l_val - f_val); root = build(f_key, l_key, f_val, l_val);}
     ~treap() {delete_subtree(root);}
 
+    ll size() const {return gsz(root);}
+    bool empty() const {return root == 0;}
+
     template<typename I>
     void insert_array_at_pos(int pos, I first, I last) {pair<Node*, Node*> p = split_size(root, pos); root = merge(merge(p.F, build(first, last)), p.S);}
-    void insert_key_at_pos(int pos, K x, V val = UNDEF) {pair<Node*, Node*> p = split_size(root, pos); root = merge(merge(p.F, new Node(x, val)), p.S);}
-    void insert_key(K x, V val = UNDEF) {root = insert_node(root, new Node(x, val));}
+    void insert_key_at_pos(int pos, K key, V val = UNDEF) {pair<Node*, Node*> p = split_size(root, pos); root = merge(merge(p.F, new Node(key, val)), p.S);}
+    void insert_key(K key, V val = UNDEF) {root = insert_node(root, new Node(key, val));}
     void update_val_at_pos(int pos, V new_val) {update_val_at_pos(root, pos, new_val);}
 
     void erase_pos(ll pos) {root = erase_pos(root, pos);}
-    void erase_one_key(K x) {root = erase_one_key(root, x);}
+    void erase_one_key(K key) {root = erase_one_key(root, key);}
     void erase_seg(int l, int len) {pair<Node*, Node*> p1 = split_size(root, l); pair<Node*, Node*> p2 = split_size(p1.S, len); root = merge(p1.F, p2.S);}
 
     K extract_pos_get_key(ll pos) {erase_pos(pos); return last_erased_key;}
     V extract_pos_get_val(ll pos) {erase_pos(pos); return last_erased_val;}
     pair<K, V> extract_pos(ll pos) {erase_pos(pos); return {last_erased_key, last_erased_val};}
 
-    ll size() const {return gsz(root);}
-    bool empty() const {return root == 0;}
+    pair<K, V> operator[](ll pos) {Node *n = root; assert(0 <= pos && pos < gsz(n)); for (;;) {push(n); const int szl = gsz(n->l); if (pos == szl) return {n->key, n->val}; if (pos < szl) n = n->l; else {pos -= szl + 1; n = n->r;}} assert(0); return { -1, -1};}
+    V get_value_by_key(K key) {Node* n = root; while (n) {push(n); if (n->key == key) return n->val; if (key < n->key) n = n->l; else n = n->r;} assert(0); return 0;}
+    ll get_leftest_pos_of_key(K key) {Node* n = root; ll pos = 0, o = size; while (n) {push(n); if (key == n->x) o = min(o, pos + gsz(n->l)), n = n->l; else if (key < n->x) n = n->l; else pos += gsz(n->l) + 1, n = n->r;} assert(o < size()); return o;}
 
-    pair<K, V> operator[](ll pos) {Node *n = root; assert(0 <= pos && pos < gsz(n)); for (;;) {push(n); const int szl = gsz(n->l); if (pos == szl) return {n->x, n->val}; if (pos < szl) n = n->l; else {pos -= szl + 1; n = n->r;}} assert(0); return { -1, -1};}
-
-    ll count_keys_leq(K x) {Node* n = root; ll o = 0; while (n) {push(n); if (n->x <= x) o += gsz(n->l) + 1, n = n->r; else n = n->l;} return o;}
-    ll count_keys_less(K x) {Node* n = root; ll o = 0; while (n) {push(n); if (n->x < x) o += gsz(n->l) + 1, n = n->r; else n = n->l;} return o;}
+    ll count_keys_leq(K key) {Node* n = root; ll o = 0; while (n) {push(n); if (n->key <= key) o += gsz(n->l) + 1, n = n->r; else n = n->l;} return o;}
+    ll count_keys_less(K key) {Node* n = root; ll o = 0; while (n) {push(n); if (n->key < key) o += gsz(n->l) + 1, n = n->r; else n = n->l;} return o;}
     ll count_keys_in_seg(K l, K r) {return l > r ? 0 : count_keys_leq(r) - count_keys_less(l);}
-    ll count_keys_geq(K x) {return gsz(root) - count_keys_less(x);}
-    ll count_keys_greater(K x) {return gsz(root) - count_keys_leq(x);}
+    ll count_keys_geq(K key) {return gsz(root) - count_keys_less(key);}
+    ll count_keys_greater(K key) {return gsz(root) - count_keys_leq(key);}
+    ll count_keys_eq(K key) {return count_keys_leq(key) - count_keys_less(key);}
 
     int get_pos_of_leftest_min_key() {return pos_of_leftest_min_key(root);}
-    vector<K> get_keys_from_seg(int l, int len) {vector<K> res; get_keys_on_subsegment(root, l, len, res); get_keys_on_subsegment(l, len, res); return res;}
+    vector<K> get_keys_from_seg(int l, int len) {vector<K> res; get_keys_on_subsegment(root, l, len, res); return res;}
 
-    void print_keys(str end_string = "") {print_keys(root); cout << end_string;}
-    void print_vals(str end_string = "") {print_vals(root); cout << end_string;}
+    void print_keys(string end_string = "") {print_keys(root); cout << end_string;}
+    void print_vals(string end_string = "") {print_vals(root); cout << end_string;}
 };
