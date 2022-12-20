33611c5aeef6d27fd58072ab97c54c1931efe6cc
wery0 <wery0123@yandex.ru>
2022-12-20 16:37:08 +0300

    Added reserve

--- Math & number theory/Divisors, generating all possible quotients.cpp
+++ Math & number theory/Divisors, generating all possible quotients.cpp
@@ -2,14 +2,14 @@
 //divisors  = [1,  2,  3, 4, 5, 6, 7, 11, 21]
 //quotients = [20, 10, 6, 5, 4, 3, 2,  1, 0]
 template<typename T>
-vec<T> smallest_divisors_producing_all_possible_quotietns(T num) {
-    vec<T> divisors;
+vector<T> smallest_divisors_producing_all_possible_quotietns(T num) {
+    vector<T> divisors; divisors.reserve((int)(sqrt(num) * 2) + 1);
     T divisor = 1;
     while (divisor <= num) {
-        divisors.pb(divisor);
+        divisors.push_back(divisor);
         T quotient = num / divisor;
         divisor = num / quotient + 1;
     }
-    divisors.pb(num + 1);
+    divisors.push_back(num + 1);
     return divisors;
 }
