3be3ecd8b93e6a8d766d58be0df4f963d937473a
wery0 <wery0123@yandex.ru>
2022-12-26 20:10:48 +0300

    Added factorization through min_divisors array.

--- Math & number theory/Linear sieve.cpp
+++ Math & number theory/Linear sieve.cpp
@@ -1,16 +1,31 @@
-vec<int> min_divisors(int n) {
-    vec<int> mnd(n + 1);
-    vec<int> primes;
+//Returns vector of minimal divisors for numbers [0; n]
+//O(n)
+vector<int> min_divisors(int n) {
+    vector<int> min_div(n + 1), primes;
     for (int q = 2; q <= n; ++q) {
-        if (mnd[q] == 0) {
-            primes.pb(q);
-            mnd[q] = q;
+        if (min_div[q] == 0) {
+            primes.push_back(q);
+            min_div[q] = q;
         }
         for (int p : primes) {
             int c = p * q;
-            if (p > mnd[q] || c >= n) break;
-            mnd[c] = p;
+            if (p > min_div[q] || c >= n) break;
+            min_div[c] = p;
         }
     }
-    return mnd;
+    return min_div;
+}
+
+//O(# of multipliers in the factorization of n) <= O(log_2(n))
+vector<pair<int, int>> get_factorization(vector<int>& min_div, int n) {
+    assert(1 <= n && n < min_div.size());
+    vector<pair<int, int>> ans;
+    for (; n != 1;) {
+        int mnd = min_div[n], cnt = 0;
+        for (; min_div[n] == mnd; n /= mnd) {
+            ++cnt;
+        }
+        ans.emplace_back(mnd, cnt);
+    }
+    return ans;
 }
