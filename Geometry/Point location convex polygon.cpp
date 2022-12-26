508bb80f7386266e3559c612beaf01401644f951
wery0 <wery0123@yandex.ru>
2022-12-26 18:30:43 +0300

    Merged two functions into one, added comments

--- Geometry/Point location convex polygon.cpp
+++ Geometry/Point location convex polygon.cpp
@@ -1,19 +1,18 @@
-template<typename T>
-bool pis(pt<T> p1, pt<T> p2, pt<T> p) {
-    return dot(p2 - p1, p - p1) >= 0 && dot(p1 - p2, p - p2) >= 0;
-}
-
 //1 - inside, 0 - border, -1 - outside
+//O(log(n))
 template<typename T>
-int point_location_convex_polygon(vector<pt<T>> &ch, pt<T> &p) {
-    int a = ch.size();
-    if (a == 0) return -1;
-    if (a == 1) return -1;                   //could be 0 if (p == ch[0])?
-    if (a == 2) return cross(ch[0] - p, ch[1] - p) == 0 && pis(ch[0], ch[1], p) ? 0 : -1;
+int point_location_convex_polygon(vector<pt<T>>& ch, pt<T>& p) {    //ch - convex hull of polygon
+    auto pis = [&](pt<T> p1, pt<T> p2, pt<T> p) -> bool{
+        return dot(p2 - p1, p - p1) >= 0 && dot(p1 - p2, p - p2) >= 0;
+    };
+    int n = ch.size();
+    if (n == 0) return -1;
+    if (n == 1) return -1;                   //could be 0 if (p == ch[0])?
+    if (n == 2) return cross(ch[0] - p, ch[1] - p) == 0 && pis(ch[0], ch[1], p) ? 0 : -1;
     if (p.x < ch[0].x) return -1;
     if (cross(p - ch[0], ch[1] - ch[0]) > 0) return -1;
     if (cross(ch.back() - ch[0], p - ch[0]) > 0) return -1;
-    int l = 1, r = a;
+    int l = 1, r = n;
     while (l + 1 < r) {
         int md = (l + r) / 2;
         pt<T> v = ch[md] - ch[0];
@@ -22,7 +21,8 @@ int point_location_convex_polygon(vector<pt<T>> &ch, pt<T> &p) {
     }
     pt<T> v = ch[l] - ch[0];
     if (cross(v, p - ch[0]) == 0) {
-        return p - ch[0] == v ? 0 : pis(ch[0], ch[l], p) ? (l == 1 || l == a - 1 ? 0 : 1) : -1;
+        return p - ch[0] == v ? 0 :
+               pis(ch[0], ch[l], p) ? (l == 1 || l == n - 1 ? 0 : 1) : -1;
     }
     T val = cross(ch[l + 1] - p, p - ch[l]);
     return sign(val);
