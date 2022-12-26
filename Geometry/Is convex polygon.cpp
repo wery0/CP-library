583c5916d4b6aa9f9f2ec54f1538b2086e5cc618
wery0 <wery0123@yandex.ru>
2022-12-26 18:42:38 +0300

    Minor code improvements

--- Geometry/Is convex polygon.cpp
+++ Geometry/Is convex polygon.cpp
@@ -1,9 +1,9 @@
 template<typename T>
-bool is_convex_polygon(vec<pt<T>> m) {
-    int a = m.size(), sgn = 0;
-    for (int q = 0; q < a; ++q) {
-        pt<T> v1 = m[(q + 1) % a] - m[q];
-        pt<T> v2 = m[(q + 2) % a] - m[(q + 1) % a];
+bool is_convex_polygon(const vector<pt<T>>& m) {
+    int n = m.size(), sgn = 0;
+    for (int q = 0; q < n; ++q) {
+        pt<T> v1 = m[(q + 1) % n] - m[q];
+        pt<T> v2 = m[(q + 2) % n] - m[(q + 1) % n];
         T val = sign(cross(v1, v2));
         if (val == 0) continue;
         if (sgn == 0) sgn = val;
