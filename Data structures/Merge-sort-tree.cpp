8f8ba1d4a5069cd23f3b3605494695fcf23ff305
wery0 <wery0123@yandex.ru>
2022-12-26 20:19:15 +0300

    Struct -> class

--- Data structures/Merge-sort-tree.cpp
+++ Data structures/Merge-sort-tree.cpp
@@ -1,13 +1,17 @@
 template<typename T>
-struct merge_sort_tree {
+class merge_sort_tree {
     int G, U;
-    vec<vec<T>> store;
+    vector<vector<T>> store;
 
+    inline int work(vector<T>& arr, T val_l, T val_r) {
+        auto it_r = upper_bound(all(arr), val_r);
+        auto it_l = lower_bound(all(arr), val_l);
+        return it_r - it_l;
+    }
+
+public:
     template<typename Iterator>
-    merge_sort_tree(Iterator first, Iterator last) {
-        U = geq_pow2(last - first);
-        G = U * 2;
-        store = vec<vec<T>>(G);
+    merge_sort_tree(Iterator first, Iterator last): U(last - first), G(U * 2), store(G) {
         for (auto it = store.begin() + U; first != last; ++first, ++it) {
             *it = {*first};
         }
@@ -17,16 +21,12 @@ struct merge_sort_tree {
     }
 
     template<typename T_arr>
-    merge_sort_tree(T_arr &m) {
+    merge_sort_tree(T_arr& m) {
         (*this) = merge_sort_tree(all(m));
     }
 
-    inline int work(vec<T> &arr, T val_l, T val_r) {
-        auto it_r = upper_bound(all(arr), val_r);
-        auto it_l = lower_bound(all(arr), val_l);
-        return it_r - it_l;
-    }
-
+    //Counts # values in range [val_l, val_r] on segment [l, r]
+    //O(log(n)^2)
     int query(int l, int r, T val_l, T val_r) {
         l += U, r += U;
         int ans = 0;
@@ -39,4 +39,3 @@ struct merge_sort_tree {
         return ans;
     }
 };
-//Can count # values in range [val_l, val_r] on segment [l, r] in O(log(N)^2)
