fb4ae2aec73b376fd34c13bcb01a0bcd36c49ee6
wery0 <wery0123@yandex.ru>
2022-12-26 19:17:06 +0300

    Removed repetition of code, improved codestyle

--- Algorithms/Derangement solver.cpp
+++ Algorithms/Derangement solver.cpp
@@ -1,43 +1,20 @@
 template<typename T>
-vec<T> derangement_solver(vec<T> &m) {
-    int a = m.size(), diff = 0;
+vector<T> derangement_solver(vector<T>& m) {
+    int n = m.size(), diff = 0;
     umap<T, int> mp;
-    vec<int> cnt;
-    for (int q = 0; q < a; q++) {
-        if (!mp.count(m[q])) mp[m[q]] = diff++, cnt.pb(1);
+    vector<int> cnt;
+    for (int q = 0; q < n; q++) {
+        if (!mp.count(m[q])) mp[m[q]] = diff++, cnt.push_back(1);
         else ++cnt[mp[m[q]]];
     }
-    if (*max_element(all(cnt)) * 2 > a) return {};
-    for (int q = 1; q < diff; q++) cnt[q] += cnt[q - 1];
-    vec<int> num(a), gde(a);
-    vec<T> ans(a);
-    for (int q = 0; q < a; q++) {
+    if (*max_element(all(cnt)) * 2 > n) return {};
+    partial_sum(cnt.begin(), cnt.end(), cnt.begin());
+    vector<int> num(n), gde(n);
+    vector<T> ans(n);
+    for (int q = 0; q < n; q++) {
         num[q] = --cnt[mp[m[q]]];
         gde[num[q]] = q;
     }
-    for (int q = 0; q < a; q++) ans[q] = m[gde[(num[q] + a / 2) % a]];
-    return ans;
-}
-
-template<typename Iterator>
-auto derangement_solver(Iterator first, Iterator last) {
-    using T = typename iterator_traits<Iterator>::value_type;
-    auto m = vector(first, last);
-    int a = m.size(), diff = 0;
-    umap<T, int> mp;
-    vec<int> cnt;
-    for (int q = 0; q < a; q++) {
-        if (!mp.count(m[q])) mp[m[q]] = diff++, cnt.pb(1);
-        else ++cnt[mp[m[q]]];
-    }
-    if (*max_element(all(cnt)) * 2 > a) return vec<T>();
-    for (int q = 1; q < diff; q++) cnt[q] += cnt[q - 1];
-    vec<int> num(a), gde(a);
-    vec<T> ans(a);
-    for (int q = 0; q < a; q++) {
-        num[q] = --cnt[mp[m[q]]];
-        gde[num[q]] = q;
-    }
-    for (int q = 0; q < a; q++) ans[q] = m[gde[(num[q] + a / 2) % a]];
+    for (int q = 0; q < n; q++) ans[q] = m[gde[(num[q] + a / 2) % a]];
     return ans;
 }
