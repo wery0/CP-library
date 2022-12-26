4ae40cfc40fe92749dfc2a9d5d0e6436e9df932b
wery0 <wery0123@yandex.ru>
2022-12-26 20:23:23 +0300

    Algorith made more general

--- Math & number theory/Sum over subsets.cpp
+++ Math & number theory/Sum over subsets.cpp
@@ -1,12 +1,10 @@
 template<typename T>
-vec<T> sum_over_subsets(vec<T> m, const T mod) {
-    int a = m.size();
-    int lg = __lg(geq_pow2(a));
+vector<T> sum_over_subsets(vector<T> m) {
+    int n = m.size(), lg = __lg(geq_pow2(n));
     for (int q = 0; q < lg; q++) {
-        for (int w = 0; w < a; ++w) {
+        for (int w = 0; w < n; ++w) {
             if (w >> q & 1) m[w] += m[w ^ (1 << q)];
         }
-        for (int q = 0; q < a; ++q) m[q] -= m[q] < mod ? 0 : mod;
     }
     return m;
 }
