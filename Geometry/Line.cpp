e596dbe869cfbd7689730d9ed3f8bd9d31018383
wery0 <wery0123@yandex.ru>
2022-12-28 20:49:15 +0300

    Added support for integral types

--- Geometry/Line.cpp
+++ Geometry/Line.cpp
@@ -3,11 +3,7 @@ struct line {
     T A, B, C;
 
     line() = default;
-
-    line(T A, T B, T C): A(A), B(B), C(C) {
-        self_normalize();
-    }
-
+    line(T A, T B, T C): A(A), B(B), C(C) {self_normalize();}
     template<typename U>
     line(const pt<U>& a, const pt<U>& b) {
         A = a.y - b.y;
@@ -17,15 +13,26 @@ struct line {
     }
 
     template<typename U>
-    ld dst_to_pt(const pt<U>& p) const {
-        return abs(A * p.x + B * p.y + C);
+    ld get_dst_to_pt(const pt<U>& p) const {
+        if constexpr(is_integral_v<T>) {
+            return abs(A * p.x + B * p.y + C) / sqrtl(A * A + B * B);
+        } else {
+            return abs(A * p.x + B * p.y + C);
+        }
     }
 
     void self_normalize() {
-        assert(abs(A) + abs(B) > EPS);
-        ld u = sqrtl(A * A + B * B);
-        A /= u, B /= u, C /= u;
-        if (A < -EPS || (abs(A) < EPS && B < -EPS)) A *= -1, B *= -1, C *= -1;
+        if constexpr(is_integral_v<T>) {
+            assert(abs(A) + abs(B) > 0);
+            T gc = __gcd(__gcd(abs(A), abs(B)), abs(C));
+            A /= gc, B /= gc, C /= gc;
+            if (A < 0 || (A == 0 && B < 0)) A *= -1, B *= -1, C *= -1;
+        } else {
+            assert(abs(A) + abs(B) > EPS);
+            ld u = sqrtl(A * A + B * B);
+            A /= u, B /= u, C /= u;
+            if (A < -EPS || (abs(A) < EPS && B < -EPS)) A *= -1, B *= -1, C *= -1;
+        }
     }
 
     template<typename U>
@@ -41,40 +48,40 @@ struct line {
 
     template<typename U>
     pt<ld> get_projection_of_point(const pt<U>& p) const {
-        ld dst = dst_to_pt(p);
+        ld dst = get_dst_to_pt(p);
         pt<ld> norm{A, B};
         norm.self_normalize();
         norm *= dst;
         pt<ld> o{(ld)p.x, (ld)p.y}; o += norm;
-        if (dst_to_pt(o) < EPS) return o;
+        if (get_dst_to_pt(o) < EPS) return o;
         o -= norm * 2;
-        assert(dst_to_pt(o) < EPS);
+        assert(get_dst_to_pt(o) < EPS);
         return o;
     }
 
     template<typename U>
-    pt<ld> intersect(const line<U> &l) const {
+    pt<ld> intersect(const line<U>& l) const {
         assert(abs(A - l.A) + abs(B - l.B) > EPS);
-        ld x = 1.0 * (l.C * B - C * l.B) / (A * l.B - l.A * B);
-        ld y = 1.0 * (l.A * C - A * l.C) / (A * l.B - l.A * B);
+        ld x = (ld)1.0 * (l.C * B - C * l.B) / (A * l.B - l.A * B);
+        ld y = (ld)1.0 * (l.A * C - A * l.C) / (A * l.B - l.A * B);
         return {x, y};
     }
 
     template<typename U>
-    bool is_parallel_to(const line<U> &l) const {
+    bool is_parallel_to(const line<U>& l) const {
         return abs(l.A - A) + abs(l.B - B) < EPS;
     }
 
     template<typename U>
-    bool is_equal_to(const line<U> &l) const {
+    bool is_equal_to(const line<U>& l) const {
         return abs(l.A - A) + abs(l.B - B) + abs(l.C - C) < EPS;
     }
 
     void printkxb() const {
         if (abs(B) < EPS) {
-            cout << "X = " << -C / A << '\n';
+            cout << "X = " << -((ld)C / A) << '\n';
         } else {
-            cout << "Y = " << -A / B << "X + " << (-C / B) << '\n';
+            cout << "Y = " << -((ld)A / B) << "X + " << -((ld)C / B) << '\n';
         }
     }
 
