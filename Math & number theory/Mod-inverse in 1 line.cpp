57c6f6be803ba4561c21c97d28cc665a52a34bc3
wery0 <wery0123@yandex.ru>
2022-12-30 19:38:05 +0300

    Improved description

--- Math & number theory/Mod-inverse in 1 line.cpp
+++ Math & number theory/Mod-inverse in 1 line.cpp
@@ -1,4 +1,6 @@
-//to prevent overflow, x^2 and m^2 should fit in long long.
+//To prevent overflow, x^2 and m^2 should fit in long long.
+//If inverse value exists (<=> gcd(x, m) == 1), this function will always return it.
+//If doesn't - runtime error (division by zero)
 ll inv(ll x, ll m) {
     return x == 1 ? x : (1 - inv(m % x, x) * m) / x + m;
 }
