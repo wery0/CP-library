b77146bca47463e0761726e6fb9af3255940d87d
wery0 <wery0123@yandex.ru>
2022-12-30 04:14:54 +0300

    Struct -> class, vec -> vector

--- Data structures/Matrix.cpp
+++ Data structures/Matrix.cpp
@@ -1,23 +1,24 @@
 template<typename T>
-struct Matrix {
+class Matrix {
     int a = 0, b = 0;
-    vec<vec<T>> m;
+    vector<vector<T>> m;
 
+public:
     Matrix() = default;
-    Matrix(int a, int b): a(a), b(b) {m.resize(a, vec<T>(b));}
-    Matrix(const Matrix<T> &o) {a = o.a, b = o.b, m = o.m;}
+    Matrix(int a, int b): a(a), b(b) {m.resize(a, vector<T>(b));}
+    Matrix(const Matrix<T>& o) {a = o.a, b = o.b, m = o.m;}
     template<typename U>
-    Matrix(const vec<vec<U>> &o) {
+    Matrix(const vector<vector<U>>& o) {
         a = o.size(), b = a ? o[0].size() : 0;
-        m.resize(a, vec<T>(b));
+        m.resize(a, vector<T>(b));
         for (int q = 0; q < a; ++q) for (int w = 0; w < b; ++w) m[q][w] = o[q][w];
     }
     template<typename U>
-    Matrix(const initializer_list<initializer_list<U>> &l) {
+    Matrix(const initializer_list<initializer_list<U>>& l) {
         a = l.size(), b = a ? (*l.begin()).size() : 0;
-        m.resize(a, vec<T>(b));
+        m.resize(a, vector<T>(b));
         int q = 0;
-        for (const auto &i : l) {
+        for (const auto& i : l) {
             assert(b == i.size());
             int w = 0;
             for (const auto &j : i) m[q][w++] = j;
@@ -25,7 +26,7 @@ struct Matrix {
         }
     }
 
-    Matrix& operator=(const Matrix<T> &o) {a = o.a, b = o.b, m = o.m; return *this;}
+    Matrix& operator=(const Matrix<T>& o) {a = o.a, b = o.b, m = o.m; return *this;}
     Matrix& operator=(ll x) {for (int q = 0; q < min(a, b); ++q) m[q][q] = x; return *this;}
 
     size_t size() {return a;}
@@ -34,19 +35,19 @@ struct Matrix {
     inline bool is_sum_compatible(const Matrix& o) const {return a == o.a && b == o.b;}
     inline bool is_mul_compatible(const Matrix& o) const {return b == o.a;}
 
-    Matrix operator+(const Matrix<T> &o) const {
+    Matrix operator+(const Matrix<T>& o) const {
         assert(is_sum_compatible(o));
         Matrix<T> res = *this;
         for (int q = 0; q < a; ++q) for (int w = 0; w < b; ++w) res[q][w] += o[q][w];
         return res;
     }
-    Matrix operator-(const Matrix<T> &o) const {
+    Matrix operator-(const Matrix<T>& o) const {
         assert(is_sum_compatible(o));
         Matrix<T> res = *this;
         for (int q = 0; q < a; ++q) for (int w = 0; w < b; ++w) res[q][w] -= o[q][w];
         return res;
     }
-    Matrix operator*(const Matrix<T> &o) const {
+    Matrix operator*(const Matrix<T>& o) const {
         assert(is_mul_compatible(o));
         int c = o.b;
         Matrix<T> res(a, c);
@@ -138,15 +139,15 @@ struct Matrix {
         return n;
     }
 
-    void self_transpose() {vec<vec<T>> n(b, vec<T>(a)); for (int w = 0; w < b; ++w) for (int q = 0; q < a; ++q) n[w][q] = m[q][w]; m = n; swap(a, b);}
-    Matrix get_transpose() {vec<vec<T>> n(b, vec<T>(a)); for (int w = 0; w < b; ++w) for (int q = 0; q < a; ++q) n[w][q] = m[q][w]; return Matrix(n);}
+    void self_transpose() {vector<vector<T>> n(b, vector<T>(a)); for (int w = 0; w < b; ++w) for (int q = 0; q < a; ++q) n[w][q] = m[q][w]; m = n; swap(a, b);}
+    Matrix get_transpose() {vector<vector<T>> n(b, vector<T>(a)); for (int w = 0; w < b; ++w) for (int q = 0; q < a; ++q) n[w][q] = m[q][w]; return Matrix(n);}
 
-    void operator+=(const Matrix<T> &o) {assert(is_sum_compatible(o)); for (int q = 0; q < a; ++q) for (int w = 0; w < b; ++w) m[q][w] += o[q][w];}
-    void operator-=(const Matrix<T> &o) {assert(is_sum_compatible(o)); for (int q = 0; q < a; ++q) for (int w = 0; w < b; ++w) m[q][w] += o[q][w];}
-    void operator*=(const Matrix<T> &o) {(*this) = (*this) * o;}
+    void operator+=(const Matrix<T>& o) {assert(is_sum_compatible(o)); for (int q = 0; q < a; ++q) for (int w = 0; w < b; ++w) m[q][w] += o[q][w];}
+    void operator-=(const Matrix<T>& o) {assert(is_sum_compatible(o)); for (int q = 0; q < a; ++q) for (int w = 0; w < b; ++w) m[q][w] += o[q][w];}
+    void operator*=(const Matrix<T>& o) {(*this) = (*this) * o;}
 
-    const vec<T>& operator[](uint x) const {assert(0 <= x && x < a); return m[x];}
-    vec<T>& operator[](uint x) {assert(0 <= x && x < a); return m[x];}
+    const vector<T>& operator[](uint x) const {assert(0 <= x && x < a); return m[x];}
+    vector<T>& operator[](uint x) {assert(0 <= x && x < a); return m[x];}
 
     friend istream& operator>>(istream& in, Matrix& m) {for (int q = 0; q < m.a; ++q) for (int w = 0; w < m.b; ++w) in >> m[q][w]; return in;}
     friend ostream& operator<<(ostream& out, const Matrix& m) {for (int q = 0; q < m.a; ++q) {for (int w = 0; w < m.b; ++w) {out << m[q][w]; if (w + 1 != m.b) out << ' ';} out << '\n';} return out;}
