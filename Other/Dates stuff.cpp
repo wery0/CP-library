fcd18c9b8fd6c959d205bba9c4bfd335649ec1ab
wery0 <wery0123@yandex.ru>
2023-01-01 06:06:54 +0300

    Improved codestyle

--- Other/Dates stuff.cpp
+++ Other/Dates stuff.cpp
@@ -1,19 +1,20 @@
-vec<str> days_of_week_names = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-vec<str> months_names = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
-vec<int> days_in_months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+vector<string> days_of_week_names = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+vector<string> months_names = {"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+vector<int> days_in_months = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 bool is_leap_year(int y) {
     return ((y % 400) == 0) || ((y % 4) == 0 && (y % 100));
 }
 
 int date_to_int(int y, int m, int d) {
-    return 1461 * (y + 4800 + (m - 14) / 12) / 4 +
-           367 * (m - 2 - (m - 14) / 12 * 12) / 12 -
-           3 * ((y + 4900 + (m - 14) / 12) / 100) / 4 +
-           d - 32075;
+    int jd = 1461 * (y + 4800 + (m - 14) / 12) / 4 +
+             367 * (m - 2 - (m - 14) / 12 * 12) / 12 -
+             3 * ((y + 4900 + (m - 14) / 12) / 100) / 4 +
+             d - 32075;
+    return jd;
 }
 
-void int_to_date(int jd, int &y, int &m, int &d) {
+void int_to_date(int jd, int& y, int& m, int& d) {
     int x, n, i, j;
     x = jd + 68569;
     n = 4 * x / 146097;
