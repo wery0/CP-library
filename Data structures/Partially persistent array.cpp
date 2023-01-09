13b78e3fb2b4d1f89cf3e443b823703259d95298
wery0 <wery0123@yandex.ru>
2023-01-09 10:23:24 +0300

    Resolved ambiguity of (size_t, T) and (I, I) constructors

--- Data structures/Partially persistent array.cpp
+++ Data structures/Partially persistent array.cpp
@@ -2,45 +2,50 @@ template<typename T>
 class partially_persistent_array {
     vector<vector<pair<int, T>>> store;
     int cur_version = 0;
+    size_t n = 0;
 
 public:
-    partially_persistent_array(int n): store(n, {{0, T{}}}) {}
+    partially_persistent_array() = default;
+    partially_persistent_array(size_t n): n(n), store(n, {{0, T{}}}) {}
+    partially_persistent_array(size_t n, T val): n(n), store(n, {{0, val}}) {}
+    template<typename U>
+    partially_persistent_array(const initializer_list<U>& l) {
+        *this = partially_persistent_array(l.begin(), l.end());
+    }
     template<typename I>
-    partially_persistent_array(I first, I last) {
-        int n = last - first;
+    partially_persistent_array(I first, I last, typename enable_if<!is_integral_v<I>>::type* = 0) {
+        n = last - first;
         store.resize(n, {{0, T{}}});
         for (int i = 0; first != last; ++first, ++i) {
             store[i][0].second = *first;
         }
     }
-    template<typename U>
-    partially_persistent_array(const initializer_list<U>& l) {
-        *this = partially_persistent_array(all(l));
-    }
+
+    size_t size() const {return n;}
 
     int get_cur_version() const {return cur_version;}
 
     //<O(1), O(1)>
+    //Creates new version
     void set_value(int pos, T val) {
-        assert(0 <= pos && pos < store.size());
+        assert(0 <= pos && pos < n);
         store[pos].emplace_back(++cur_version, val);
     }
 
     //O(log(n))
-    T get_value(int version, int pos) {
-        assert(0 <= pos && pos < store.size());
+    T operator()(int version, int pos) const {
         assert(0 <= version && version <= cur_version);
+        assert(0 <= pos && pos < n);
         auto it = upper_bound(all(store[pos]), version, [&](int v, const pair<int, T>& r) {return v < r.first;});
-        assert(it != store[pos].begin());
         return (*prev(it)).second;
     }
 
     //O(nlog(n))
-    vector<T> get_version_as_vector(int version) {
+    vector<T> get_version_as_vector(int version) const {
         assert(0 <= version && version <= cur_version);
-        vector<T> ans(store.size());
-        for (int q = 0; q < store.size(); ++q) {
-            ans[q] = get_value(version, q);
+        vector<T> ans(n);
+        for (int q = 0; q < n; ++q) {
+            ans[q] = operator()(version, q);
         }
         return ans;
     }
