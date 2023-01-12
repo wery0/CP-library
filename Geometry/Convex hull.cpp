8f62b51adae090c2bab96f812ca7eecf6d435567
wery0 <wery0123@yandex.ru>
2023-01-12 09:02:07 +0300

    Minor code improvements

--- Geometry/Convex hull.cpp
+++ Geometry/Convex hull.cpp
@@ -3,15 +3,14 @@ template<typename T>
 vector<pt<T>> convex_hull(vector<pt<T>> m) {
     pt<T> mnp = *min_element(all(m));
     for (auto& p : m) p -= mnp;
-    pt<T> origin = {0, 0};
-    sort(all(m), [&](const auto& p1, const auto& p2) {
+    sort(all(m), [&](const auto & p1, const auto & p2) {
         T c = cross(p1, p2);
-        return c ? c > 0 : mhdst(origin, p1) < mhdst(origin, p2);
+        return c ? c > 0 : abs(p1.x) + abs(p1.y) < abs(p2.x) + abs(p2.y);
     });
     vector<pt<T>> ch;
     for (int q = 0; q < m.size(); ++q) {
         for (; ch.size() > 1;) {
-            if (cross(ch.back() - ch[ch.size() - 2], m[q] - ch.back()) > 0) break;
+            if (cross(ch.back() - end(ch)[-2], m[q] - ch.back()) > 0) break;
             ch.pop_back();
         }
         ch.push_back(m[q]);
