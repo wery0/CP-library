e8c8b49bb47f4effe4bd5ca60ee827b75c2eb19f
wery0 <wery0123@yandex.ru>
2023-01-15 18:54:15 +0300

    Improved namings & codestyle

--- Algorithms/Area of union of rectangles.cpp
+++ Algorithms/Area of union of rectangles.cpp
@@ -1,30 +1,29 @@
 template<typename T>
-struct area_of_rect_union {
+class area_of_rect_union {
 
     struct segtree_on_points {
 
         const T inf = numeric_limits<T>::max();
 
         uint a, U;
-        vec<pair<T, T>> seg_gr;
-        vec<T> cnt_mn;
-        vec<int> mn, ps_add;
+        vector<pair<T, T>> seg_gr;
+        vector<T> cnt_mn;
+        vector<int> mn, ps_add;
 
-        segtree_on_points(vec<T>& points) {
+        segtree_on_points(vector<T>& points) {
             if (points.empty()) return;
-            vec<pair<T, T>> gr; 
+            vector<pair<T, T>> gr;
             gr.reserve(points.size() * 2);
             for (int q = 0; q < points.size(); ++q) {
                 if (q && points[q - 1] + 1 < points[q]) {
-                    gr.pb({points[q - 1] + 1, points[q] - 1});
+                    gr.emplace_back(points[q - 1] + 1, points[q] - 1);
                 }
-                gr.pb({points[q], points[q]});
+                gr.emplace_back(points[q], points[q]);
             }
             a = gr.size();
             U = geq_pow2(a);
-            seg_gr.resize(U * 2);
+            seg_gr.resize(U * 2, {gr[a - 1].second + 1, gr[a - 1].second + 1});
             for (int q = 0; q < a; ++q) seg_gr[U + q] = gr[q];
-            for (int q = a; q < U; ++q) seg_gr[U + q].F = seg_gr[U + q].S = gr[a - 1].S + 1;
             mn.resize(U * 2, inf);
             cnt_mn.resize(U * 2);
             ps_add.resize(U * 2);
@@ -33,13 +32,13 @@ struct area_of_rect_union {
                 cnt_mn[U + q] = gsz(U + q);
             }
             for (uint q = U; --q;) {
-                seg_gr[q] = {seg_gr[q << 1].F, seg_gr[q << 1 | 1].S};
+                seg_gr[q] = {seg_gr[q << 1].first, seg_gr[q << 1 | 1].second};
                 upd(q);
             }
         }
 
-        inline constexpr T gsz(int v) {
-            return seg_gr[v].S - seg_gr[v].F + 1;
+        inline T gsz(int v) {
+            return seg_gr[v].second - seg_gr[v].first + 1;
         }
 
         void apply_add(uint v, T val) {
@@ -62,7 +61,7 @@ struct area_of_rect_union {
         }
 
         pair<T, T> seg_min_cmin(T ql, T qr, uint v) {
-            const T l = seg_gr[v].F, r = seg_gr[v].S;
+            const auto [l, r] = seg_gr[v];
             if (qr < l || r < ql) return {inf, 1};
             if (ql <= l && r <= qr) {
                 return {mn[v], cnt_mn[v]};
@@ -70,12 +69,12 @@ struct area_of_rect_union {
             push(v);
             const auto lf = seg_min_cmin(ql, qr, v << 1);
             const auto rg = seg_min_cmin(ql, qr, v << 1 | 1);
-            if (lf.F == rg.F) return {lf.F, lf.S + rg.S};
+            if (lf.first == rg.first) return {lf.first, lf.second + rg.second};
             return min(lf, rg);
         }
 
         void seg_add(T ql, T qr, uint v, T val) {
-            const T l = seg_gr[v].F, r = seg_gr[v].S;
+            const auto [l, r] = seg_gr[v];
             if (qr < l || r < ql) return;
             if (ql <= l && r <= qr) {
                 apply_add(v, val);
@@ -87,8 +86,8 @@ struct area_of_rect_union {
             upd(v);
         }
 
-        pair<T, T> seg_min_cmin(T ql, T qr) { return seg_min_cmin(ql, qr, 1); }
-        inline void seg_add(T ql, T qr, T val) { seg_add(ql, qr, 1, val); }
+        pair<T, T> seg_min_cmin(T ql, T qr) {return seg_min_cmin(ql, qr, 1);}
+        inline void seg_add(T ql, T qr, T val) {seg_add(ql, qr, 1, val);}
     };
 
     struct vseg {
@@ -96,9 +95,10 @@ struct area_of_rect_union {
         bool is_left;
     };
 
-    vec<vseg> store;
-    vec<T> pts;
+    vector<vseg> store;
+    vector<T> pts;
 
+public:
     area_of_rect_union(uint nrect = 0) {
         store.reserve(nrect * 2);
         pts.reserve(nrect * 2);
@@ -109,24 +109,22 @@ struct area_of_rect_union {
         if (y1 > y2) swap(y1, y2);
         if (x1 == x2 || y1 == y2) return;
         --y2;
-        store.pb({x1, y1, y2, 1});
-        store.pb({x2, y1, y2, 0});
-        pts.pb(y1);
-        pts.pb(y2);
+        store.emplace_back(x1, y1, y2, 1);
+        store.emplace_back(x2, y1, y2, 0);
+        pts.push_back(y1);
+        pts.push_back(y2);
     }
 
+    //O(nlog(n)), where n is amount of added rectangles
     ll calc_area() {
         if (store.empty()) return 0;
-        ll mny = *min_element(all(pts));
-        for (auto &i : pts) i -= mny;
-        for (auto &i : store) i.y1 -= mny, i.y2 -= mny;
         unify(pts);
         segtree_on_points st(pts);
-        sort(all(store), [&](const auto & v1, const auto & v2) {return v1.x < v2.x;});
+        sort(all(store), [&](const auto& v1, const auto& v2) {return v1.x < v2.x;});
         ll ans = 0, prx = 0;
-        for (const auto &seg : store) {
-            pll p = st.seg_min_cmin(0, pts.back());
-            ans += 1ll * (pts.back() + 1 - (p.F == 0 ? p.S : 0)) * (seg.x - prx);
+        for (const auto& seg : store) {
+            auto [mn, cmn] = st.seg_min_cmin(pts[0], pts.back());
+            ans += 1ll * (pts.back() - pts[0] + 1 - (mn == 0 ? cmn : 0)) * (seg.x - prx);
             st.seg_add(seg.y1, seg.y2, seg.is_left ? 1 : -1);
             prx = seg.x;
         }
@@ -134,4 +132,4 @@ struct area_of_rect_union {
     }
 };
 //Coordinates of rectangle endpoints are lattice points, i. e. rect(0, 0, 3, 3) has area 9.
-//area_of_rect_union<coordinats_type> kek([nrect]);
+//Usage: area_of_rect_union<coordinates_type> kek([nrect]);
