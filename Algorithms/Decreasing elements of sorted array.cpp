15ea6f856bb2cfe54968bbf6e23dfa2b9232e846
wery0 <wery0123@yandex.ru>
2023-01-27 10:38:19 +0300

    Minor codestyle improvements

--- Algorithms/Decreasing elements of sorted array.cpp
+++ Algorithms/Decreasing elements of sorted array.cpp
@@ -1,25 +1,25 @@
-//constraints: k, m_i >= 0
-//decreases sum(m) on min(k, sum(m)), minimizing max remaining element
-//returns sorted array after decreasing
+//Constraints: k, m_i >= 0
+//Decreases sum(m) on min(k, sum(m)), minimizing max remaining element
+//Returns sorted array after decreasing
 template<typename T>
-vec<T> remove_k(vec<T> m, T k) {
-    int a = m.size();
-    sort(all(m));
-    ll sm = accumulate(all(m), 0ll);
+vector<T> remove_k(vector<T> m, T k) {
+    const int n = m.size();
+    sort(m.begin(), m.end());
+    ll sm = accumulate(m.begin(), m.end(), 0ll);
     ll rem = max(0ll, sm - k);
     ll pr = 0;
-    for (int q = 0; q < a; ++q) {
-        if (m[q] == pr) continue;
-        ll len_suf = a - q;
-        ll tyt = len_suf * (m[q] - pr);
+    for (int i = 0; i < n; ++i) {
+        if (m[i] == pr) continue;
+        ll len_suf = n - i;
+        ll tyt = len_suf * (m[i] - pr);
         if (tyt <= rem) {
             rem -= tyt;
-            pr = m[q];
+            pr = m[i];
             continue;
         }
         ll can = pr + rem / len_suf;
         ll eso = rem % len_suf;
-        fill(m.begin() + q, m.end() - eso, can);
+        fill(m.begin() + i, m.end() - eso, can);
         fill(m.end() - eso, m.end(), can + 1);
         break;
     }
