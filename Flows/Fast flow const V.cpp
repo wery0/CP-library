6fdfdb1453b56c137fe7a2e2a070038dd7805c23
wery0 <wery0123@yandex.ru>
2023-01-27 08:42:00 +0300

    Struct -> class

--- Flows/Fast flow const V.cpp
+++ Flows/Fast flow const V.cpp
@@ -1,37 +1,18 @@
 template <int V, typename T>
-struct HLPP {
+class HLPP {
     const T INF = numeric_limits<T>::max();
     struct edge {
         int to, rev;
         T f;
 
         edge() = default;
-
         edge(int x, int y, T c): to(x), rev(y), f(c) {}
     };
 
-    int s = V - 2, t = V - 1;
     vector<edge> adj[V];
     vector<int> lst[V], gap[V];
     T ex[V];
     int mxh, h[V], cnt[V], work;
-
-    void clear() {
-        for (int i = 0; i < V; ++i) {
-            adj[i].clear();
-            lst[i].clear();
-            gap[i].clear();
-        }
-        mxh = work = 0;
-        fill(h, h + V, 0);
-        fill(cnt, cnt + V, 0);
-        fill(ex, ex + V, 0);
-    }
-
-    void add_edge(int x, int to, T f, int is_dir) {
-        adj[x].pb(edge(to, adj[to].size(), f));
-        adj[to].pb(edge(x, adj[x].size() - 1, is_dir ? 0 : f));
-    }
 
     void updh(int v, int nh) {
         work++;
@@ -39,8 +20,8 @@ struct HLPP {
         h[v] = nh;
         if (nh == V) return;
         cnt[nh]++, mxh = nh;
-        gap[nh].pb(v);
-        if (ex[v] > 0) lst[nh].pb(v);
+        gap[nh].push_back(v);
+        if (ex[v] > 0) lst[nh].push_back(v);
     }
 
     void globalRelabel() {
@@ -53,7 +34,7 @@ struct HLPP {
         while (!q.empty()) {
             int v = q.front();
             q.pop();
-            for (auto &e : adj[v]) {
+            for (auto& e : adj[v]) {
                 if (h[e.to] == V && adj[e.to][e.rev].f > 0) {
                     q.push(e.to), updh(e.to, h[v] + 1);
                 }
@@ -62,8 +43,8 @@ struct HLPP {
         }
     }
 
-    void push(int v, edge &e) {
-        if (ex[e.to] == 0) lst[h[e.to]].pb(e.to);
+    void push(int v, edge& e) {
+        if (ex[e.to] == 0) lst[h[e.to]].push_back(e.to);
         T df = min(ex[v], e.f);
         e.f -= df, adj[e.to][e.rev].f += df;
         ex[v] -= df, ex[e.to] += df;
@@ -71,7 +52,7 @@ struct HLPP {
 
     void discharge(int v) {
         int nh = V;
-        for (auto &e : adj[v]) {
+        for (auto& e : adj[v]) {
             if (e.f > 0) {
                 if (h[v] == h[e.to] + 1) {
                     push(v, e);
@@ -88,11 +69,31 @@ struct HLPP {
         }
     }
 
+public:
+    int s = V - 2, t = V - 1;
+
+    void add_edge(int x, int to, T f, int is_dir) {
+        adj[x].emplace_back(to, adj[to].size(), f);
+        adj[to].emplace_back(x, adj[x].size() - 1, is_dir ? 0 : f);
+    }
+
+    void clear() {
+        for (int i = 0; i < V; ++i) {
+            adj[i].clear();
+            lst[i].clear();
+            gap[i].clear();
+        }
+        mxh = work = 0;
+        fill(h, h + V, 0);
+        fill(cnt, cnt + V, 0);
+        fill(ex, ex + V, 0);
+    }
+
     T calc(int heur_n = V) {
         fill(ex, ex + V, 0);
         ex[s] = INF;
         globalRelabel();
-        for (auto &e : adj[s]) push(s, e);
+        for (auto& e : adj[s]) push(s, e);
         for (; mxh >= 0; --mxh) {
             while (!lst[mxh].empty()) {
                 int v = lst[mxh].back();
