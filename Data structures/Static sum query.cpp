65f623059ec91bc7a21d9061eb07f9aa5234638c
wery0 <wery0123@yandex.ru>
2023-01-28 16:49:46 +0300

    Removed dependency on old template

--- Data structures/Static sum query.cpp
+++ Data structures/Static sum query.cpp
@@ -1,19 +1,16 @@
 template<typename T>
 struct static_sum_query {
-    vec<T> m;
+    vector<T> m;
 
     static_sum_query() = default;
-
     template<typename I>
-    static_sum_query(I f, I l) {
-        m.resize(l - f + 1);
-        for (auto it = m.begin() + 1; f != l; ++f, ++it) {
-            *it = *(it - 1) + *f;
+    static_sum_query(I first, I last): m(last - first + 1) {
+        for (auto it = m.begin() + 1; first != last; ++first, ++it) {
+            *it = *(it - 1) + *first;
         }
     }
-
     template<typename T_arr> static_sum_query(T_arr& m) {
-        (*this) = static_sum_query(all(m));
+        (*this) = static_sum_query(m.begin(), m.end());
     }
 
     inline T query(const int l, const int r) const {
