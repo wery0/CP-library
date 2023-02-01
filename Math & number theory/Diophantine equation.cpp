c6e6885025dbd45581e035c2688834147e3f91a0
wery0 <wery0123@yandex.ru>
2023-02-01 06:00:38 +0300

    Shortened code, improved description, merged functions, removed namespace, function made template

--- Math & number theory/Diophantine equation.cpp
+++ Math & number theory/Diophantine equation.cpp
@@ -1,32 +1,30 @@
-namespace diophantine_equation {
-    ll gcd(ll a, ll b, ll &x, ll &y) {
+//Solves diophantine equation Ax + By = C.
+//Returns solution with minimal positive x.
+template<typename T>
+pair<T, T> diophantine_solver(T A, T B, T C) {
+    auto gcd = [](auto&& gcd, T a, T b, T& x, T& y) {
         if (!a) {
             x = 0, y = 1;
             return b;
         }
-        ll g = gcd(b % a, a, x, y);
-        ll nx = y - x * (b / a), ny = x;
+        T g = gcd(gcd, b % a, a, x, y);
+        T nx = y - x * (b / a), ny = x;
         x = nx, y = ny;
         return g;
+    };
+    T x, y, g = gcd(gcd, abs(A), abs(B), x, y);
+    if (A < 0) x *= -1;
+    if (B < 0) y *= -1;
+    assert(C % g == 0);               //No solution exists in this case. Handle it.
+    //if (C % g) return {-1, -1};     //Example of handling this case.
+    x *= C / g, y *= C / g;
+    T dx = B / g, dy = -A / g;
+    T c = x / dx;
+    x -= c * dx;
+    y -= c * dy;
+    if (x <= 0) {
+        if (dx > 0) x += dx, y += dy;
+        else x -= dx, y -= dy;
     }
-
-    //returns solution with minimal positive x.
-    pll solve(ll A, ll B, ll C) {
-        ll x, y, g = gcd(abs(A), abs(B), x, y);
-        if (A < 0) x *= -1;
-        if (B < 0) y *= -1;
-        //assert(C % g == 0);
-        if (C % g) return { -1, -1};
-        x *= C / g, y *= C / g;
-        ll dx = B / g, dy = -A / g;
-        ll c = x / dx;
-        x -= c * dx;
-        y -= c * dy;
-        if (x <= 0) {
-            if (dx > 0) x += dx, y += dy;
-            else x -= dx, y -= dy;
-        }
-        assert(x > 0);
-        return {x, y};
-    }
-};
+    return {x, y};
+}
