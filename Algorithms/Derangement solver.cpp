bb44f8ef9b75a1d5c1e3ea397ff9069baeb7bf03
wery0 <wery0123@yandex.ru>
2023-02-01 06:10:17 +0300

    Added description and constness

--- Algorithms/Derangement solver.cpp
+++ Algorithms/Derangement solver.cpp
@@ -1,5 +1,7 @@
+//Reorders elements of m such that ans[i] != m[i] for all i, if it's possible.
+//O(n)
 template<typename T>
-vector<T> derangement_solver(vector<T>& m) {
+vector<T> derangement_solver(const vector<T>& m) {
     int n = m.size(), diff = 0;
     umap<T, int> mp;
     vector<int> cnt;
@@ -7,7 +9,7 @@ vector<T> derangement_solver(vector<T>& m) {
         if (!mp.count(m[q])) mp[m[q]] = diff++, cnt.push_back(1);
         else ++cnt[mp[m[q]]];
     }
-    if (*max_element(all(cnt)) * 2 > n) return {};
+    if (*max_element(cnt.begin(), cnt.end()) * 2 > n) return {};
     partial_sum(cnt.begin(), cnt.end(), cnt.begin());
     vector<int> num(n), gde(n);
     vector<T> ans(n);
@@ -15,6 +17,6 @@ vector<T> derangement_solver(vector<T>& m) {
         num[q] = --cnt[mp[m[q]]];
         gde[num[q]] = q;
     }
-    for (int q = 0; q < n; q++) ans[q] = m[gde[(num[q] + a / 2) % a]];
+    for (int q = 0; q < n; q++) ans[q] = m[gde[(num[q] + n / 2) % n]];
     return ans;
 }
