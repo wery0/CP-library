8e671f6d4738a5fb0f0159c356056beb1793ce66
wery0 <wery0123@yandex.ru>
2023-02-04 07:32:25 +0300

    Improved description & namings

--- Geometry/Convex hull.cpp
+++ Geometry/Convex hull.cpp
@@ -1,20 +1,20 @@
-//Returns set of points of minimal size, generating CH, sorted in CCW order.
+//Returns smallest subset of arr which generates convex hull, sorted in CCW order.
 template<typename T>
-vector<pt<T>> convex_hull(vector<pt<T>> m) {
-    pt<T> mnp = *min_element(all(m));
-    for (auto& p : m) p -= mnp;
-    sort(all(m), [&](const auto & p1, const auto & p2) {
+vector<pt<T>> convex_hull(vector<pt<T>> arr) {
+    pt<T> mnp = *min_element(arr.begin(), arr.end());
+    for (auto& p : arr) p -= mnp;
+    sort(arr.begin(), arr.end(), [&](const auto& p1, const auto& p2) {
         T c = cross(p1, p2);
         return c ? c > 0 : abs(p1.x) + abs(p1.y) < abs(p2.x) + abs(p2.y);
     });
     vector<pt<T>> ch;
-    for (int q = 0; q < m.size(); ++q) {
+    for (int i = 0; i < arr.size(); ++i) {
         for (; ch.size() > 1;) {
-            if (cross(ch.back() - end(ch)[-2], m[q] - ch.back()) > 0) break;
+            if (cross(ch.back() - end(ch)[-2], arr[i] - ch.back()) > 0) break;
             ch.pop_back();
         }
-        ch.push_back(m[q]);
+        ch.push_back(arr[i]);
     }
     for (auto& p : ch) p += mnp;
     return ch;
-};
+}
