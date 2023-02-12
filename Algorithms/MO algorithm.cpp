a03988ff09d2d40303f5da5fbcf81ceeb88254d9
wery0 <wery0123@yandex.ru>
2023-02-12 15:05:04 +0300

    Improved codestyle

--- Algorithms/MO algorithm.cpp
+++ Algorithms/MO algorithm.cpp
@@ -1,56 +1,52 @@
 namespace MO {
 
-    //edit, if need
+    //Edit, if need
     const int MAXN = 300000;
     const int MAXQ = 300000;
-    const int BLOCK_SIZE = 512;
+    const int BLOCK_SIZE = (int)sqrt(MAXN);
 
     struct query {
         int l, r, n;
-        //add needed params
+        //Add needed params
     };
 
     query qarr[MAXQ];
     int qsz = 0;
-    ll ans[MAXQ];
+    int ans[MAXQ];
 
     inline void add_query(int l, int r) {
-        qarr[qsz]  = {l, r, qsz};
+        qarr[qsz] = {l, r, qsz};
         ++qsz;
     }
 
-    inline void addR(int ps) {
-
+    inline void addR(int i) {
     }
 
-    inline void addL(int ps) {
-
+    inline void addL(int i) {
     }
 
-    inline void remR(int ps) {
-
+    inline void remR(int i) {
     }
 
-    inline void remL(int ps) {
-
+    inline void remL(int i) {
     }
 
     void go() {
-        sort(qarr, qarr + qsz, [](const query & q1, const query & q2) {
+        sort(qarr, qarr + qsz, [](const query& q1, const query& q2) {
             int bl1 = q1.l / BLOCK_SIZE;
             int bl2 = q2.l / BLOCK_SIZE;
             if (bl1 != bl2) return bl1 < bl2;
             return (bool)((q1.r < q2.r) ^ (bl1 & 1));
         });
         int cl = 0, cr = -1;
-        for (int q = 0; q < qsz; q++) {
-            const auto &i = qarr[q];
-            while (cr < i.r) addR(++cr);
-            while (cl > i.l) addL(--cl);
-            while (cr > i.r) remR(cr--);
-            while (cl < i.l) remL(cl++);
-            //edit
-            ans[i.n] = 998244353;
+        for (int i = 0; i < qsz; i++) {
+            const auto& [ql, qr, qn] = qarr[i];
+            while (cr < qr) addR(++cr);
+            while (cl > ql) addL(--cl);
+            while (cr > qr) remR(cr--);
+            while (cl < ql) remL(cl++);
+            //Edit
+            ans[qn] = 998244353;
         }
     }
 };
