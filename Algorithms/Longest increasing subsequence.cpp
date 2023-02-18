05bf132d7bb14b1641c7cd2893f12f9a760d7b3c
wery0 <wery0123@yandex.ru>
2023-02-18 18:42:19 +0300

    Added description & example

--- Algorithms/Longest increasing subsequence.cpp
+++ Algorithms/Longest increasing subsequence.cpp
@@ -1,10 +1,16 @@
+//Returns positions of longest increasing subsequence.
+/*
+Example:
+f({-5, 3, 0, 7}) = {0, 2, 3}
+f({1, 1, 1}) = {2}
+*/
 template<typename T>
-vector<int> LIS_positions(vector<T> &m) {
-    int a = m.size(), len = 0, cur_pos = a - 1;
-    vector<T> mn(a + 1);
-    vector<int> cur_num(a + 1), pr(a + 1);
+vector<int> LIS_positions(vector<T>& m) {
+    int n = m.size(), len = 0, cur_pos = n - 1;
+    vector<T> mn(n + 1);
+    vector<int> cur_num(n + 1), pr(n + 1);
     cur_num[0] = pr[0] = -1;
-    for (int q = 0; q < a; ++q) {
+    for (int q = 0; q < n; ++q) {
         int ps = lower_bound(mn.begin() + 1, mn.begin() + len + 1, m[q]) - mn.begin();
         mn[ps] = m[q];
         cur_num[ps] = q;
