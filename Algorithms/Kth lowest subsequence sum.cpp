4d113a449985aa8814ba38bd3feeb975fe7616ee
wery0 <wery0123@yandex.ru>
2023-02-20 04:28:41 +0300

    Added complexity, push -> emplace

--- Algorithms/Kth lowest subsequence sum.cpp
+++ Algorithms/Kth lowest subsequence sum.cpp
@@ -1,20 +1,21 @@
 //Sum of all elements must fit in T
+//O(nlogn + klogk)
 template<typename T>
-T kth_lowest_subsequence_sum(vector<T> m, ll k) {
-    T o = 0;
-    for (T &i : m) if (i < 0) o += i, i *= -1;
-    if (k-- == 0) return o;
-    sort(all(m));
-    priority_queue<pair<T, int>, vector<pair<T, int>>, greater<pair<T, int>>> pq;
-    pq.push({m[0], 0});
-    for (; pq.size();) {
+T kth_lowest_subsequence_sum(vector<T> m, size_t k) {
+    T ans = 0;
+    for (T& i : m) if (i < 0) ans += i, i *= -1;
+    if (k-- == 0) return ans;
+    sort(m.begin(), m.end());
+    priority_queue<pair<T, size_t>, vector<pair<T, size_t>>, greater<pair<T, size_t>>> pq;
+    pq.emplace(m[0], 0);
+    while (!pq.empty()) {
         auto [sm, i] = pq.top(); pq.pop();
-        if (k-- == 0) return o + sm;
+        if (k-- == 0) return ans + sm;
         if (i + 1 != m.size()) {
-            pq.push({sm + m[i + 1] - m[i], i + 1});
-            pq.push({sm + m[i + 1], i + 1});
+            pq.emplace(sm + m[i + 1] - m[i], i + 1);
+            pq.emplace(sm + m[i + 1], i + 1);
         }
     }
-    assert(0);
+    assert(0 && "k must be < 2^n");
     return -1;
 }
