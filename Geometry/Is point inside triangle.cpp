d3b0dbb6291ec564fc6edb0f1de112e346968ed7
wery0 <wery0123@yandex.ru>
2023-02-23 13:22:19 +0300

    Simplified code

--- Geometry/Is point inside triangle.cpp
+++ Geometry/Is point inside triangle.cpp
@@ -1,13 +1,9 @@
+//Checks if point p is inside or on the border of triangle {p1, p2, p3}
 template<typename T>
-T double_triangle_surface(pt<T> p1, pt<T> p2, pt<T> p3) {
-    return abs(cross(p2 - p1, p3 - p1));
-}
-
-template<typename T>
-bool is_point_inside_triangle(pt<T> p1, pt<T> p2, pt<T> p3, pt<T> p) {
-    T s = double_triangle_surface(p1, p2, p3);
-    T s1 = double_triangle_surface(p, p2, p3);
-    T s2 = double_triangle_surface(p1, p, p3);
-    T s3 = double_triangle_surface(p1, p2, p);
+bool is_point_inside_triangle(const pt<T>& p1, const pt<T>& p2, const pt<T>& p3, const pt<T>& p) {
+    T s = abs(cross(p2 - p1, p3 - p1));
+    T s1 = abs(cross(p1 - p, p2 - p));
+    T s2 = abs(cross(p2 - p, p3 - p));
+    T s3 = abs(cross(p3 - p, p1 - p));
     return s == s1 + s2 + s3;
 }
