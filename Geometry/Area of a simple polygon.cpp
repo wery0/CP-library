6ff5f6e927f452e8d505a313c9acbaf06aff2bcb
wery0 <wery0123@yandex.ru>
2023-02-24 22:52:37 +0300

    Added 'Area of a simple polygon'

---
+++ Geometry/Area of a simple polygon.cpp
@@ -0,0 +1,12 @@
+//Simple polygon - a polygon that doesn't intersect itself and has no holes.
+template<typename T>
+T calc_area_of_simple_polygon(const vector<pt<T>>& m) {
+    if (m.empty()) return 0;
+    T ans = 0;
+    pt<T> pr = m.back();
+    for (const auto& p : m) {
+        ans += cross(pr, p);
+        pr = p;
+    }
+    return abs(ans);
+}
