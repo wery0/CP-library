140f457ece42ca8cec43efc91586b6d430579d3f
wery0 <wery0123@yandex.ru>
2023-02-28 17:28:11 +0300

    Fixed function name

--- Geometry/Area of a simple polygon.cpp
+++ Geometry/Area of a simple polygon.cpp
@@ -1,6 +1,6 @@
 //Simple polygon - a polygon that doesn't intersect itself and has no holes.
 template<typename T>
-T calc_area_of_simple_polygon(const vector<pt<T>>& m) {
+T calc_double_area_of_simple_polygon(const vector<pt<T>>& m) {
     if (m.empty()) return 0;
     T ans = 0;
     pt<T> pr = m.back();
