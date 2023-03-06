adec9d027e8b39e3fd82075bd22966e60f801fa7
wery0 <wery0123@yandex.ru>
2023-03-06 05:04:27 +0300

    Renamed

--- Geometry/Tangents to convex polygon from point.cpp
+++ Geometry/Tangents to convex polygon from point.cpp
@@ -10,17 +10,17 @@
 If line(p, ch[A]) contains some ch[T] (T != A), then 'furthest_points' decides, will A be reptaced with T or not.
 The same logic with B.
 */
-//Requirement: ch - points of convex hull of polygon, ordered in CCW order, starting from the lowest of the leftmost points.
+//Requirement: ch - points of strict convex hull, ordered in CCW order, starting from the lowest of the leftmost points.
 //Complexity: O(log(n))
 //Correctness: Stress tested
 //Note: Handle cases 0 <= n <= 2 differently, if need
 template<typename T>
-pair<int, int> get_tangents_to_polygon(const vector<pt<T>>& ch, const pt<T>& p,
-                                       pair<int, int> location = {-3, -3}, bool furthest_points = false) {
+pair<int, int> get_tangents_to_convex_polygon_from_point(const vector<pt<T>>& ch, const pt<T>& p,
+        bool furthest_points = false, pair<int, int> location = {-3, -3}) {
     static constexpr pair<int, int> NO = {-1, -1};
     auto sign = [](T x) -> int {return x < 0 ? -1 : x > 0 ? 1 : 0;};
     auto eval = [](const line<T>& l, const pt<T>& p) -> T {return l.A * p.x + l.B * p.y + l.C;};
-    const int n = ch.size();
+    const size_t n = ch.size();
     if (location.first == -3) location = point_location_convex_polygon(ch, p);
     if (location.first == -1) return NO;
     if (location.first >= 0) {
@@ -40,22 +40,22 @@ pair<int, int> get_tangents_to_polygon(const vector<pt<T>>& ch, const pt<T>& p,
         } else return {0, 1};
     }
     line<T> l(p, ch[0]);
-    int lf = 0, rg = n;
+    size_t lf = 0, rg = n;
     int sgn = sign(eval(l, ch[1]));
     if (sgn == 0) {
         lf = 1;
     } else {
         while (lf + 1 < rg) {
-            int md = (lf + rg) / 2;
+            size_t md = (lf + rg) / 2;
             int tyt = sign(eval(l, ch[md]));
             if (tyt == -sgn) rg = md;
             else lf = md;
         }
     }
-    auto go = [&](int l, int r) -> int {
+    auto go = [&](size_t l, size_t r) -> size_t {
         int st = sign(cross(ch[l] - p, ch[(l + 1) % n] - p));
         while (l + 1 < r) {
-            int md = (l + r) / 2;
+            size_t md = (l + r) / 2;
             int tyt = sign(cross(ch[md] - p, ch[(md + 1) % n] - p));
             if (tyt == st) l = md;
             else r = md;
@@ -66,10 +66,10 @@ pair<int, int> get_tangents_to_polygon(const vector<pt<T>>& ch, const pt<T>& p,
             return mdist(p, ch[(l + 1) % n]) < mdist(p, ch[(l + 2) % n]) ? (l + 1) % n : (l + 2) % n;
         } else return (l + 1) % n;
     };
-    int p1 = go(0, lf);
-    int p2 = go(lf, n);
+    size_t p1 = go(0, lf);
+    size_t p2 = go(lf, n);
     int need_sign = sign(cross(ch[p2] - ch[p1], p - ch[p1]));
-    int np = (p1 + 1) % n;
+    size_t np = (p1 + 1) % n;
     if (np == p2) {
         swap(p1, p2);
         need_sign *= -1;
