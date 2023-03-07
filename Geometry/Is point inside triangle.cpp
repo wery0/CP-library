a342edcee07cd0b341e4ba892a71e25d20053319
wery0 <wery0123@yandex.ru>
2023-03-07 23:07:27 +0300

    Added maintaining of degenerative triangles

--- Geometry/Is point inside triangle.cpp
+++ Geometry/Is point inside triangle.cpp
@@ -5,5 +5,11 @@ bool is_point_inside_triangle(const pt<T>& p1, const pt<T>& p2, const pt<T>& p3,
     T s1 = abs(cross(p1 - p, p2 - p));
     T s2 = abs(cross(p2 - p, p3 - p));
     T s3 = abs(cross(p3 - p, p1 - p));
+    if (s == 0 && s1 + s2 + s3 == 0) {
+        T d1 = dot(p1 - p, p2 - p);
+        T d2 = dot(p2 - p, p3 - p);
+        T d3 = dot(p3 - p, p1 - p);
+        return min({d1, d2, d3}) <= 0 && max({d1, d2, d3}) >= 0;
+    }
     return s == s1 + s2 + s3;
 }
