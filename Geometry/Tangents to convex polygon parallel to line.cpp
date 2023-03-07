954dc57d84ba607e63f70d522084dd7ba645adca
wery0 <wery0123@yandex.ru>
2023-03-08 01:36:47 +0300

    Added support for floating point convex hull

--- Geometry/Tangents to convex polygon parallel to line.cpp
+++ Geometry/Tangents to convex polygon parallel to line.cpp
@@ -11,18 +11,19 @@ Same logic applies for P2.
 //Complexity: O(log(n))
 //Correctness: Stress tested with linear version of this function
 //Note: Be careful with cases 0 <= n <= 2
-template<typename T>
-pair<pair<int, int>, pair<int, int>> get_tangents_to_convex_polygon_parallel_to_line(const vector<pt<T>>& ch, const line<T>& l) {
+template<typename T, typename U>
+pair<pair<int, int>, pair<int, int>> get_tangents_to_convex_polygon_parallel_to_line(const vector<pt<T>>& ch, const line<U>& l) {
+    const U eps = is_integral_v<U> ? 0 : EPS;
     const size_t n = ch.size();
     if (n == 0) return {{-1, -1}, {-1, -1}};
     if (n == 1) return {{0, 0}, {0, 0}};
-    auto cmp = [&](const auto & l, const auto & r) -> bool {
-        int pl = l.x > 0 ? 0 : !l.x && l.y >= 0 ? 1 : 2;
-        int pr = r.x > 0 ? 0 : !r.x && r.y >= 0 ? 1 : 2;
-        return pl != pr ? pl < pr : l.x * r.y - l.y * r.x > 0;
+    auto cmp = [&](const pt<T>& l, const pt<U>& r) -> bool {
+        int pl = l.x > 0 ? 0 : l.x == 0 && l.y >= 0 ? 1 : 2;
+        int pr = r.x > eps ? 0 : abs(r.x) <= eps && r.y >= -eps ? 1 : 2;
+        return pl != pr ? pl < pr : cross(l, r) > eps;
     };
     auto get_side = [&](size_t i) -> pt<T> {return ch[(i + 1) % n] - ch[i];};
-    auto binsearch = [&](const pt<T>& v) -> size_t {
+    auto binsearch = [&](const pt<U>& v) -> size_t {
         ssize_t l = -1, r = n;
         while (l + 1 < r) {
             ssize_t md = l + (r - l) / 2;
@@ -30,13 +31,13 @@ pair<pair<int, int>, pair<int, int>> get_tangents_to_convex_polygon_parallel_to_
         }
         return r % n;
     };
-    pt<T> v1(l.B, -l.A), v2 = -v1;
+    pt<U> v1(l.B, -l.A), v2 = -v1;
     size_t p1 = binsearch(v1);
     size_t p2 = binsearch(v2);
     pair<int, int> ans1 = {p1, p1};
     pair<int, int> ans2 = {p2, p2};
-    if (cross(get_side(p1), v1) == 0) ans1.second = (ans1.second + 1) % n;
-    if (cross(get_side(p2), v2) == 0) ans2.second = (ans2.second + 1) % n;
+    if (abs(cross(get_side(p1), v1)) <= eps) ans1.second = (ans1.second + 1) % n;
+    if (abs(cross(get_side(p2), v2)) <= eps) ans2.second = (ans2.second + 1) % n;
     if (p1 > p2) swap(ans1, ans2);
     return {ans1, ans2};
 }
