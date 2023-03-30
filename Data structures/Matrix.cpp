1317230f7c3d744f3d5ba4364119f9cd6cd332b6
wery0 <wery0123@yandex.ru>
2023-03-30 05:15:10 +0300

    Improved namings and codestyle, removed dependency on template

--- Data structures/Matrix.cpp
+++ Data structures/Matrix.cpp
@@ -1,143 +1,66 @@
 template<typename T>
 class Matrix {
-    int a = 0, b = 0;
+    size_t a = 0, b = 0;
     vector<vector<T>> m;
-
-    T calc_determimant_() {
-        assert(a == b);
-        T res = 1;
-        auto n = m;
-        for (int q = 0; q < a; ++q) {
-            int qq = q;
-            while (qq < a && n[qq][q] == 0) ++qq;
-            if (qq == a) return 0;
-            if (qq != q) swap(n[q], n[qq]), res *= T(-1);
-            res *= n[q][q];
-            T inv = T(1) / n[q][q];
-            for (int w = q; w < a; ++w) n[q][w] *= inv;
-            for (int i = q + 1; i < a; ++i) {
-                const T koef = n[i][q];
-                if (koef == 0) continue;
-                for (int w = q; w < a; ++w) n[i][w] -= n[q][w] * koef;
-            }
-        }
-        return res;
-    }
-
-    Matrix calc_inverse_() {
-        assert(a == b);
-        auto n = m;
-        for (int q = 0; q < a; ++q) {
-            n[q].resize(a * 2);
-            n[q][a + q] = 1;
-        }
-        for (int q = 0; q < a; ++q) {
-            int qq = q;
-            while (qq < a && n[qq][q] == 0) ++qq;
-            assert(qq < a);                             //Inverse matrix doesn't exist
-            if (qq != q) swap(n[q], n[qq]);
-            T inv = T(1) / n[q][q];
-            for (int w = q; w < a * 2; ++w) n[q][w] *= inv;
-            for (int i = q + 1; i < a; ++i) {
-                const T koef = n[i][q];
-                if (koef == 0) continue;
-                for (int w = q; w < a * 2; ++w) n[i][w] -= n[q][w] * koef;
-            }
-        }
-        for (int q = 1; q < a; ++q) {
-            for (int i = 0; i < q; ++i) {
-                const T koef = n[i][q];
-                if (koef == 0) continue;
-                for (int w = q; w < a * 2; ++w) {
-                    n[i][w] -= koef * n[q][w];
-                }
-            }
-        }
-        for (int q = 0; q < a; ++q) {
-            rotate(n[q].begin(), n[q].begin() + a, n[q].end());
-            n[q].resize(a);
-        }
-        return n;
-    }
 
 public:
     Matrix() = default;
-    Matrix(int a, int b): a(a), b(b) {m.resize(a, vector<T>(b));}
-    Matrix(const Matrix<T>& o) {a = o.a, b = o.b, m = o.m;}
+    Matrix(size_t n, size_t m): a(n), b(m), m(a, vector<T>(b)) {}
+    Matrix(const Matrix<T>& rhs): a(rhs.a), b(rhs.b), m(rhs.m) {}
+
     template<typename U>
-    Matrix(const vector<vector<U>>& o) {
-        a = o.size(), b = a ? o[0].size() : 0;
-        m.resize(a, vector<T>(b));
-        for (int q = 0; q < a; ++q) for (int w = 0; w < b; ++w) m[q][w] = o[q][w];
+    Matrix(const vector<vector<U>>& o): a(o.size()), b(a ? o[0].size() : 0), m(a) {
+        for (size_t i = 0; i < a; ++i) m[i] = vector<T>(o[i].begin(), o[i].end());
     }
+
     template<typename U>
-    Matrix(const initializer_list<initializer_list<U>>& l) {
-        a = l.size(), b = a ? (*l.begin()).size() : 0;
-        m.resize(a, vector<T>(b));
-        int q = 0;
-        for (const auto& i : l) {
-            assert(b == i.size());
-            int w = 0;
-            for (const auto &j : i) m[q][w++] = j;
-            ++q;
+    Matrix(const initializer_list<initializer_list<U>>& l): a(l.size()), b(a ? (*l.begin()).size() : 0), m(a) {
+        for (size_t i = 0; const auto& c : l) {
+            assert(b == c.size());
+            m[i++] = vector<T>(c.begin(), c.end());
         }
     }
 
-    Matrix& operator=(const Matrix<T>& o) {a = o.a, b = o.b, m = o.m; return *this;}
-    Matrix& operator=(ll x) {for (int q = 0; q < min(a, b); ++q) m[q][q] = x; return *this;}
+    Matrix& operator=(const Matrix<T>& rhs) {a = rhs.a, b = rhs.b, m = rhs.m; return *this;}
+    Matrix& operator=(T val) {for (size_t i = 0; i < min(a, b); ++i) m[i][i] = val; return *this;}
 
-    Matrix operator+(const Matrix<T>& o) const {
-        assert(is_sum_compatible(o));
-        Matrix<T> res = *this;
-        for (int q = 0; q < a; ++q) for (int w = 0; w < b; ++w) res[q][w] += o[q][w];
-        return res;
-    }
-    Matrix operator-(const Matrix<T>& o) const {
-        assert(is_sum_compatible(o));
-        Matrix<T> res = *this;
-        for (int q = 0; q < a; ++q) for (int w = 0; w < b; ++w) res[q][w] -= o[q][w];
-        return res;
-    }
-    Matrix operator*(const Matrix<T>& o) const {
-        assert(is_mul_compatible(o));
-        int c = o.b;
-        Matrix<T> res(a, c);
-        Matrix<T> tro = o;
-        tro.self_transpose();
-        for (int q = 0; q < a; ++q) {
-            for (int w = 0; w < c; ++w) {
+    Matrix operator+(const Matrix<T>& rhs) const {assert(is_sum_compatible(rhs)); Matrix<T> res = *this; for (size_t i = 0; i < a; ++i) for (size_t j = 0; j < b; ++j) res[i][j] += rhs[i][j]; return res;}
+    Matrix operator-(const Matrix<T>& rhs) const {assert(is_sum_compatible(rhs)); Matrix<T> res = *this; for (size_t i = 0; i < a; ++i) for (size_t j = 0; j < b; ++j) res[i][j] -= rhs[i][j]; return res;}
+    Matrix operator*(const Matrix<T>& rhs) const {
+        assert(is_mul_compatible(rhs));
+        const size_t c = rhs.b;
+        Matrix<T> res(a, c), tr = rhs.get_transpose();
+        for (size_t i = 0; i < a; ++i) {
+            for (size_t j = 0; j < c; ++j) {
 #pragma GCC unroll 16
-                for (int e = 0; e < b; ++e) res[q][w] += m[q][e] * tro[w][e];
+                for (size_t k = 0; k < b; ++k) res[i][j] += m[i][k] * tr[j][k];
             }
         }
         return res;
     }
-    void operator+=(const Matrix<T>& o) {assert(is_sum_compatible(o)); for (int q = 0; q < a; ++q) for (int w = 0; w < b; ++w) m[q][w] += o[q][w];}
-    void operator-=(const Matrix<T>& o) {assert(is_sum_compatible(o)); for (int q = 0; q < a; ++q) for (int w = 0; w < b; ++w) m[q][w] += o[q][w];}
-    void operator*=(const Matrix<T>& o) {(*this) = (*this) * o;}
-    const vector<T>& operator[](uint x) const {assert(0 <= x && x < a); return m[x];}
-    vector<T>& operator[](uint x) {assert(0 <= x && x < a); return m[x];}
+    Matrix& operator+=(const Matrix<T>& rhs) {assert(is_sum_compatible(rhs)); for (size_t i = 0; i < a; ++i) for (size_t j = 0; j < b; ++j) m[i][j] += rhs[i][j]; return *this;}
+    Matrix& operator-=(const Matrix<T>& rhs) {assert(is_sum_compatible(rhs)); for (size_t i = 0; i < a; ++i) for (size_t j = 0; j < b; ++j) m[i][j] -= rhs[i][j]; return *this;}
+    Matrix& operator*=(const Matrix<T>& rhs) {return *this = *this * rhs;}
 
-    size_t size() {return a;}
-    void fill(T x) {for (auto& i : m) std::fill(all(i), x);}
+    vector<T>& operator[](size_t i) {assert(0 <= i && i < a); return m[i];}
+    const vector<T>& operator[](size_t i) const {assert(0 <= i && i < a); return m[i];}
 
-    inline bool is_sum_compatible(const Matrix& o) const {return a == o.a && b == o.b;}
-    inline bool is_mul_compatible(const Matrix& o) const {return b == o.a;}
+    size_t size() const {return a;}
+    void fill(T val) {for (auto& row : m) fill(row.begin(), row.end(), val);}
 
-    Matrix get_pow(ll k) {
-        assert(a == b);
-        Matrix<T> o(a, a), x = *this;
-        for (o = 1; k; k >>= 1, x *= x) if (k & 1) o *= x;
-        return o;
-    }
-    void self_pow(ll k) {*this = get_pow(k);}
+    bool is_sum_compatible(const Matrix& rhs) const {return a == rhs.a && b == rhs.b;}
+    bool is_mul_compatible(const Matrix& rhs) const {return b == rhs.a;}
 
-    T calc_determinant() {return calc_determimant_();}
-    Matrix calc_inverse() {return calc_inverse_();}
+    Matrix get_pow(long long k) const {assert(a == b); assert(k >= 0); Matrix<T> res(a, a), x = *this; for (res = 1; k; k >>= 1, x *= x) if (k & 1) res *= x; return res;}
+    void self_pow(long long k) {*this = get_pow(k);}
 
-    Matrix get_transpose() {vector<vector<T>> n(b, vector<T>(a)); for (int w = 0; w < b; ++w) for (int q = 0; q < a; ++q) n[w][q] = m[q][w]; return Matrix(n);}
+    //For these two functions, T must be invertible
+    T calc_determinant() const {assert(a == b); T res = 1; auto n = m; for (size_t i = 0; i < a; ++i) {size_t ii = i; while (ii < a && n[ii][i] == 0) ++ii; if (ii == a) return 0; if (ii != i) swap(n[i], n[ii]), res *= T(-1); res *= n[i][i]; T inv = T(1) / n[i][i]; for (size_t j = i; j < a; ++j) n[i][j] *= inv; for (size_t ii = i + 1; ii < a; ++ii) {const T koef = n[ii][i]; if (koef == 0) continue; for (size_t j = i; j < a; ++j) n[ii][j] -= n[i][j] * koef;}} return res;}
+    Matrix calc_inverse() const {assert(a == b); auto n = m; for (size_t i = 0; i < a; ++i) {n[i].resize(a * 2); n[i][a + i] = 1;} for (size_t i = 0; i < a; ++i) {size_t ii = i; while (ii < a && n[ii][i] == 0) ++ii; assert(ii < a && "Inverse matrix doesn't exist"); if (ii != i) swap(n[i], n[ii]); T inv = T(1) / n[i][i]; for (size_t j = i; j < a * 2; ++j) n[i][j] *= inv; for (size_t ii = i + 1; ii < a; ++ii) {const T koef = n[ii][i]; if (koef == 0) continue; for (size_t j = i; j < a * 2; ++j) n[ii][j] -= n[i][j] * koef;}} for (size_t q = 1; q < a; ++q) {for (size_t i = 0; i < q; ++i) {const T koef = n[i][q]; if (koef == 0) continue; for (size_t j = q; j < a * 2; ++j) {n[i][j] -= koef * n[q][j];}}} for (size_t i = 0; i < a; ++i) {rotate(n[i].begin(), n[i].begin() + a, n[i].end()); n[i].resize(a);} return n;}
+
+    Matrix get_transpose() const {vector<vector<T>> n(b, vector<T>(a)); for (size_t j = 0; j < b; ++j) for (size_t i = 0; i < a; ++i) n[j][i] = m[i][j]; return Matrix(n);}
     void self_transpose() {*this = get_transpose();}
 
-    friend istream& operator>>(istream& in, Matrix& m) {for (int q = 0; q < m.a; ++q) for (int w = 0; w < m.b; ++w) in >> m[q][w]; return in;}
-    friend ostream& operator<<(ostream& out, const Matrix& m) {for (int q = 0; q < m.a; ++q) {for (int w = 0; w < m.b; ++w) {out << m[q][w]; if (w + 1 != m.b) out << ' ';} out << '\n';} return out;}
+    friend istream& operator>>(istream& in, Matrix& m) {for (size_t i = 0; i < m.a; ++i) for (size_t j = 0; j < m.b; ++j) in >> m[i][j]; return in;}
+    friend ostream& operator<<(ostream& out, const Matrix& m) {for (size_t i = 0; i < m.a; ++i) for (size_t j = 0; j < m.b; ++j) out << m[i][j] << " \n"[j == m.b - 1]; return out;}
 };
