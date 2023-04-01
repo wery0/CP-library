f426eae82e00e9a066fbe844d3f874b377745e43
wery0 <wery0123@yandex.ru>
2023-04-01 18:02:02 +0300

    Added description, removed dependency on template

--- Algorithms/Kadane's algorithm.cpp
+++ Algorithms/Kadane's algorithm.cpp
@@ -1,43 +1,39 @@
-//shortest subsegment with max sum
-template<typename I>
-auto subsegment_with_max_sum(I first, I last) {
-    ll cur = 0, mxs = 0;
-    pii seg_borders = { -1, -1};
+//Finds shortest subsegment with max sum
+//Sum of any subsegment must fit in T
+template<typename I, typename T = long long>
+T subsegment_with_max_sum(I first, I last) {
+    T cur = 0, mxs = 0;
+    array<int, 2> seg_borders = {-1, -1};
     int cur_l = -1;
     auto cit = first;
-    for (int q = 0; cit != last; ++q, ++cit) {
+    for (int i = 0; cit != last; ++i, ++cit) {
         cur += *cit;
-        if (cur > mxs || (cur == mxs && q - cur_l < seg_borders.S - seg_borders.F)) {
+        if (cur > mxs || (cur == mxs && i - cur_l < seg_borders[1] - seg_borders[0])) {
             mxs = cur;
-            seg_borders = {cur_l + 1, q};
+            seg_borders = {cur_l + 1, i};
         }
-        if (cur <= 0) {
-            cur = 0;
-            cur_l = q;
-        }
+        if (cur <= 0) cur = 0, cur_l = i;
     }
-    if (seg_borders.F == -1) return *max_element(first, last);
+    if (seg_borders[0] == -1) return *max_element(first, last);
     return mxs;
 }
 
-//shortest subsegment with min sum
-template<typename I>
-auto subsegment_with_min_sum(I first, I last) {
-    ll cur = 0, mns = 0;
-    pii seg_borders = { -1, -1};
+//Finds shortest subsegment with min sum
+//Sum of any subsegment must fit in T
+template<typename I, typename T = long long>
+T subsegment_with_min_sum(I first, I last) {
+    T cur = 0, mns = 0;
+    array<int, 2> seg_borders = {-1, -1};
     int cur_l = -1;
     auto cit = first;
-    for (int q = 0; cit != last; ++q, ++cit) {
+    for (int i = 0; cit != last; ++i, ++cit) {
         cur += *cit;
-        if (cur < mns || (cur == mns && q - cur_l < seg_borders.S - seg_borders.F)) {
+        if (cur < mns || (cur == mns && i - cur_l < seg_borders[1] - seg_borders[0])) {
             mns = cur;
-            seg_borders = {cur_l + 1, q};
+            seg_borders = {cur_l + 1, i};
         }
-        if (cur >= 0) {
-            cur = 0;
-            cur_l = q;
-        }
+        if (cur >= 0) cur = 0, cur_l = i;
     }
-    if (seg_borders.F == -1) return *min_element(first, last);
+    if (seg_borders[0] == -1) return *min_element(first, last);
     return mns;
 }
