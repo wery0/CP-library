0ea1803e9c79cd043cfe75d5a54c19e2b288a08a
wery0 <wery0123@yandex.ru>
2023-04-01 05:45:36 +0300

    Fixed bug in ctor

--- Graphs/Graph/Max bipartite matching kyhn.cpp
+++ Graphs/Graph/Max bipartite matching kyhn.cpp
@@ -1,16 +1,16 @@
-struct MaxBipartiteMatchingKyhn {
-private:
-    int L, R;
+class MaxBipartiteMatchingKyhn {
+    int L, R, tim = 0;
     graph<edge<void>> g;
-    vector<int> mtl,  mtr;
-    vector<bool> us;
+    vector<int> mtl, mtr, us;
     bool sw = 0;
 
     int dfs(int v) {
-        if (us[v]) return 0;
-        us[v] = 1;
-        for (const auto &h : g[v]) if (mtr[h.to] == -1) return mtr[h.to] = v, mtl[v] = h.to, 1;
-        for (const auto &h : g[v]) if (dfs(mtr[h.to])) return mtr[h.to] = v, mtl[v] = h.to, 1;
+        if (us[v] == tim) return 0;
+        us[v] = tim;
+        for (const auto& [h] : g[v]) if (mtr[h] == -1) return mtr[h] = v, mtl[v] = h, 1;
+        rotate(g[v].begin(), g[v].begin() + g[v].size() / 3, g[v].end());
+        //shuffle(g[v].begin(), g[v].end(), rnd);
+        for (const auto& [h] : g[v]) if (dfs(mtr[h])) return mtr[h] = v, mtl[v] = h, 1;
         return 0;
     }
 
@@ -31,21 +31,20 @@ public:
     vector<pair<int, int>> calc() {
         g.prepare();
         int mt_size = 0;
-        for (;;) {
-            fill(all(us), 0);
+        while (true) {
+            ++tim;
             int was = mt_size;
-            for (int q = 0; q < L; ++q) {
-                if (mtl[q] == -1) mt_size += dfs(q);
+            for (int i = 0; i < L; ++i) {
+                if (mtl[i] == -1) mt_size += dfs(i);
             }
             if (was == mt_size) break;
         }
-        vector<pair<int, int>> ans(mt_size);
-        for (int q = 0, i = 0; q < L; ++q) {
-            if (mtl[q] != -1) {
-                ans[i++] = {q, mtl[q]};
-            }
+        vector<pair<int, int>> ans;
+        ans.reserve(mt_size);
+        for (int i = 0; i < L; ++i) {
+            if (mtl[i] != -1) ans.emplace_back(i, mtl[i]);
         }
-        if (sw) for (auto &p : ans) swap(p.F, p.S);
+        if (sw) for (auto& [x, y] : ans) swap(x, y);
         return ans;
     }
 };
--- Graphs/Max bipartite matching kyhn.cpp
+++ Graphs/Max bipartite matching kyhn.cpp
@@ -1,20 +1,21 @@
 class MaxBipartiteMatchingKyhn {
-    int L, R;
+    int L, R, tim = 0;
     vector<vector<int>> l;
-    vector<int> mtl, mtr;
-    vector<bool> us;
+    vector<int> mtl, mtr, us;
     bool sw = 0;
 
     int dfs(int v) {
-        if (us[v]) return 0;
-        us[v] = 1;
+        if (us[v] == tim) return 0;
+        us[v] = tim;
         for (int h : l[v]) if (mtr[h] == -1) return mtr[h] = v, mtl[v] = h, 1;
+        //shuffle(l[v].begin(), l[v].end(), rnd);
+        rotate(l[v].begin(), l[v].begin() + l[v].size() / 3, l[v].end());
         for (int h : l[v]) if (dfs(mtr[h])) return mtr[h] = v, mtl[v] = h, 1;
         return 0;
     }
 
 public:
-    MaxBipartiteMatchingKyhn(int L, int R): L(L), R(R) {
+    MaxBipartiteMatchingKyhn(int _L, int _R): L(_L), R(_R) {
         if (L > R) swap(L, R), sw = 1;
         l.resize(L);
         mtl.resize(L, -1);
@@ -29,19 +30,18 @@ public:
 
     vector<pair<int, int>> calc() {
         int mt_size = 0;
-        for (;;) {
-            fill(us.begin(), us.end(), 0);
+        while (true) {
+            ++tim;
             int was = mt_size;
-            for (int q = 0; q < L; ++q) {
-                if (mtl[q] == -1) mt_size += dfs(q);
+            for (int i = 0; i < L; ++i) {
+                if (mtl[i] == -1) mt_size += dfs(i);
             }
             if (was == mt_size) break;
         }
-        vector<pair<int, int>> ans(mt_size);
-        for (int q = 0, i = 0; q < L; ++q) {
-            if (mtl[q] != -1) {
-                ans[i++] = {q, mtl[q]};
-            }
+        vector<pair<int, int>> ans;
+        ans.reserve(mt_size);
+        for (int i = 0; i < L; ++i) {
+            if (mtl[i] != -1) ans.emplace_back(i, mtl[i]);
         }
         if (sw) for (auto& [x, y] : ans) swap(x, y);
         return ans;
