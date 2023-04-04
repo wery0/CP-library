8ba27b9b9ce54fb0835c18accf5dd7bd1d8a1f64
wery0 <wery0123@yandex.ru>
2023-04-04 09:51:41 +0300

    Added maintaining of contiguos segments, added erase_seg() method, improved codestyle

--- Data structures/Segment union (size + decomposition).cpp
+++ Data structures/Segment union (size + decomposition).cpp
@@ -1,33 +1,57 @@
-template<typename T>
-struct segment_union {
+//Maintains union of segments [l, r]
+//if is_lattice == true, then segment [l, r] contains numbers l, l + 1, ..., r and T must be integral
+//if is_lattice == false, then segment [l, r] is contiguous
+template<typename T, const bool is_lattice = true>
+class segment_union {
+    static_assert(is_integral_v<T> || !is_lattice);
     set<pair<T, T>> s;
     T sz = 0;
 
+public:
     segment_union() = default;
 
-    inline void add_seg(T l, T r) {
+    void clear() {s.clear(); sz = 0;}
+
+    void add_seg(T l, T r) {
+        assert(l <= r);
+        if (!is_lattice && l == r) return;
         auto it = s.upper_bound({l, numeric_limits<T>::min()});
         if (it != s.begin() && (--it)->second < l) ++it;
         while (it != s.end() && it->first <= r) {
-            chmin(l, it->first);
-            chmax(r, it->second);
-            sz -= it->second - it->first + 1;
+            l = min(l, it->first);
+            r = max(r, it->second);
+            sz -= it->second - it->first + is_lattice;
             it = s.erase(it);
         }
-        s.insert({l, r});
-        sz += r - l + 1;
+        s.emplace(l, r);
+        sz += r - l + is_lattice;
     }
 
-    inline T size() {return sz;}
+    void erase_seg(T l, T r) {
+        assert(l <= r);
+        if (!is_lattice && l == r) return;
+        add_seg(l, r);
+        auto it = prev(s.upper_bound({l + 1, l}));
+        auto [sl, sr] = *it;
+        s.erase(it);
+        sz -= sr - sl + is_lattice;
+        if (sl < l) s.emplace(sl, l - is_lattice), sz += l - sl;
+        if (sr > r) s.emplace(r + is_lattice, sr), sz += sr - r;
+    }
 
-    //Returns list of segments of minimal size, which union equals to the union of containing segments.
-    //{[1, 2], [2, 3], [5, 8], [7, 10]} -> {[1, 3], [5, 10]}
-    vector<pair<T, T>> get_segmentation() {
+    //If is_lattice == true returns # numbers in union of all segments
+    //If is_lattice == false returns length of union of all segments
+    T size() const {return sz;}
+
+    //Returns list of segments of minimal size, which union equals to the union of containing segments
+    //{[1, 2], [2, 3], [5, 8], [7, 10], [12, 13], [14, 15]} -> {[1, 3], [5, 10], [12, 15]} if is_lattice == true
+    //{[1, 2], [2, 3], [5, 8], [7, 10], [12, 13], [14, 15]} -> {[1, 3], [5, 10], [12, 13], [14, 15]} if is_lattice == false
+    vector<pair<T, T>> get_segmentation() const {
         if (s.empty()) return {};
         vector<pair<T, T>> m;
-        auto [l, r] = *(s.begin());
+        auto [l, r] = *s.begin();
         for (auto it = next(s.begin()); it != s.end(); ++it) {
-            if ((*it).first - 1 == r) r = (*it).second;
+            if ((*it).first - is_lattice == r) r = (*it).second;
             else {
                 m.emplace_back(l, r);
                 l = (*it).first;
@@ -38,4 +62,3 @@ struct segment_union {
         return m;
     }
 };
-//Maintains size of union of segments [l, r].
