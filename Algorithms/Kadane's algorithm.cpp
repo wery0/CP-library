4a48fc9292e8d1d3b28f15d62fd4f54c9e1dfc00
wery0 <wery0123@yandex.ru>
2023-04-10 10:26:57 +0300

    Improved description, processed case n = 0

--- Algorithms/Kadane's algorithm.cpp
+++ Algorithms/Kadane's algorithm.cpp
@@ -1,7 +1,10 @@
 //Finds shortest subsegment with max sum
 //Sum of any subsegment must fit in T
+//If first == last - returns 0
+//If [first, last) doesn't contain positive numbers - returns max number
 template<typename I, typename T = long long>
 T subsegment_with_max_sum(I first, I last) {
+    if (first == last) return 0;
     T cur = 0, mxs = 0;
     array<int, 2> seg_borders = {-1, -1};
     int cur_l = -1;
@@ -20,8 +23,11 @@ T subsegment_with_max_sum(I first, I last) {
 
 //Finds shortest subsegment with min sum
 //Sum of any subsegment must fit in T
+//If first == last - returns 0
+//If [first, last) doesn't contain positive numbers - returns min number
 template<typename I, typename T = long long>
 T subsegment_with_min_sum(I first, I last) {
+    if (first == last) return 0;
     T cur = 0, mns = 0;
     array<int, 2> seg_borders = {-1, -1};
     int cur_l = -1;
