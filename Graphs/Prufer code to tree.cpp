b1509ff80d53619fc9a0b1383c9081bd8b78b72b
wery0 <wery0123@yandex.ru>
2023-04-14 01:52:02 +0300

    Removed dependency on templat

--- Graphs/Prufer code to tree.cpp
+++ Graphs/Prufer code to tree.cpp
@@ -1,17 +1,16 @@
 //O(n)
 vector<pair<int, int>> prufer_code_to_tree(vector<int> pcode) {
-    int n = pcode.size() + 2;
+    const int n = pcode.size() + 2;
     vector<int> cnt(n);
     for (int v : pcode) ++cnt[v];
     vector<pair<int, int>> ans(n - 1);
-    int leaf = find(all(cnt), 0) - cnt.begin();
-    for (int q = 0, ptr = leaf + 1; q < n - 2; q++) {
-        int p = pcode[q];
-        ans[q] = {p, leaf};
-        if (--cnt[p] == 0 && p < ptr) {
-            leaf = p;
-        } else {
-            for (; cnt[ptr]; ) ++ptr;
+    int leaf = find(cnt.begin(), cnt.end(), 0) - cnt.begin();
+    for (int i = 0, ptr = leaf + 1; i < n - 2; ++i) {
+        int p = pcode[i];
+        ans[i] = {p, leaf};
+        if (--cnt[p] == 0 && p < ptr) leaf = p;
+        else {
+            while (cnt[ptr]) ++ptr;
             leaf = ptr++;
         }
     }
