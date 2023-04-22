0585296a9757cc2f3442bf463d614123d53b90fa
wery0 <wery0123@yandex.ru>
2023-04-22 23:03:29 +0300

    Added new combination

--- Language/Pragma's.cpp
+++ Language/Pragma's.cpp
@@ -1,3 +1,7 @@
 #pragma GCC optimize("Ofast")
 #pragma GCC target("avx,avx2,fma")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,tune=native")
+
+//For bit manipulations
+#pragma GCC optimize("O3,unroll-loops")
+#pragma GCC target("avx2,bmi,bmi2,popcnt,lzcnt")
