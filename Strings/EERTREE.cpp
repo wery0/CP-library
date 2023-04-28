33ed82b7e685b80f41e90dbe07e968ad46295d01
wery0 <wery0123@yandex.ru>
2023-04-28 06:28:41 +0300

    Struct -> class, added comment, removed dependency on template

--- Strings/EERTREE.cpp
+++ Strings/EERTREE.cpp
@@ -1,21 +1,22 @@
-template<const int ALPHABET = 26>
-struct EERTREE {
-
+//All adding characters must be in range [Lchar, Lchar + ALPHABET)
+template<const char Lchar = 'a', const int ALPHABET = 26>
+class EERTREE {
+    
     struct Node {
         int len = 0;
-        Node *sl = 0;
-        Node *m[ALPHABET] = {0};
+        Node* sl = 0;
+        Node* m[ALPHABET] = {0};
 
         Node() = default;
         Node(int d): len(d) {}
     };
 
-    str t;
-    Node *r0 = new Node(0);
-    Node *r1 = new Node(-1);
-    Node *lst = r0;
+    string t;
+    Node* r0 = new Node(0);
+    Node* r1 = new Node(-1);
+    Node* lst = r0;
 
-    Node* go(Node *n) {
+    Node* go(Node* n) {
         for (; n != r1; n = n->sl) {
             int cur = t.size() - 1;
             int pr = cur - 1 - n->len;
@@ -24,20 +25,38 @@ struct EERTREE {
         return n;
     }
 
-    EERTREE() {r0->sl = r1->sl = r1;}
+    void destroy(Node* n) {
+        if (!n) return;
+        for (auto c : n->m) destroy(c);
+        delete n;
+    }
 
+public:
     int vertex_count = 0;
 
+    EERTREE() {r0->sl = r1->sl = r1;}
+
     void add_char(char c) {
-        assert('a' <= c && c < 'a' + ALPHABET);
-        c -= 'a';
+        assert(Lchar <= c && c < Lchar + ALPHABET);
         t += c;
+        c -= 'a';
         lst = go(lst);
         if (!lst->m[c]) {
-            vertex_count++;
+            ++vertex_count;
             lst->m[c] = new Node(lst->len + 2);
             lst->m[c]->sl = lst == r1 ? r0 : go(lst->sl)->m[c];
         }
         lst = lst->m[c];
     }
+
+    void clear() {
+        destroy(r0), destroy(r1);
+        r0 = new Node(0), r1 = new Node(-1);
+        r0->sl = r1->sl = r1;
+        lst = 0;
+        vertex_count = 0;
+        t.clear();
+    }
+
+    string get_string() const {return t;}
 };
