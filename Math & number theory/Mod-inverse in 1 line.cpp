8259ca9e449d860643f205d8021bfb463f859975
wery0 <wery0123@yandex.ru>
2023-05-16 22:58:25 +0300

    Function made template

--- Math & number theory/Mod-inverse in 1 line.cpp
+++ Math & number theory/Mod-inverse in 1 line.cpp
@@ -1,6 +1,7 @@
-//To prevent overflow, x^2 and m^2 should fit in long long.
+//To prevent overflow, x^2 and m^2 should fit in T.
 //If inverse value exists (<=> gcd(x, m) == 1), this function will always return it.
 //If doesn't - runtime error (division by zero)
-ll inv(ll x, ll m) {
+template<typename T>
+T inv(T x, T m) {
     return x == 1 ? x : (1 - inv(m % x, x) * m) / x + m;
 }
