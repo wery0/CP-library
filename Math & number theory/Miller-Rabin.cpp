aa068036e2754219d2182ffb0db83e8afe3390e2
wery0 <wery0123@yandex.ru>
2023-05-23 17:06:28 +0300

    Removed dependency on template

--- Math & number theory/Miller-Rabin.cpp
+++ Math & number theory/Miller-Rabin.cpp
@@ -1,6 +1,6 @@
 namespace MillerRabin {
-    inline ull binpow(ull x, ull k, const ull mod) {
-        ull o = 1;
+    uint64_t binpow(uint64_t x, uint64_t k, const uint64_t mod) {
+        uint64_t o = 1;
         for (; k; k >>= 1) {
             if (k & 1) o = __uint128_t(o) * x % mod;
             x = __uint128_t(x) * x % mod;
@@ -8,16 +8,16 @@ namespace MillerRabin {
         return o;
     }
 
-    bool is_prime(ull p) {
+    bool is_prime(uint64_t p) {
         if (p < 2) return 0;
-        static ull A[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}; //enough for all <= 2^64 nums
-        ull k = __builtin_ctzll(p - 1), d = (p - 1) >> k;
-        for (ull a : A) {
+        static uint64_t A[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37}; //enough for all <= 2^64 nums
+        uint64_t k = __builtin_ctzll(p - 1), d = (p - 1) >> k;
+        for (uint64_t a : A) {
             if (a >= p) break;
-            ull c = binpow(a, d, p);
+            uint64_t c = binpow(a, d, p);
             if (c == 1) continue;
             int fl = 0;
-            for (int q = 0; q < k && !fl; q++) {
+            for (uint64_t q = 0; q < k && !fl; ++q) {
                 fl |= c == p - 1;
                 c = __uint128_t(c) * c % p;
             }
