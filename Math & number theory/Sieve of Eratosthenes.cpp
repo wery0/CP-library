12da181434ea676e16f137b1427ff946e8c24311
wery0 <wery0123@yandex.ru>
2023-05-26 03:25:03 +0300

    Renamed, improved namings

--- Math & number theory/Sieve of Eratosthenes.cpp
+++ Math & number theory/Sieve of Eratosthenes.cpp
@@ -1,15 +1,15 @@
-//Fast Sieve of Eratosthenes with const optimizations.
+//Sieve of Eratosthenes with optimizations.
 //Returns vector of all primes in range [1, n].
 vector<int> gen_primes(int n) {
     if (n <= 2) {if (n == 2) return {2}; return {};}
-    vector<int> o = {2, 3}; o.reserve(n / log(n));
+    vector<int> ans = {2, 3}; ans.reserve(n / log(n));
     vector<bool> is_prime(n + 1, true);
     is_prime[0] = is_prime[1] = 0;
     for (int w = 4; w <= n; w += 2) is_prime[w] = 0;
     for (int w = 3; w <= n; w += 6) is_prime[w] = 0;
     const int gr = sqrtl(n) + 1;
     for (int w = 6; w <= gr; w += 6) {
-        for (int df : { -1, 1}) {
+        for (int df : {-1, 1}) {
             const int q = w + df;
             if (q > n) break;
             if (!is_prime[q]) continue;
@@ -19,10 +19,10 @@ vector<int> gen_primes(int n) {
         }
     }
     for (int w = 6; w - 1 <= n; w += 6) {
-        for (int df : { -1, 1}) {
+        for (int df : {-1, 1}) {
             const int q = w + df;
-            if (q <= n && is_prime[q]) o.push_back(q);
+            if (q <= n && is_prime[q]) ans.push_back(q);
         }
     }
-    return o;
+    return ans;
 }
