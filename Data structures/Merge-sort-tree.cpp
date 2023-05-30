f3673431f3656c6fc2f519e2a2c3467cca6f5b42
wery0 <wery0123@yandex.ru>
2023-05-30 06:03:21 +0300

    Removed dependency on template

--- Data structures/Merge-sort-tree.cpp
+++ Data structures/Merge-sort-tree.cpp
@@ -1,22 +1,23 @@
 template<typename T>
 class merge_sort_tree {
-    int U, G;
+    size_t n, U;
     vector<vector<T>> store;
 
-    inline int work(vector<T>& arr, T val_l, T val_r) {
-        auto it_r = upper_bound(arr.begin(), arr.end(), val_r);
+    int work(vector<T>& arr, T val_l, T val_r) {
         auto it_l = lower_bound(arr.begin(), arr.end(), val_l);
+        auto it_r = upper_bound(arr.begin(), arr.end(), val_r);
         return it_r - it_l;
     }
 
 public:
     template<typename Iterator>
-    merge_sort_tree(Iterator first, Iterator last): U(geq_pow2(last - first)), G(U * 2), store(G) {
+    merge_sort_tree(Iterator first, Iterator last): n(last - first), U(n & (n - 1) ? 2 << __lg(n) : n), store(U * 2) {
+        if (n == 0) return;
         for (auto it = store.begin() + U; first != last; ++first, ++it) {
             *it = {*first};
         }
-        for (int q = U; --q; ) {
-            merge(store[q << 1].begin(), store[q << 1].end(), store[q << 1 | 1].begin(), store[q << 1 | 1].end(), back_inserter(store[q]));
+        for (size_t i = U; --i; ) {
+            merge(store[i << 1].begin(), store[i << 1].end(), store[i << 1 | 1].begin(), store[i << 1 | 1].end(), back_inserter(store[i]));
         }
     }
 
@@ -27,7 +28,7 @@ public:
 
     //Counts # values in range [val_l, val_r] on segment [l, r]
     //O(log(n)^2)
-    int query(int l, int r, T val_l, T val_r) {
+    int query(size_t l, size_t r, T val_l, T val_r) {
         l += U, r += U;
         int ans = 0;
         while (l <= r) {
