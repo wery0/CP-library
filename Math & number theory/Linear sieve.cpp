b2661f7a51b91531f316d06e04b65c82aa296b4d
wery0 <wery0123@yandex.ru>
2023-05-30 05:34:51 +0300

    Fixed bug in min_divisors() function, pair -> array

--- Math & number theory/Linear sieve.cpp
+++ Math & number theory/Linear sieve.cpp
@@ -2,14 +2,14 @@
 //O(n)
 vector<int> min_divisors(int n) {
     vector<int> min_div(n + 1), primes;
-    for (int q = 2; q <= n; ++q) {
-        if (min_div[q] == 0) {
-            primes.push_back(q);
-            min_div[q] = q;
+    for (int i = 2; i <= n; ++i) {
+        if (min_div[i] == 0) {
+            primes.push_back(i);
+            min_div[i] = i;
         }
         for (int p : primes) {
-            int c = p * q;
-            if (p > min_div[q] || c >= n) break;
+            int c = p * i;
+            if (p > min_div[i] || c > n) break;
             min_div[c] = p;
         }
     }
@@ -18,15 +18,15 @@ vector<int> min_divisors(int n) {
 
 //Example: n = 60 -> {{2, 2}, {3, 1}, {5, 1}}
 //O(# of multipliers in the factorization of n) <= O(log_2(n))
-vector<pair<int, int>> get_factorization(vector<int>& min_div, int n) {
+vector<array<int, 2>> get_factorization(vector<int>& min_div, int n) {
     assert(1 <= n && n < min_div.size());
-    vector<pair<int, int>> ans;
-    for (; n != 1;) {
+    vector<array<int, 2>> ans;
+    while (n != 1) {
         int mnd = min_div[n], cnt = 0;
         for (; min_div[n] == mnd; n /= mnd) {
             ++cnt;
         }
-        ans.emplace_back(mnd, cnt);
+        ans.push_back({mnd, cnt});
     }
     return ans;
 }
