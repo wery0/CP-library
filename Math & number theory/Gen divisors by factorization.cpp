ed199d9a92c26ad4668bc05fc034686acf3c4af1
wery0 <wery0123@yandex.ru>
2023-06-08 09:29:43 +0300

    Removed dependency on template

--- Math & number theory/Gen divisors by factorization.cpp
+++ Math & number theory/Gen divisors by factorization.cpp
@@ -4,7 +4,7 @@ template<typename T>
 vector<T> get_divisors_by_factorization(vector<T> factorization) {
     sort(factorization.begin(), factorization.end());
     vector<T> dvz, cnt;
-    for (int i = 0; i < factorization.size(); ++i) {
+    for (size_t i = 0; i < factorization.size(); ++i) {
         if (i == 0 || factorization[i] != dvz.back()) {
             dvz.push_back(factorization[i]);
             cnt.push_back(1);
@@ -13,9 +13,9 @@ vector<T> get_divisors_by_factorization(vector<T> factorization) {
     T nd = 1;
     for (T c : cnt) nd *= c + 1;
     vector<T> ans(nd), kek(dvz.size());
-    for (ll ii = 0, c = 1; ii < nd; ++ii) {
+    for (T ii = 0, c = 1; ii < nd; ++ii) {
         ans[ii] = c;
-        for (ll i = 0; i < dvz.size(); ++i) {
+        for (size_t i = 0; i < dvz.size(); ++i) {
             if (++kek[i] <= cnt[i]) {
                 c *= dvz[i];
                 break;
