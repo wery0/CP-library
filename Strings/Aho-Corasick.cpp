b3ea0bd22e923867d3a6a8d07c3ffe29ee9c39ea
wery0 <wery0123@yandex.ru>
2023-07-16 06:19:11 +0300

    struct -> class, improved codestyle

--- Strings/Aho-Corasick.cpp
+++ Strings/Aho-Corasick.cpp
@@ -1,10 +1,8 @@
-struct aho_corasick {
+class aho_corasick {
 
     //Change, if need
     static constexpr int ALPHABET = 26;
     static constexpr int FIRST_CHAR = 'a';
-
-    aho_corasick() = default;
 
     struct Node {
         Node* parent = 0;
@@ -17,14 +15,14 @@ struct aho_corasick {
         vector<int> who_ends;
         int dep = 0;
 
-        Node(Node *parent, int chr): parent(parent), parent_edge_char(chr) {
+        Node(Node* parent, int chr): parent(parent), parent_edge_char(chr) {
             dep = (parent ? parent->dep : -1) + 1;
         }
     };
 
     Node* root = new Node(0, -1);
     int added_strings = 0;
-    bool was_suf_links_built = false;
+    bool is_prepared = false;
 
     Node* go(Node* n, int chr) {
         if (n->m.count(chr)) return n->m[chr];
@@ -32,10 +30,10 @@ struct aho_corasick {
     }
 
     void build_suf_links() {
-        assert(!was_suf_links_built);
+        assert(!is_prepared);
         deque<Node*> dq = {root};
-        for (; dq.size();) {
-            Node *n = dq.front();
+        while (dq.size()) {
+            Node* n = dq.front();
             dq.pop_front();
             if (!n->parent) {
                 n->suf_link = n->compressed_suf_link = n;
@@ -45,23 +43,30 @@ struct aho_corasick {
                 n->suf_link = go(n->parent->suf_link, n->parent_edge_char);
                 n->compressed_suf_link = n->suf_link->is_terminal ? n->suf_link : n->suf_link->compressed_suf_link;
             }
-            for (auto &[chr, u] : n->m) {
+            for (auto& [chr, u] : n->m) {
                 dq.push_back(u);
             }
         }
-        was_suf_links_built = true;
+        is_prepared = true;
     }
 
-    void add_string(string &t) {
-        assert(!was_suf_links_built);
+public:
+    aho_corasick() = default;
+
+    void add_string(const string& t) {
+        assert(!is_prepared);
         Node* n = root;
-        for (int q = 0; q < t.size(); ++q) {
-            int chr = t[q] - FIRST_CHAR;
+        for (int i = 0; i < t.size(); ++i) {
+            int chr = t[i] - FIRST_CHAR;
             if (!n->m.count(chr)) n->m[chr] = new Node(n, chr);
             n = n->m[chr];
         }
         n->is_terminal = 1;
         n->who_ends.push_back(added_strings++);
+    }
+
+    void prepare() {
+        build_suf_links();
     }
 
     //For every added string returns a vector with all occurences in s.
@@ -72,15 +77,15 @@ struct aho_corasick {
         "aba" -> {0, 4}
     */
     //O(#occurences)
-    vector<vector<int>> get_all_occurences(string &s) {
-        assert(was_suf_links_built);
-        Node *n = root;
+    vector<vector<int>> get_all_occurences(const string& s) {
+        assert(is_prepared);
+        Node* n = root;
         vector<vector<int>> ans(added_strings);
-        for (int q = 0; q < s.size(); q++) {
-            n = go(n, s[q] - FIRST_CHAR);
-            for (Node *e = n; e->parent; e = e->compressed_suf_link) {
-                for (int i : e->who_ends) {
-                    ans[i].push_back(q + 1 - e->dep);
+        for (int i = 0; i < s.size(); ++i) {
+            n = go(n, s[i] - FIRST_CHAR);
+            for (Node* e = n; e->parent; e = e->compressed_suf_link) {
+                for (int j : e->who_ends) {
+                    ans[j].push_back(i + 1 - e->dep);
                 }
             }
         }
