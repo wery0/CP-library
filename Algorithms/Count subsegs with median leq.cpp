1b12a3b206c5d6f363db26ae084eaad64fe74068
Bogdan Iandulov <wery0@yandex-team.ru>
2023-07-20 18:30:01 +0300

    Added clarification in comments, removed dependency on template

--- Algorithms/Count subsegs with median leq.cpp
+++ Algorithms/Count subsegs with median leq.cpp
@@ -1,21 +1,22 @@
+//If the length of sequence is even, then its median is the left of the two elements in the middle
 //O(n)
 template<typename T_arr, typename T>
-ll calc_subsegs_with_median_leq(const T_arr& arr, T k) {
-    int n = arr.size();
+int64_t calc_subsegs_with_median_leq(const T_arr& arr, T k) {
+    const size_t n = arr.size();
     vector<int> m(n), cnt(n * 2 + 1);
-    for (int q = 0; q < n; ++q) {
-        m[q] = n + (arr[q] <= k ? -1 : 1);
-        m[q] += q ? m[q - 1] - n : 0;
-        ++cnt[m[q]];
+    for (size_t i = 0; i < n; ++i) {
+        m[i] = n + (arr[i] <= k ? -1 : 1);
+        m[i] += i ? m[i - 1] - n : 0;
+        ++cnt[m[i]];
     }
-    ll ans = 0;
-    int c = n, leqc = accumulate(cnt.begin(), cnt.begin() + n + 1, 0);
-    for (int q = 0; q < m.size(); ++q) {
+    int64_t ans = 0;
+    int64_t c = n, leqc = accumulate(cnt.begin(), cnt.begin() + n + 1, 0);
+    for (const auto x : m) {
         ans += leqc;
-        --cnt[m[q]];
-        if (m[q] <= c) --leqc;
-        leqc += m[q] == c + 1 ? cnt[c + 1] : -cnt[c];
-        c = m[q];
+        --cnt[x];
+        if (x <= c) --leqc;
+        leqc += x == c + 1 ? cnt[c + 1] : -cnt[c];
+        c = x;
     }
     return ans;
 }
