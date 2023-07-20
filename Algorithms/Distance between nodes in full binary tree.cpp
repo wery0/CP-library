4b0acb755576ba451f4db70e62699dbe4507c7ef
Bogdan Iandulov <wery0@yandex-team.ru>
2023-07-20 11:37:56 +0300

    Functions made template

--- Algorithms/Distance between nodes in full binary tree.cpp
+++ Algorithms/Distance between nodes in full binary tree.cpp
@@ -1,8 +1,9 @@
 //Root of tree has number 1.
 //O(1)
-ll distance_between_nodes_in_full_binary_tree(ll x, ll y) {
+template<typename T>
+T distance_between_nodes_in_full_binary_tree(T x, T y) {
     if (x > y) swap(x, y);
-    ll ans = __lg(y) - __lg(x);
+    T ans = __lg(y) - __lg(x);
     y >>= ans;
     return ans + (x == y ? 0 : __lg(x ^ y) + 1) * 2;
 }
--- Other/Full k-ary tree navigation.cpp
+++ Other/Full k-ary tree navigation.cpp
@@ -1,4 +1,4 @@
 //If root of tree is 1
-ll leftest_child_of_node_in_full_k_ary_tree(ll k, ll n) {return k * (n - 1) + 2;}
-ll rightest_child_of_node_in_full_k_ary_tree(ll k, ll n) {return k * n + 1;}
-ll parent_of_node_in_full_k_ary_tree(ll k, ll n) {return (n + k - 2) / k;}
+template<typename T> T leftest_child_of_node_in_full_k_ary_tree(T k, T n) {return k * (n - 1) + 2;}
+template<typename T> T rightest_child_of_node_in_full_k_ary_tree(T k, T n) {return k * n + 1;}
+template<typename T> T parent_of_node_in_full_k_ary_tree(T k, T n) {return (n + k - 2) / k;}
