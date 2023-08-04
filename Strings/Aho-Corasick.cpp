87134fd15134487cab02aaa9b486c70e190917cb
Bogdan Iandulov <wery0@yandex-team.ru>
2023-08-04 11:11:23 +0300

    Added support of different containers for childrens in node

--- Strings/Aho-Corasick.cpp
+++ Strings/Aho-Corasick.cpp
@@ -1,23 +1,33 @@
-template<const bool are_strings_different>
+enum STORAGE {ARRAY, MAP, UNORDERED_MAP};
+template<const bool are_strings_distinct, STORAGE storage = ARRAY>
 class aho_corasick {
-	using K = conditional_t<are_strings_different, int, vector<int>>;
-	
+
 	//Change, if need
 	static constexpr int ALPHABET = 26;
 	static constexpr int FIRST_CHAR = 'a';
+
+	struct Node;
+	using K = conditional_t<are_strings_distinct, int, vector<int>>;
+	using M = conditional_t<storage == ARRAY, Node*[ALPHABET], conditional_t<storage == MAP, map<int, Node*>, unordered_map<int, Node*>>>;
 
 	struct Node {
 		Node* parent = 0;
 		Node* suf_link = 0;
 		Node* compressed_suf_link = 0;
 		int parent_edge_char = -1;
-		map<int, Node*> m;
+		M m = {};
+
+		Node() {
+			if (storage == ARRAY) {
+				memset(m, 0, sizeof(Node*) * ALPHABET);
+			}
+		}
 
 		K who_ends;
 		int dep = 0;
 
 		Node(Node* parent, int chr): parent(parent), parent_edge_char(chr) {
-			if constexpr(are_strings_different) who_ends = -1;
+			if constexpr(are_strings_distinct) who_ends = -1;
 			dep = (parent ? parent->dep : -1) + 1;
 		}
 	};
@@ -28,14 +38,19 @@ class aho_corasick {
 	int cnt_different_strings = 0;
 	bool is_prepared = false;
 
-	Node* go(Node* n, int chr) {
-		if (n->m.count(chr)) return n->m[chr];
-		return n->m[chr] = n->parent ? go(n->suf_link, chr) : n;
+	inline bool has(Node* n, int chr) {
+		if constexpr(storage == ARRAY) return n->m[chr];
+		else return n->m.count(chr);
 	}
 
 	inline bool is_terminal(Node* n) const {
-		if constexpr(are_strings_different) return n->who_ends != -1;
+		if constexpr(are_strings_distinct) return n->who_ends != -1;
 		else return !n->who_ends.empty();
+	}
+
+	Node* go(Node* n, int chr) {
+		if (has(n, chr)) return n->m[chr];
+		return n->m[chr] = n->parent ? go(n->suf_link, chr) : n;
 	}
 
 	void build_suf_links() {
@@ -52,8 +67,14 @@ class aho_corasick {
 				n->suf_link = go(n->parent->suf_link, n->parent_edge_char);
 				n->compressed_suf_link = is_terminal(n->suf_link) ? n->suf_link : n->suf_link->compressed_suf_link;
 			}
-			for (auto& [chr, u] : n->m) {
-				dq.push_back(u);
+			if constexpr(storage == ARRAY) {
+				for (size_t i = 0; i < ALPHABET; ++i) {
+					if (n->m[i]) dq.push_back(n->m[i]);
+				}
+			} else {
+				for (auto& [chr, u] : n->m) {
+					dq.push_back(u);
+				}
 			}
 		}
 		is_prepared = true;
@@ -65,14 +86,15 @@ public:
 	void add_string(const string& s) {
 		assert(!is_prepared);
 		Node* n = root;
-		for (int i = 0; i < s.size(); ++i) {
-			int chr = s[i] - FIRST_CHAR;
-			if (!n->m.count(chr)) n->m[chr] = new Node(n, chr), ++cnt_nodes;
+		for (int chr : s) {
+			chr -= FIRST_CHAR;
+			assert(0 <= chr < ALPHABET);
+			if (!has(n, chr)) n->m[chr] = new Node(n, chr), ++cnt_nodes;
 			n = n->m[chr];
 		}
 		cnt_different_strings += !is_terminal(n);
-		if constexpr(are_strings_different) {
-			assert(n->who_ends == -1);
+		if constexpr(are_strings_distinct) {
+			assert(n->who_ends == -1 && "Strings should be different!");
 			n->who_ends = cnt_added_strings++;
 		} else {
 			n->who_ends.push_back(cnt_added_strings++);       	
@@ -90,7 +112,7 @@ public:
 		"a" -> {0, 2, 4, 6}
 		"aba" -> {0, 4}
 	*/
-	//O(#occurences)
+	//O(|s| + #occurences)
 	vector<vector<int>> get_all_occurences(const string& s) {
 		assert(is_prepared);
 		Node* n = root;
@@ -98,7 +120,7 @@ public:
 		for (int i = 0; i < s.size(); ++i) {
 			n = go(n, s[i] - FIRST_CHAR);
 			for (Node* e = n; e->parent; e = e->compressed_suf_link) {
-				if constexpr(are_strings_different) {
+				if constexpr(are_strings_distinct) {
 					if (e->who_ends != -1) ans[e->who_ends].push_back(i + 1 - e->dep);
 				} else {
 					for (int j : e->who_ends) {
