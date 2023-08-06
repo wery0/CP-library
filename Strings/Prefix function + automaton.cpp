3cd855584066530dccc8f0cacb6aaf7bd998e17b
wery0 <wery0123@yandex.ru>
2023-08-07 01:33:58 +0300

    Added complexity, improved code

--- Strings/Prefix function + automaton.cpp
+++ Strings/Prefix function + automaton.cpp
@@ -1,32 +1,34 @@
+//O(n)
 template<typename I>
 vector<int> pref_func(I first, I last) {
-    int a = last - first;
-    vector<int> m(a);
-    for (int q = 1; q < a; ++q) {
-        for (int w = m[q - 1]; w && !m[q]; w = m[w - 1]) {
-            if (*(first + q) == *(first + w)) m[q] = w + 1;
+    const size_t n = last - first;
+    vector<int> m(n);
+    for (size_t i = 1; i < n; ++i) {
+        for (size_t j = m[i - 1]; j && !m[i]; j = m[j - 1]) {
+            if (*(first + i) == *(first + j)) m[i] = j + 1;
         }
-        m[q] += !m[q] && *(first + q) == *(first);
+        m[i] += !m[i] && *(first + i) == *(first);
     }
     return m;
 }
 
+//O(n * ALPHABET)
 template<typename I>
 vector<vector<int>> pref_func_automaton(I first, I last) {
     //Change, if need
-    const char lchar = 'a', rchar = 'z';
+    const size_t ALPHABET = 26;
+    const char FIRST_CHAR = 'a';
 
-    const int C = rchar - lchar + 1;
-    int a = last - first;
+    const size_t n = last - first;
     vector<int> pf = pref_func(first, last);
-    vector<vector<int>> m(a + 1, vector<int>(C));
-    m[0][(*first) - lchar] = 1;
-    for (int q = 1; q <= a; q++) {
-        for (int w = 0; w < C; w++) {
-            char c = lchar + w;
-            m[q][w] = *(first + q) == c ? q + 1 : m[pf[q - 1]][w];
+    vector<vector<int>> m(n + 1, vector<int>(ALPHABET));
+    m[0][(*first) - FIRST_CHAR] = 1;
+    for (size_t i = 1; i <= n; ++i) {
+        for (size_t j = 0; j < ALPHABET; ++j) {
+            char c = FIRST_CHAR + j;
+            m[i][j] = *(first + i) == c ? i + 1 : m[pf[i - 1]][j];
         }
     }
-    for (int w = 0; w < C; w++) m[a][w] = m[pf[a - 1]][w];
+    for (size_t j = 0; j < ALPHABET; ++j) m[n][j] = m[pf[n - 1]][j];
     return m;
 }
