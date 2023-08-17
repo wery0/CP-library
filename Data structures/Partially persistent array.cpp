c58f24e110528315b9ca0b334b32d5221f4577d0
wery0 <wery0123@yandex.ru>
2023-08-17 17:55:18 +0300

    Removed dependency on tempate

--- Data structures/Partially persistent array.cpp
+++ Data structures/Partially persistent array.cpp
@@ -16,7 +16,7 @@ public:
     partially_persistent_array(I first, I last, typename enable_if<!is_integral_v<I>>::type* = 0) {
         n = last - first;
         store.resize(n, {{0, T{}}});
-        for (int i = 0; first != last; ++first, ++i) {
+        for (size_t i = 0; first != last; ++first, ++i) {
             store[i][0].second = *first;
         }
     }
@@ -27,25 +27,25 @@ public:
 
     //<O(1), O(1)>
     //Creates new version
-    void set_value(int pos, T val) {
-        assert(0 <= pos && pos < n);
+    void set_value(size_t pos, T val) {
+        assert(pos < n);
         store[pos].emplace_back(++cur_version, val);
     }
 
     //O(log(n))
-    T operator()(int version, int pos) const {
-        assert(0 <= version && version <= cur_version);
-        assert(0 <= pos && pos < n);
-        auto it = upper_bound(all(store[pos]), version, [&](int v, const pair<int, T>& r) {return v < r.first;});
+    T operator()(size_t version, size_t pos) const {
+        assert(version <= cur_version);
+        assert(pos < n);
+        auto it = upper_bound(store[pos].begin(), store[pos].end(), version, [&](int v, const pair<int, T>& r) {return v < r.first;});
         return (*prev(it)).second;
     }
 
     //O(nlog(n))
-    vector<T> get_version_as_vector(int version) const {
-        assert(0 <= version && version <= cur_version);
+    vector<T> get_version_as_vector(size_t version) const {
+        assert(version <= cur_version);
         vector<T> ans(n);
-        for (int q = 0; q < n; ++q) {
-            ans[q] = operator()(version, q);
+        for (size_t i = 0; i < n; ++i) {
+            ans[i] = operator()(version, i);
         }
         return ans;
     }
