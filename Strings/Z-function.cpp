77b033dcf6de6f214ef363ab824205db0778e23b
wery0 <wery0123@yandex.ru>
2023-08-17 18:13:16 +0300

    Removed dependency on template

--- Strings/Z-function.cpp
+++ Strings/Z-function.cpp
@@ -1,13 +1,16 @@
 template<typename I>
 vector<int> Z(I first, I last) {
-    int a = last - first;
-    vector<int> m(a);
-    for (int q = 1, l = 0, r = 0; q < a; ++q) {
-        m[q] = max(0, min(r - q, m[q - l]));
-        while (q + m[q] < a && *(first + m[q]) == *(first + m[q] + q)) {
-            ++m[q];
+    const size_t n = last - first;
+    vector<int> zf(n);
+    for (int i = 1, l = 0, r = 0; i < n; ++i) {
+        zf[i] = max(0, min(r - i, zf	[i - l]));
+        while (i + zf[i] < n && *(first + zf[i]) == *(first + zf[i] + i)) {
+            ++zf[i];
         }
-        l = chmax(r, q + m[q]) ? q : l;
+        if (i + zf[i] > r) {
+            r = i + zf[i];
+            l = i;
+        }
     }
-    return m;
+    return zf;
 }
