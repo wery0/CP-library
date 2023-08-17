eda1d6052200fc221852ac0c81ed5e4d3621912e
wery0 <wery0123@yandex.ru>
2023-08-17 05:03:49 +0300

    Removed dependency on template

--- Math & number theory/Romberg integration.cpp
+++ Math & number theory/Romberg integration.cpp
@@ -4,12 +4,12 @@ template<typename T = long double>
 T romberg(T l, T r) {
     T d = r - l;
     vector<T> t = {d * (f(l) + f(r)) / 2};
-    for (ll k = 1, i = 1; ; k *= 2, d /= 2, ++i) {
+    for (int64_t k = 1, i = 1; ; k *= 2, d /= 2, ++i) {
         T last = t.back(), x = l + d / 2, cur = 0;
-        for (int j = 0; j < k; j++) cur += f(x), x += d;
+        for (int64_t j = 0; j < k; ++j) cur += f(x), x += d;
         cur = (t[0] + d * cur) / 2;
         T k1 = (T)4.0 / 3.0, k2 = (T)1.0 / 3.0;
-        for (int j = 0; j < i; j++) {
+        for (int64_t j = 0; j < i; ++j) {
             T temp = k1 * cur - k2 * t[j];
             t[j] = cur; cur = temp;
             k2 /= 4 * k1 - k2;
