691de76776fd4faae312275dadea15e26cd7bd85
wery0 <wery0123@yandex.ru>
2023-08-17 18:06:24 +0300

    Removed dependency on template

--- Strings/All left shifts.cpp
+++ Strings/All left shifts.cpp
@@ -7,19 +7,22 @@
 //O(n)
 template<typename T_arr>
 vector<int> find_all_left_shifts(T_arr arr1, T_arr arr2) {
-    int n = arr1.size(); assert(arr2.size() == n);
+    const size_t n = arr1.size(); assert(arr2.size() == n);
     arr2.push_back(0);
     arr2.insert(arr2.end(), arr1.begin(), arr1.end());
     arr2.insert(arr2.end(), arr1.begin(), arr1.end());
     vector<int> zf(arr2.size()), ans;
-    for (int q = 1, l = 0, r = 0, len = arr2.size(); q < len; ++q) {
-        zf[q] = max(0, min(r - q, zf[q - l]));
-        while (q + zf[q] < len && zf[q] != n && zf[q] + q != n && arr2[zf[q]] == arr2[zf[q] + q]) {
-            ++zf[q];
+    for (int i = 1, l = 0, r = 0, len = arr2.size(); i < len; ++i) {
+        zf[i] = max(0, min(r - i, zf[i - l]));
+        while (i + zf[i] < len && zf[i] != n && zf[i] + i != n && arr2[zf[i]] == arr2[zf[i] + i]) {
+            ++zf[i];
         }
-        l = chmax(r, q + zf[q]) ? q : l;
+        if (i + zf[i] > r) {
+            r = i + zf[i];
+            l = i;
+        }
     }
-    for (int i = 0; i < n; ++i) {
+    for (size_t i = 0; i < n; ++i) {
         if (zf[n + 1 + i] == n) ans.push_back(i);
     }
     return ans;
