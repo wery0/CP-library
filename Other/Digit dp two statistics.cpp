8a7d6bf261fd2abaa4b6f66ac6758bacc1505767
wery0 <wery0123@yandex.ru>
2023-08-20 03:22:17 +0300

    Rewritten digit dp`s

---
+++ Other/Digit dp two statistics.cpp
@@ -0,0 +1,61 @@
+//Returns # good numbers in range [l, r], where l > 0
+mint count_good_numbers_in_range_two_statistics(string l, string r) {
+    //Returns # good numbers in range [1, num]
+    auto calc_pref = [](string num, bool lst) -> mint {
+        const int MAX_STATISTIC1 = ?;
+        const int MAX_STATISTIC2 = ?;
+        auto f1 = [](int statistic, int digit) {return ?;};
+        auto f2 = [](int statistic, int digit) {return ?;};
+        const size_t n = num.size();
+        const int gr = num.size() == 1 ? stoi(num) : 9;
+        for (auto& c : num) c -= '0';
+        //dp[i][s1][s2][k] = # good numbers of length exactly i, with first statistic s1, second statistic s2, and relation to num is k.
+        vector dp(n, vector(MAX_STATISTIC1, vector(MAX_STATISTIC2, array<mint, 3>{0, 0, 0})));
+        for (int digit = 1; digit <= gr; ++digit) {
+            const int _ = digit < num[0] ? 0 : digit == num[0] ? 1 : 2;
+            const int s1 = f1(?, digit);
+            const int s2 = f2(?, digit);
+            dp[0][s1][s2][_] += 1;
+        }
+        for (int i = 0; i + 1 < n; ++i) {
+            for (int s1 = 0; s1 < MAX_STATISTIC1; ++s1) {
+                for (int s2 = 0; s2 < MAX_STATISTIC2; ++s2) {
+                    for (int u = 0; u < 3; ++u) {
+                        if (dp[i][s1][s2][u] == 0) continue;
+                        for (int digit = 0; digit < 10; ++digit) {
+                            const int _ = digit < num[i + 1] ? 0 : digit == num[i + 1] ? 1 : 2;
+                            const int ns1 = f1(s1, digit);
+                            const int ns2 = f2(s2, digit);
+                            if (digit < 0 || digit > 9) continue;
+                            if (ns1 < 0 || ns1 >= MAX_STATISTIC1) continue;
+                            if (ns2 < 0 || ns2 >= MAX_STATISTIC2) continue;
+                            dp[i + 1][ns1][ns2][u == 1 ? _ : u] += dp[i][s1][s2][u];
+                        }
+                    }
+                }
+            }
+        }
+        mint o = 0;
+        for (int i = 0; i < n; ++i) {
+            const int G = i + 1 < n ? 3 : lst ? 2 : 1;
+            for (int s1 = 0; s1 < MAX_STATISTIC1; ++s1) {
+                for (int s2 = 0; s2 < MAX_STATISTIC2; ++s2) {
+                    for (int j = 0; j < G; ++j) {
+                        o += dp[i][s1][s2][j];
+                    }
+                }
+            }
+        }
+        return o;
+    };
+    auto simplify = [](string& s) {
+        assert(!s.empty());
+        for (auto& c : s) assert(isdigit(c));
+        s.erase(s.begin(), s.begin() + s.find_first_not_of('0'));
+        if (s.empty()) s = "0";
+    };
+    simplify(l);
+    simplify(r);
+    if (l.size() > r.size() || l.size() == r.size() && l > r) return 0;
+    return calc_pref(r, 1) - calc_pref(l, 0);
+}
