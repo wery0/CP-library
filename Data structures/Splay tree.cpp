81b0a614b58311ef843ff61df4a6c5099661b3b3
wery0 <wery0123@yandex.ru>
2023-08-27 20:35:14 +0300

    Code made template, removed dependency on template

--- Data structures/Splay tree.cpp
+++ Data structures/Splay tree.cpp
@@ -1,28 +1,25 @@
-struct splay_tree {
-
-    static const ll inf = 1e9 + 7;
-
+template<typename K>
+class splay_tree {
     struct Node {
-        Node *m[2] = {0}, *p = 0;
-        ll x;
-        ll mn = inf;
-        ll sz = 0;
-        ll rev = 0;
+        Node* m[2] = {0}, *p = 0;
+        K key;
+        K mnk;
+        size_t sz;
+        bool rev = 0;
 
         Node() = default;
 
-        Node(ll a) {
-            x = a;
-            sz = 1;
-            mn = a;
+        Node(K k): sz(1) {
+            key = k;
+            mnk = k;
         }
     };
     Node* root = 0;
 
-    ll gsz(Node *n) { return n ? n->sz : 0; }
-    ll gmn(Node *n) { return n ? n->mn : inf; }
+    size_t gsz(Node* n) const {return n ? n->sz : 0;}
+    K gmnk(Node* n) const {return n ? n->mnk : std::numeric_limits<K>::max();}
 
-    void push(Node *n) {
+    void push(Node* n) {
         if (!n) return;
         if (n->rev) {
             if (n->m[0]) n->m[0]->rev ^= 1;
@@ -32,36 +29,44 @@ struct splay_tree {
         }
     }
 
-    void super_push(Node *n) {
-        if (n->p) super_push(n->p);
-        push(n);
+    void super_push(Node* n) {
+        static vector<Node*> store;
+        while (n) {
+            store.push_back(n);
+            n = n->p;
+        }
+        while (store.size()) {
+            push(store.back());
+            store.pop_back();
+        }
     }
 
-    void upd(Node *n) {
+    void upd(Node* n) {
         if (!n) return;
-        n->mn = min({gmn(n->m[0]), n->x, gmn(n->m[1])});
+        n->mnk = min({gmnk(n->m[0]), n->key, gmnk(n->m[1])});
+
         n->sz = gsz(n->m[0]) + 1 + gsz(n->m[1]);
     }
 
-    int dir(Node *n) {return n->p->m[1] == n;}
+    int dir(Node* n) const {return n->p->m[1] == n;}
 
-    void disconnect(Node *n) {
+    void disconnect(Node* n) {
         if (!n || !n->p) return;
-        Node *p = n->p;
+        Node* p = n->p;
         p->m[dir(n)] = 0;
         n->p = 0;
         upd(p);
     }
 
-    void connect(Node *p, Node *n, int d) {
+    void connect(Node* p, Node* n, int d) {
         p->m[d] = n;
         if (n) n->p = p;
         upd(p);
     }
 
-    void rotate(Node *n) {
+    void rotate(Node* n) {
         if (!n->p) return;
-        Node *p = n->p, *g = n->p->p;
+        Node* p = n->p, *g = n->p->p;
         int d = dir(n);
         if (g) {
             connect(g, n, dir(p));
@@ -72,59 +77,87 @@ struct splay_tree {
         connect(n, p, d ^ 1);
     }
 
-    Node* splay(Node *n) {
+    Node* splay(Node* n) {
         if (!n) return 0;
         super_push(n);
-        for (; n->p;) {
+        while (n->p) {
             if (n->p->p) rotate(dir(n) == dir(n->p) ? n->p : n);
             rotate(n);
         }
         return n;
     }
 
-    Node* leftmost(Node *n) {
-        push(n);
-        return n->m[0] ? leftmost(n->m[0]) : n;
+    Node* leftmost(Node* n) {
+        for (push(n); n->m[0]; push(n)) n = n->m[0];
+        return n;
     }
 
-    Node* rightmost(Node *n) {
-        push(n);
-        return n->m[1] ? rightmost(n->m[1]) : n;
+    Node* rightmost(Node* n) {
+        for (push(n); n->m[1]; push(n)) n = n->m[1];
+        return n;
     }
 
-    Node* merge(Node *l, Node *r) {
+    Node* merge(Node* l, Node* r) {
         if (!l || !r) return l ? l : r;
         l = splay(rightmost(l));
         connect(l, r, 1);
         return l;
     }
 
-    Node* access(Node *n, int ps) {
-        push(n);
-        if (ps <= gsz(n->m[0])) return access(n->m[0], ps);
-        if (ps > gsz(n->m[0]) + 1) return access(n->m[1], ps - gsz(n->m[0]) - 1);
-        splay(n);
+    Node* access(Node* n, size_t pos) {
+        assert(0 <= pos && pos < gsz(n));
+        while (n) {
+            push(n);
+            const size_t szl = gsz(n->m[0]);
+            if (pos == szl) {
+                root = splay(n);
+                return root;
+            }
+            if (pos < szl) n = n->m[0];
+            else pos -= szl + 1, n = n->m[1];
+        }
+        assert(0);
+        return 0;
+    }
+
+    array<Node*, 2> split_size(Node* n, size_t k) {
+        if (!k) return {0, n};
+        assert(k <= gsz(n));
+        n = access(n, k - 1);
+        Node* r = n->m[1];
+        disconnect(r);
+        return {n, r};
+    }
+
+    template<typename I>
+    Node* build(I f_key, I l_key) {
+        if (f_key >= l_key) return 0;
+        I md = f_key + (l_key - f_key) / 2;
+        Node* n = new Node(*md);
+        n->m[0] = build(f_key, md);
+        n->m[1] = build(md + 1, l_key);
+        if (n->m[0]) n->m[0]->p = n;
+        if (n->m[1]) n->m[1]->p = n;
+        upd(n);
         return n;
     }
 
-    pnn splitSz(Node *n, int k) {
-        if (!k) return {0, n};
-        n = access(n, k);
-        Node *R = n->m[1];
-        disconnect(R);
-        return {n, R};
-    }
+public:
+    splay_tree() = default;
+    template<typename I> splay_tree(I f_key, I l_key) {root = build(f_key, l_key);}
 
-    ll seg_min(ll l, ll r) {
-        pnn p1 = splitSz(root, l - 1), p2 = splitSz(p1.S, r - l + 1);
-        ll res = gmn(p2.F);
-        root = merge(p1.F, merge(p2.F, p2.S));
+    K seg_min(size_t l, size_t r) {
+        auto [lf, tmp] = split_size(root, l);
+        auto [mid, rg] = split_size(tmp, r - l + 1);
+        K res = gmnk(mid);
+        root = merge(merge(lf, mid), rg);
         return res;
     }
 
-    void seg_rev(ll l, ll r) {
-        pnn p1 = splitSz(root, l - 1), p2 = splitSz(p1.S, r - l + 1);
-        p2.F->rev ^= 1;
-        root = merge(p1.F, merge(p2.F, p2.S));
+    void seg_rev(size_t l, size_t r) {
+        auto [lf, tmp] = split_size(root, l);
+        auto [mid, rg] = split_size(tmp, r - l + 1);
+        mid->rev ^= 1;
+        root = merge(merge(lf, mid), rg);
     }
 };
