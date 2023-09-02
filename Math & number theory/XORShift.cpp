6cd12d1ec8ce965b6c58b97e5a3996cee452a78f
wery0 <wery0123@yandex.ru>
2023-09-03 02:16:12 +0300

    Added functionality, changed style, removed dependency on template

--- Math & number theory/XORShift.cpp
+++ Math & number theory/XORShift.cpp
@@ -1,14 +1,22 @@
-struct XORShift {
-    ll seed;
-    XORShift(int seed) {this->seed = seed; }
-    XORShift() { this->seed = std::chrono::steady_clock::now().time_since_epoch().count(); }
-    ll genRandomNumber() {
+template<typename T = int64_t>
+class xor_shift {
+    T seed;
+
+public:
+    xor_shift(T seed = -1): seed(seed == -1 ? chrono::steady_clock::now().time_since_epoch().count() : seed) {}
+
+    T gen() {
         seed ^= seed << 21;
-        seed ^= seed >> 35u;
+        seed ^= seed >> 35;
         seed ^= seed << 4;
         return seed;
     }
-    ll genRandomPositiveNumber(ll maximal) {
-        return (abs(genRandomNumber()) + maximal) % maximal;
+
+    T gen_mod(T mod) {
+        return abs(gen() % mod);
+    }
+
+    T gen_in_range(T l, T r) {
+        return l + gen_mod(r - l + 1);
     }
 };
