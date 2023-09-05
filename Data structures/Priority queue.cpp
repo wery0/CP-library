a917c4ef0406e6ffd5c356ee23eccc439a6fc708
wery0 <wery0123@yandex.ru>
2023-09-06 00:58:36 +0300

    Improved codestyle & namings

--- Data structures/Priority queue.cpp
+++ Data structures/Priority queue.cpp
@@ -2,22 +2,22 @@
 //Performs the same as std::pq, but with reserve could be several percents faster
 template<typename T, typename Compare = std::less<T>>
 class my_priority_queue {
-    int sz = 0;
+    size_t N = 0;
     vector<T> store{T{}};
 
     void sift_up(size_t pos) {
-        for (; pos > 1 && Compare{}(store[pos >> 1], store[pos]);) {
+        while (pos > 1 && Compare{}(store[pos >> 1], store[pos])) {
             swap(store[pos], store[pos >> 1]);
             pos >>= 1;
         }
     }
 
     void sift_down(size_t pos) {
-        for (; pos * 2 <= sz;) {
+        while (pos * 2 <= N) {
             const size_t left = pos << 1;
             const size_t right = pos << 1 | 1;
             size_t swap_with = left;
-            if (right <= sz && Compare{}(store[left], store[right])) {
+            if (right <= N && Compare{}(store[left], store[right])) {
                 swap_with = right;
             }
             if (Compare{}(store[swap_with], store[pos])) {
@@ -32,32 +32,32 @@ public:
     my_priority_queue() = default;
     my_priority_queue(size_t n): store(n + 1) {}
 
-    size_t size() const {return sz;}
-    bool empty() const {return sz == 0;}
+    size_t size() const {return N;}
+    bool empty() const {return N == 0;}
 
-    void clear() {sz = 0;}
+    void clear() {N = 0;}
 
     void push(T x) {
-        if (++sz >= store.size()) store.resize(sz * 2);
-        store[sz] = x;
-        sift_up(sz);
+        if (++N >= store.size()) store.resize(N * 2);
+        store[N] = x;
+        sift_up(N);
     }
 
     T top() const {
-        assert(sz);
+        assert(N);
         return store[1];
     }
 
     void pop() {
-        assert(sz);
-        swap(store[1], store[sz--]);
+        assert(N);
+        swap(store[1], store[N--]);
         sift_down(1);
     }
 
     T extract_top() {
-        assert(sz);
+        assert(N);
         T res = store[1];
-        swap(store[1], store[sz--]);
+        swap(store[1], store[N--]);
         sift_down(1);
         return res;
     }
