a94bbe951444522f8dc52b827667a07f818173b0
wery0 <wery0123@yandex.ru>
2023-09-10 05:01:17 +0300

    2D vector replaced with 1D, resulting in huge speed and memory improvement

--- Data structures/Merge sort tree/Basic.cpp
+++ Data structures/Merge sort tree/Basic.cpp
@@ -3,34 +3,47 @@ class merge_sort_tree {
 
     static constexpr T inf = numeric_limits<T>::max();
 
-    size_t n, U;
-    vector<vector<T>> store;
+    size_t n, U, M;
+    vector<T> store;
+    vector<int> st;
 
-    int work_seg(vector<T>& arr, T val_l, T val_r) {
-        auto it_l = lower_bound(arr.begin(), arr.end(), val_l);
-        auto it_r = upper_bound(arr.begin(), arr.end(), val_r);
+    int work_seg(size_t i, T val_l, T val_r) {
+        auto it_l = lower_bound(store.begin() + st[i], store.begin() + st[i + 1], val_l);
+        auto it_r = upper_bound(store.begin() + st[i], store.begin() + st[i + 1], val_r);
         return it_r - it_l;
     }
 
-    int work_leq(vector<T>& arr, T val_r) {
-        auto it_r = upper_bound(arr.begin(), arr.end(), val_r);
-        return it_r - arr.begin();
+    int work_leq(size_t i, T val_r) {
+        auto it_r = upper_bound(store.begin() + st[i], store.begin() + st[i + 1], val_r);
+        return it_r - store.begin() - st[i];
     }
 
-    T work_max_leq(vector<T>& arr, T x, T NO_ANSWER) {
-        auto it_r = upper_bound(arr.begin(), arr.end(), x);
-        return it_r == arr.begin() ? NO_ANSWER : *--it_r;
+    T work_max_leq(size_t i, T x, T NO_ANSWER) {
+        auto it_r = upper_bound(store.begin() + st[i], store.begin() + st[i + 1], x);
+        return it_r == store.begin() + st[i] ? NO_ANSWER : *--it_r;
     }
 
 public:
     template<typename Iterator>
     merge_sort_tree(Iterator first, Iterator last): n(last - first), U(n & (n - 1) ? 2 << __lg(n) : n), store(U * 2) {
         if (n == 0) return;
-        for (auto it = store.begin() + U; first != last; ++first, ++it) {
-            *it = {*first};
+        M = (n ? __lg(n) + 1 : 0) * n + (n & (n - 1) ? n : 0);
+        store.resize(M);
+        st.resize(U * 2 + 1);
+        auto go = [&](auto&& go, size_t l, size_t r, size_t v, size_t dep = 0) -> void {
+            st[v] = n * dep + min(l, n);
+            if (l == r) return;
+            size_t md = (l + r) >> 1;
+            go(go, l, md, v << 1, dep + 1);
+            go(go, md + 1, r, v << 1 | 1, dep + 1);
+        };
+        go(go, 0, U - 1, 1);
+        st.back() = M;
+        for (size_t i = 0; i < n; ++i, ++first) {
+            store[st[U + i]] = *first;
         }
-        for (size_t i = U; --i; ) {
-            merge(store[i << 1].begin(), store[i << 1].end(), store[i << 1 | 1].begin(), store[i << 1 | 1].end(), back_inserter(store[i]));
+        for (size_t i = U; --i;) {
+            merge(store.begin() + st[i << 1], store.begin() + st[i << 1 | 1], store.begin() + st[i << 1 | 1], store.begin() + st[(i << 1) + 2], store.begin() + st[i]);
         }
     }
 
@@ -45,8 +58,8 @@ public:
         l += U, r += U;
         int ans = 0;
         while (l <= r) {
-            if (l & 1) ans += work_seg(store[l], val_l, val_r);
-            if (~r & 1) ans += work_seg(store[r], val_l, val_r);
+            if (l & 1) ans += work_seg(l, val_l, val_r);
+            if (~r & 1) ans += work_seg(r, val_l, val_r);
             l = (l + 1) >> 1;
             r = (r - 1) >> 1;
         }
@@ -59,8 +72,8 @@ public:
         l += U, r += U;
         int ans = 0;
         while (l <= r) {
-            if (l & 1) ans += work_leq(store[l], val_r);
-            if (~r & 1) ans += work_leq(store[r], val_r);
+            if (l & 1) ans += work_leq(l, val_r);
+            if (~r & 1) ans += work_leq(r, val_r);
             l = (l + 1) >> 1;
             r = (r - 1) >> 1;
         }
@@ -76,8 +89,8 @@ public:
         l += U, r += U;
         T ans = -inf;
         while (l <= r) {
-            if (l & 1) ans = max(ans, work_max_leq(store[l], x, -inf));
-            if (~r & 1) ans = max(ans, work_max_leq(store[r], x, -inf));
+            if (l & 1) ans = max(ans, work_max_leq(l, x, -inf));
+            if (~r & 1) ans = max(ans, work_max_leq(r, x, -inf));
             l = (l + 1) >> 1;
             r = (r - 1) >> 1;
         }
