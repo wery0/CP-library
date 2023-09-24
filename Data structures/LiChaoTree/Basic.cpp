a1d854c42dd3e2439da851880a7ced4ce672e181
wery0 <wery0123@yandex.ru>
2023-09-25 00:36:38 +0300

    Function parameters moved into a new structure

--- Data structures/LiChaoTree/Basic.cpp
+++ Data structures/LiChaoTree/Basic.cpp
@@ -3,9 +3,20 @@ class lichao {
 
     static constexpr Y inf = numeric_limits<Y>::max();
 
+    struct func {
+        Y k, b;
+
+        func() = default;
+        func(Y k, Y b): k(k), b(b) {}
+
+        Y eval(X x) const {
+            return k * x + b;
+        }
+    };
+
     struct Node {
         Node *l = 0, *r = 0;
-        Y k = 0, b = inf;
+        func f{0, inf};
 
         Node() = default;
     };
@@ -13,37 +24,37 @@ class lichao {
     X L, R;
     Node* root = new Node();
 
-    void add_seg(X ql, X qr, X l, X r, Y k, Y b, Node*& n) {
+    void add_seg(X ql, X qr, X l, X r, func nf, Node*& n) {
         if (qr < l || r < ql) return;
         if (!n) n = new Node();
         X md = l + (r - l) / 2;
         if (l < ql || r > qr) {
-            add_seg(ql, qr, l, md, k, b, n->l);
-            add_seg(ql, qr, md + 1, r, k, b, n->r);
+            add_seg(ql, qr, l, md, nf, n->l);
+            add_seg(ql, qr, md + 1, r, nf, n->r);
             return;
         }
-        add_line(l, r, k, b, n);
+        add_line(l, r, nf, n);
     }
 
-    void add_line(X l, X r, Y k, Y b, Node*& n) {
+    void add_line(X l, X r, func nf, Node*& n) {
         if (!n) n = new Node();
-        Y vl_cur = n->k * l + n->b;
-        Y vr_cur = n->k * r + n->b;
-        Y vl_new = k * l + b;
-        Y vr_new = k * r + b;
+        Y vl_cur = n->f.eval(l);
+        Y vr_cur = n->f.eval(r);
+        Y vl_new = nf.eval(l);
+        Y vr_new = nf.eval(r);
         if ((vl_cur <= vl_new) == (vr_cur <= vr_new)) {
-            if (vl_new < vl_cur) n->k = k, n->b = b;
+            if (vl_new < vl_cur) n->f = nf;
             return;
         }
         X md = l + (r - l) / 2;
-        Y vmd_cur = n->k * md + n->b;
-        Y vmd_new = k * md + b;
+        Y vmd_cur = n->f.eval(md);
+        Y vmd_new = nf.eval(md);
         if (vmd_new < vmd_cur) {
-            swap(n->k, k), swap(n->b, b);
+        	swap(n->f, nf);
             swap(vl_cur, vl_new);
         }
-        if (vl_new < vl_cur) add_line(l, md, k, b, n->l);
-        else add_line(md + 1, r, k, b, n->r);
+        if (vl_new < vl_cur) add_line(l, md, nf, n->l);
+        else add_line(md + 1, r, nf, n->r);
     }
 
     void destroy(Node* n) {
@@ -58,12 +69,13 @@ public:
     lichao(X L, X R): L(L), R(R) {}
     ~lichao() {destroy(root);}
 
+    //O(log(C))
     Y get_min(X x) {
         Node* n = root;
         X l = L, r = R;
         Y o = inf;
         while (n) {
-            o = min(o, n->k * x + n->b);
+            o = min(o, n->f.eval(x));
             X md = l + (r - l) / 2;
             (x <= md ? r : l) = md + (x > md);
             n = x <= md ? n->l : n->r;
@@ -72,10 +84,10 @@ public:
     }
 
     //O(log(C))
-    void add_line(Y k, Y b) {add_line(L, R, k, b, root);}
+    void add_line(Y k, Y b) {add_line(L, R, func(k, b), root);}
 
     //O(log(C) ^ 2)
-    void add_seg(X l, X r, Y k, Y b) {add_seg(l, r, L, R, k, b, root);}
+    void add_seg(X l, X r, Y k, Y b) {add_seg(l, r, L, R, func(k, b), root);}
 };
 //Usage: lichao<X, Y> kek(L, R), where
 //X - type of coordinates,
--- Data structures/LiChaoTree/On points.cpp
+++ Data structures/LiChaoTree/On points.cpp
@@ -3,37 +3,48 @@ class lichao_on_points {
 
     static constexpr Y inf = numeric_limits<Y>::max();
 
+    struct func {
+        Y k, b;
+
+        func() = default;
+        func(Y k, Y b): k(k), b(b) {}
+
+        Y eval(X x) const {
+            return k * x + b;
+        }
+    };
+
     size_t n, U;
     vector<X> bl, br;
-    vector<Y> vk, vb;
+    vector<func> vf;
 
-    void add_seg(X ql, X qr, Y k, Y b, size_t v) {
+    void add_seg(X ql, X qr, func nf, size_t v) {
         const X l = bl[v], r = br[v];
         if (qr < l || r < ql) return;
         if (ql <= l && r <= qr) {
-            add_line(k, b, v);
+            add_line(nf, v);
             return;
         }
-        add_seg(ql, qr, k, b, v << 1);
-        add_seg(ql, qr, k, b, v << 1 | 1);
+        add_seg(ql, qr, nf, v << 1);
+        add_seg(ql, qr, nf, v << 1 | 1);
     }
 
-    void add_line(Y k, Y b, size_t v) {
+    void add_line(func nf, size_t v) {
         while (true) {
             const X l = bl[v], r = br[v];
-            Y vl_cur = vk[v] * l + vb[v];
-            Y vr_cur = vk[v] * r + vb[v];
-            Y vl_new = k * l + b;
-            Y vr_new = k * r + b;
+            Y vl_cur = vf[v].eval(l);
+            Y vr_cur = vf[v].eval(r);
+            Y vl_new = nf.eval(l);
+            Y vr_new = nf.eval(r);
             if ((vl_cur <= vl_new) == (vr_cur <= vr_new)) {
-                if (vl_new < vl_cur) vk[v] = k, vb[v] = b;
+                if (vl_new < vl_cur) vf[v] = nf;
                 return;
             }
             X md = br[v << 1];
-            Y vmd_cur = vk[v] * md + vb[v];
-            Y vmd_new = k * md + b;
+            Y vmd_cur = vf[v].eval(md);
+            Y vmd_new = nf.eval(md);
             if (vmd_new < vmd_cur) {
-                swap(vk[v], k), swap(vb[v], b);
+            	swap(vf[v], nf);
                 swap(vl_cur, vl_new);
             }
             v = vl_new < vl_cur ? v << 1 : v << 1 | 1;
@@ -50,8 +61,7 @@ public:
         U = n & (n - 1) ? 2 << __lg(n) : n;
         bl.resize(U * 2);
         br.resize(U * 2);
-        vk.resize(U * 2);
-        vb.resize(U * 2, inf);
+        vf.resize(U * 2, func(0, inf));
         for (size_t i = 0; i < n; ++i) bl[U + i] = br[U + i] = points[i];
         for (size_t i = n; i < U; ++i) bl[U + i] = br[U + i] = points[n - 1];
         for (size_t i = U; --i; ) {
@@ -60,22 +70,23 @@ public:
         }
     }
 
+    //O(log(|points|))
     Y get_min(X x) {
         size_t v = 1;
-        Y o = vk[v] * x + vb[v];
+        Y o = vf[v].eval(x);
         while (v < U) {
             v <<= 1;
             v += x > br[v];
-            o = min(o, vk[v] * x + vb[v]);
+            o = min(o, vf[v].eval(x));
         }
         return o;
     }
 
     //O(log(|points|))
-    void add_line(Y k, Y b) {add_line(k, b, 1);}
+    void add_line(Y k, Y b) {add_line(func(k, b), 1);}
 
     //O(log(|points|) ^ 2)
-    void add_seg(X l, X r, Y k, Y b) {add_seg(l, r, k, b, 1);}
+    void add_seg(X l, X r, Y k, Y b) {add_seg(l, r, func(k, b), 1);}
 };
 //Usage: lichao_on_points<X, Y> kek(points), where
 //X - type of coordinates,
