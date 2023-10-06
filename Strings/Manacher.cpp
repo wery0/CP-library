b802f2e13a0bf5e8ff43596acb68991bce7f599b
wery0 <wery0123@yandex.ru>
2023-10-06 15:56:45 +0300

    Added description and complexity, improved namings

--- Strings/Manacher.cpp
+++ Strings/Manacher.cpp
@@ -1,21 +1,23 @@
-template<typename Arr, typename T = int64_t>
-T manacher(const Arr& t) {
-    size_t n = t.size();
+//Calculates # palindromic subarrays
+//O(n)
+template<typename T_arr, typename C = int64_t>
+C manacher(const T_arr& arr) {
+    const size_t n = arr.size();
     vector<int> odd(n);   //odd("aaaaa")  = {1, 2, 3, 2, 1}
     vector<int> even(n);  //even("aaaaa") = {0, 1, 2, 2, 1}
     for (int i = 0, l = 0, r = 0; i < n; ++i) {
         int& d = odd[i];
         d = i > r ? 1 : max(0, min(r - i, odd[r - i + l]));
-        while (i - d >= 0 && i + d < n && t[i - d] == t[i + d]) ++d;
+        while (i - d >= 0 && i + d < n && arr[i - d] == arr[i + d]) ++d;
         if (i + d - 1 > r) r = i + d - 1, l = i - d + 1;
     }
     for (int i = 1, l = 0, r = 0; i < n; ++i) {
         int& d = even[i];
         d = i > r ? 0 : max(0, min(r - i, even[r - i + l + 1]));
-        while (i - d - 1 >= 0 && i + d < n && t[i - d - 1] == t[i + d]) ++d;
+        while (i - d - 1 >= 0 && i + d < n && arr[i - d - 1] == arr[i + d]) ++d;
         if (i + d > r) r = i + d, l = i - d - 1;
     }
-    return accumulate(odd.begin(), odd.end(), (T)0) + accumulate(even.begin(), even.end(), (T)0);
+    return accumulate(odd.begin(), odd.end(), (C)0) + accumulate(even.begin(), even.end(), (C)0);
 }
 
 // bool is_palindrome(int l, int r) {
