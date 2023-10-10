0038873997800fc752489db9c7f71140720caf80
wery0 <wery0123@yandex.ru>
2023-10-10 19:33:09 +0300

    Added support of cyclic shifts

--- Strings/Suffix array & LCP.cpp
+++ Strings/Suffix array & LCP.cpp
@@ -1,8 +1,8 @@
 //O(nlog(n))
 template<typename T_arr>
-vector<int> calc_suffix_array(T_arr& s) {
+vector<int> calc_suffix_array(T_arr& s, bool is_cyclic) {
     using T = T_arr::value_type;
-    s.push_back(0);           //This symbol must not appear in the sequence and must be the smallest one
+    if (!is_cyclic) s.push_back(0);    //This symbol must not appear in the sequence and must be the smallest one
     const size_t n = s.size();
     vector<int> cnt(n), cl(n), pcl(n);
     vector<T> df(s.begin(), s.end());
@@ -18,9 +18,19 @@ vector<int> calc_suffix_array(T_arr& s) {
         memset(&cnt[0], 0, sizeof(cnt[0]) * n);
         for (int i : cl) ++cnt[i];
     }
-    s.pop_back();
-    vector<int> sa(n - 1);
-    for (size_t i = 0; i < n; ++i) if (cl[i]) sa[cl[i] - 1] = i;
+    if (is_cyclic) {
+        partial_sum(cnt.begin(), cnt.end(), cnt.begin());
+        for (ssize_t i = n - 1; i >= 0; --i) {
+            cl[i] = --cnt[cl[i]];
+        }
+    }
+    vector<int> sa(n - !is_cyclic);
+    if (is_cyclic) {
+        for (size_t i = 0; i < n; ++i) sa[cl[i]] = i;
+    } else {
+        s.pop_back();
+        for (size_t i = 0; i < n; ++i) if (cl[i]) sa[cl[i] - 1] = i;
+    }
     return sa;
 }
 
@@ -32,7 +42,7 @@ lcp = [1 2 0 2 1] => lcp(sa[i], sa[i + 1]) = lcp[i]
 //Kasai's arimura's arikawa's li's and pac's algorithm
 //O(n)
 template<typename T_arr>
-vector<int> calc_lcp(const T_arr& s, const vector<int>& sa) {
+vector<int> calc_lcp(const T_arr& s, const vector<int>& sa, bool is_cyclic) {
     const size_t n = sa.size();
     vector<int> pos_in_sa(n), lcp(n - 1);
     for (size_t i = 0; i < n; ++i) pos_in_sa[sa[i]] = i;
@@ -41,7 +51,8 @@ vector<int> calc_lcp(const T_arr& s, const vector<int>& sa) {
         if (!p) continue;
         size_t j = sa[p - 1];
         d -= !!d;
-        while (max(i, j) + d < n && s[i + d] == s[j + d]) ++d;
+        if (is_cyclic) while (d < n && s[(i + d) % n] == s[(j + d) % n]) ++d;
+        else while (max(i, j) + d < n && s[i + d] == s[j + d]) ++d;
         lcp[p - 1] = d;
     }
     return lcp;
