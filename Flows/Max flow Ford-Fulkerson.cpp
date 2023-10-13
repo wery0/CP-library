97a5f670d016f3b55a614335d2fd01884845dfc2
wery0 <wery0123@yandex.ru>
2023-10-13 21:59:39 +0300

    Added function for getting flow on edges, added complexity

--- Flows/Max flow Ford-Fulkerson.cpp
+++ Flows/Max flow Ford-Fulkerson.cpp
@@ -2,9 +2,10 @@ template<typename T_flow>
 class ford_fulkerson {
     struct edge {
         int from, to;
-        T_flow f;
+        T_flow cap;
+        T_flow f = 0;
 
-        edge(int from, int to, T_flow f): from(from), to(to), f(f) {}
+        edge(int from, int to, T_flow cap): from(from), to(to), cap(cap) {}
     };
 
     int V, ss, tt, us_iter = 0;
@@ -18,11 +19,11 @@ class ford_fulkerson {
         us[v] = us_iter;
         for (int i : l[v]) {
             auto& e = store[i];
-            if (e.f < scl) continue;
-            T_flow res = dfs(e.to, scl, min(min_flow, e.f));
+            if (e.cap - e.f < scl) continue;
+            T_flow res = dfs(e.to, scl, min(min_flow, e.cap - e.f));
             if (res > 0) {
-                e.f -= res;
-                store[i ^ 1].f += res;
+                e.f += res;
+                store[i ^ 1].f -= res;
                 return res;
             }
         }
@@ -32,6 +33,11 @@ class ford_fulkerson {
 public:
     ford_fulkerson(int V, int ss, int tt): V(V), ss(ss), tt(tt), l(V), us(V) {}
 
+    void clear() {
+        store.clear();
+        for (auto& i : l) i.clear(); 
+    }
+
     void add_edge(int x, int y, T_flow capacity, bool is_directed) {
         assert(0 <= min(x, y) && max(x, y) < V);
         l[x].push_back(store.size());
@@ -40,7 +46,13 @@ public:
         store.emplace_back(y, x, is_directed ? 0 : capacity);
     }
 
-    T_flow calc(bool do_scaling = true) {
+
+    /*
+      Complexity: at most 
+      - O(EF) without scaling (F is the answer, i. e. max flow)
+      - O(E^2log(C)) with scaling (C is max edge capacity)
+    */
+    T_flow calc_max_flow(bool do_scaling = true) {
         T_flow ans = 0;
         for (T_flow mxf = do_scaling ? MAX_EDGE_CAPACITY : 1; mxf > 0; mxf /= 2) {
             while (true) {
@@ -51,5 +63,17 @@ public:
             }
         }
         return ans;
+    }
+
+    
+    //For every added edge returns the flow going through it.
+    //If the flow on edge x->y is negative, then the flow goes from y to x (could only happen with undirected edges)
+    vector<array<T_flow, 3>> get_flow_on_edges() {
+        vector<array<T_flow, 3>> res(store.size() / 2);
+        for (size_t i = 0; i < store.size(); i += 2) {
+            auto& e = store[i];
+            res[i / 2] = {e.from, e.to, e.f};
+        }
+        return res;
     }
 };
