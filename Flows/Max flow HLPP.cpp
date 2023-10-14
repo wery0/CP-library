7bc950141d7cc3c67eb6a9dd8bdf25fd0d8afbff
wery0 <wery0123@yandex.ru>
2023-10-14 08:06:10 +0300

    Added functionality, changed way of storing edges

--- Flows/Max flow HLPP.cpp
+++ Flows/Max flow HLPP.cpp
@@ -1,19 +1,22 @@
-template<typename T>
+template<typename T_flow>
 class HLPP {
-    const T INF = numeric_limits<T>::max();
+    static constexpr T_flow INF = numeric_limits<T_flow>::max();
     struct edge {
-        int to, rev;
-        T f;
+        int to;
+        T_flow f = 0, cap;
 
         edge() = default;
-        edge(int x, int y, T c): to(x), rev(y), f(c) {}
+        edge(int to, T_flow cap): to(to), cap(cap) {}
     };
 
-    int V, s, t, mxh, work;
-    vector<vector<edge>> adj;
+    int V, ss, tt;
+    int mxh = 0, work = 0;
+    vector<edge> store;
+    vector<vector<int>> adj;
     vector<vector<int>> lst, gap;
-    vector<T> ex;
+    vector<T_flow> ex;
     vector<int> h, cnt;
+    bool flow_calculated = false;
 
     void updh(int v, int nh) {
         work++;
@@ -25,18 +28,19 @@ class HLPP {
         if (ex[v] > 0) lst[nh].push_back(v);
     }
 
-    void globalRelabel() {
+    void global_relabel() {
         work = 0;
         fill(h.begin(), h.end(), V);
         fill(cnt.begin(), cnt.end(), 0);
-        for (int i = 0; i < min(mxh, V); i++) lst[i].clear(), gap[i].clear();
-        h[t] = 0;
-        queue<int> q({t});
+        for (int i = 0; i < min(mxh, V); ++i) lst[i].clear(), gap[i].clear();
+        h[tt] = 0;
+        queue<int> q({tt});
         while (!q.empty()) {
             int v = q.front();
             q.pop();
-            for (auto& e : adj[v]) {
-                if (h[e.to] == V && adj[e.to][e.rev].f > 0) {
+            for (int i : adj[v]) {
+                auto& e = store[i];
+                if (h[e.to] == V && store[i ^ 1].cap - store[i ^ 1].f > 0) {
                     q.push(e.to), updh(e.to, h[v] + 1);
                 }
             }
@@ -44,19 +48,21 @@ class HLPP {
         }
     }
 
-    void push(int v, edge& e) {
+    void push(int v, int i) {
+        auto& e = store[i];
         if (ex[e.to] == 0) lst[h[e.to]].push_back(e.to);
-        T df = min(ex[v], e.f);
-        e.f -= df, adj[e.to][e.rev].f += df;
+        T_flow df = min(ex[v], e.cap - e.f);
+        e.f += df, store[i ^ 1].f -= df;
         ex[v] -= df, ex[e.to] += df;
     }
 
     void discharge(int v) {
         int nh = V;
-        for (auto& e : adj[v]) {
-            if (e.f > 0) {
+        for (int i : adj[v]) {
+            edge& e = store[i];
+            if (e.cap - e.f > 0) {
                 if (h[v] == h[e.to] + 1) {
-                    push(v, e);
+                    push(v, i);
                     if (ex[v] <= 0) return;
                 } else {
                     nh = min(nh, h[e.to] + 1);
@@ -73,16 +79,23 @@ class HLPP {
         }
     }
 
-public:
-    HLPP(int n, int s, int t): V(n), s(s), t(t), adj(V), lst(V + 1), gap(V), ex(V), h(V), cnt(V) {}
+ public:
+    HLPP(int n, int ss, int tt): V(n), ss(ss), tt(tt), adj(V), lst(V + 1), gap(V), ex(V), h(V), cnt(V) {
+        assert(ss != tt);
+    }
 
-    void add_edge(int x, int to, T f, int is_dir) {
-        assert(0 <= min(x, to) && max(x, to) < V);
-        adj[x].emplace_back(to, adj[to].size(), f);
-        adj[to].emplace_back(x, adj[x].size() - 1, is_dir ? 0 : f);
+    void add_edge(int x, int y, T_flow capacity, bool is_directed) {
+        assert(!flow_calculated);
+        assert(capacity >= 0);
+        assert(0 <= min(x, y) && max(x, y) < V);
+        adj[x].push_back(store.size());
+        store.emplace_back(y, capacity);
+        adj[y].push_back(store.size());
+        store.emplace_back(x, is_directed ? 0 : capacity);
     }
 
     void clear() {
+        store.clear();
         for (int i = 0; i < V; ++i) {
             adj[i].clear();
             lst[i].clear();
@@ -92,22 +105,36 @@ public:
         fill(h.begin(), h.end(), 0);
         fill(cnt.begin(), cnt.end(), 0);
         fill(ex.begin(), ex.end(), 0);
+        flow_calculated = false;
     }
 
-    T calc(int heur_n = -1) {
+    T_flow calc_max_flow(int heur_n = -1) {
         if (heur_n == -1) heur_n = V;
         fill(ex.begin(), ex.end(), 0);
-        ex[s] = INF;
-        globalRelabel();
-        for (auto& e : adj[s]) push(s, e);
+        ex[ss] = INF;
+        global_relabel();
+        for (int i : adj[ss]) push(ss, i);
         for (; mxh >= 0; --mxh) {
             while (!lst[mxh].empty()) {
                 int v = lst[mxh].back();
                 lst[mxh].pop_back();
                 discharge(v);
-                if (work > 4 * heur_n) globalRelabel();
+                if (work > 4 * heur_n) global_relabel();
             }
         }
-        return ex[t];
+        flow_calculated = true;
+        return ex[tt];
+    }
+
+    //For every added edge returns the flow going through it.
+    //If the flow on edge x->y is negative, then the flow goes from y to x (could only happen with undirected edges)
+    //Will work only if ex[ss] was set to max_flow at the beginning of calc_max_flow() since this push-relabel maintains preflow
+    vector<tuple<int, int, T_flow>> get_flow_on_edges() const {
+        vector<tuple<int, int, T_flow>> res(store.size() / 2);
+        for (size_t i = 0; i < store.size(); i += 2) {
+            const auto& e = store[i];
+            res[i / 2] = {store[i ^ 1].to, e.to, e.f};
+        }
+        return res;
     }
 };
