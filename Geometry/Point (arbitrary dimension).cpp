a24d08f105c8bbcdce4055aad71d52c71a89bec7
wery0 <wery0123@yandex.ru>
2023-12-31 08:19:14 +0300

    Fixed friend methods

--- Geometry/Point (arbitrary dimension).cpp
+++ Geometry/Point (arbitrary dimension).cpp
@@ -1,5 +1,5 @@
 //const long double EPS = 1e-9;
-template<typename T, const int D, typename R = long double>
+template<typename T, const size_t D, typename R = long double>
 struct hpt {
     static_assert(D > 0);
     static_assert(is_floating_point_v<R>);
@@ -34,31 +34,36 @@ struct hpt {
     hpt<T, D>& operator/=(const T c) {for (T& x : p) x /= c; return *this;}
 
     template<typename U>
-    auto dot(const hpt<U, D>& rhs) const {return dot(*this, rhs);}
+    auto dot(const hpt<U, D>& rhs) const {auto res = p[0] * rhs.p[0]; for (size_t i = 1; i < D; ++i) res += p[i] * rhs.p[i]; return res;}
     template<typename U>
-    auto dist(const hpt<U, D>& rhs) const {return dist(*this, rhs);}
+    auto dist(const hpt<U, D>& rhs) const {auto res = (p[0] - rhs.p[0]) * (p[0] - rhs.p[0]); for (size_t i = 1; i < D; ++i) res += (p[i] - rhs.p[i]) * (p[i] - rhs.p[i]); return sqrtl((R)res);}
     template<typename U>
-    R dist2(const hpt<U, D>& rhs) const {return dist2(*this, rhs);}
+    R dist2(const hpt<U, D>& rhs) const {auto res = (p[0] - rhs.p[0]) * (p[0] - rhs.p[0]); for (size_t i = 1; i < D; ++i) res += (p[i] - rhs.p[i]) * (p[i] - rhs.p[i]); return res;}
     template<typename U>
-    auto mdist(const hpt<U, D>& rhs) const {return mdist(*this, rhs);}
+    auto mdist(const hpt<U, D>& rhs) const {auto res = abs(p[0] - rhs.p[0]); for (size_t i = 1; i < D; ++i) res += abs(p[i] - rhs.p[i]); return res;}
 
-    template<typename U, const int DD>
-    friend auto dot(const hpt& p1, const hpt<U, DD>& p2) {auto res = p1[0] * p2[0]; for (size_t i = 1; i < D; ++i) res += p1[i] * p2[i]; return res;}
-    template<typename U, const int DD>
-    friend R dist(const hpt& p1, const hpt<U, DD>& p2) {auto res = (p1[0] - p2[0]) * (p1[0] - p2[0]); for (size_t i = 1; i < p1.size(); ++i) res += (p1[i] - p2[i]) * (p1[i] - p2[i]); return sqrtl((R)res);}
-    template<typename U, const int DD>
-    friend auto dist2(const hpt& p1, const hpt<U, DD>& p2) {auto res = (p1[0] - p2[0]) * (p1[0] - p2[0]); for (size_t i = 1; i < p1.size(); ++i) res += (p1[i] - p2[i]) * (p1[i] - p2[i]); return res;}
-    template<typename U, const int DD>
-    friend auto mdist(const hpt& p1, const hpt<U, DD>& p2) {auto res = abs(p1[0] - p2[0]); for (size_t i = 1; i < p1.size(); ++i) res += abs(p1[i] - p2[i]); return res;}
+    template<typename U>
+    friend auto dot(const hpt& p1, const hpt<U, D>& p2) {return p1.dot(p2);}
+    template<typename U>
+    friend R dist(const hpt& p1, const hpt<U, D>& p2) {return p1.dist(p2);}
+    template<typename U>
+    friend auto dist2(const hpt& p1, const hpt<U, D>& p2) {return p1.dist2(p2);}
+    template<typename U>
+    friend auto mdist(const hpt& p1, const hpt<U, D>& p2) {return p1.mdist(p2);}
 
-    template<typename U, const int DD>
+    auto begin() {return p.begin();}
+    const auto begin() const {return p.begin();}
+    auto end() {return p.end();}
+    const auto end() const {return p.end();}
+
+    template<typename U, const size_t DD>
     bool operator==(const hpt<U, DD>& rhs) const {
-        if constexpr(is_integral_v<T> && is_integral_v<U>) return mdist(*this, rhs) == 0;
-        return mdist(rhs - *this) < EPS;
+        if constexpr(is_integral_v<T> && is_integral_v<U>) return mdist(rhs) == 0;
+        return mdist(rhs) < EPS;
     }
-    template<typename U, const int DD>
+    template<typename U, const size_t DD>
     bool operator!=(const hpt<U, DD>& rhs) const {return !(*this == rhs);}
-    template<typename U, const int DD>
+    template<typename U, const size_t DD>
     bool operator<(const hpt<U, DD>& rhs) const {
         static const auto eps = is_integral_v<T> && is_integral_v<U> ? 0 : EPS;
         for (size_t i = 0; i < D; ++i) {
@@ -67,12 +72,12 @@ struct hpt {
         return false;
     }
 
-    R dist_to_origin() const {return sqrtl((R)dist_to_origin());}
+    R dist_to_origin() const {return sqrtl((R)dist2_to_origin());}
     T dist2_to_origin() const {T res = 0; for (const T x : p) res += x * x; return res;}
 
     void self_normalize() {
         T d2 = dist2_to_origin();
-        if (d2 < EPS) assert(0);
+        assert(d2 > EPS && "Too close to 0!");
         R len = sqrtl(d2);
         for (T& x : p) x /= len;
     }
@@ -86,6 +91,6 @@ struct hpt {
     T& operator[](size_t i) {return p[i];}
     const T& operator[](size_t i) const {return p[i];}
 
-    friend istream& operator>>(istream& is, hpt& p) {for (size_t i = 0; i < p.size(); ++i) cin >> p[i]; return is;}
+    friend istream& operator>>(istream& is, hpt& p) {for (size_t i = 0; i < p.size(); ++i) is >> p[i]; return is;}
     friend ostream& operator<<(ostream& os, const hpt& p) {os << '{'; for (size_t i = 0; i < p.size(); ++i) os << p[i] << (i + 1 == p.size() ? "}" : ", "); return os;}
 };
