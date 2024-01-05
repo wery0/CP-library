bc6b2cb1080b2057e99a666fddcf66fb6dce0075
wery0 <wery0123@yandex.ru>
2024-01-05 13:06:23 +0300

    Added rank calculation, common logic moved into separate static functions

--- Data structures/Matrix.cpp
+++ Data structures/Matrix.cpp
@@ -1,7 +1,32 @@
 template<typename T>
 class Matrix {
+    static constexpr T ZERO = 0;
+
     size_t n = 0, m = 0;
     vector<vector<T>> t;
+
+    static void add_row(vector<T>& from, vector<T>& to, size_t sti, T koef) {
+        if (koef == ZERO) return;
+        const size_t N = to.size();
+        assert(from.size() == N);
+        for (size_t i = sti; i < N; ++i) to[i] += from[i] * koef;
+    }
+
+    static void normalize_row(vector<T>& row, size_t i) {
+        const size_t N = row.size();
+        assert(i < N);
+        assert(row[i] != ZERO);
+        const T inv = T(1) / row[i];
+        for (size_t j = i; j < N; ++j) row[j] *= inv;
+    }
+
+    static vector<vector<T>> transpose_vv(const vector<vector<T>>& t) {
+        const size_t n = t.size();
+        const size_t m = n ? t[0].size() : 0;
+        vector<vector<T>> u(m, vector<T>(n));
+        for (size_t j = 0; j < m; ++j) for (size_t i = 0; i < n; ++i) u[j][i] = t[i][j];
+        return u;
+    }
 
 public:
     Matrix() = default;
@@ -55,11 +80,13 @@ public:
     Matrix get_pow(int64_t k) const {assert(n == m); assert(k >= 0); Matrix<T> res(n, n), x = *this; for (res = 1; k; k >>= 1, x *= x) if (k & 1) res *= x; return res;}
     void self_pow(int64_t k) {*this = get_pow(k);}
 
-    //For these two functions, T must be invertible
-    T calc_determinant() const {assert(n == m); T res = 1; auto u = t; for (size_t i = 0; i < n; ++i) {size_t ii = i; while (ii < n && u[ii][i] == (T)0) ++ii; if (ii == n) return 0; if (ii != i) swap(u[i], u[ii]), res *= T(-1); res *= u[i][i]; T inv = T(1) / u[i][i]; for (size_t j = i; j < n; ++j) u[i][j] *= inv; for (size_t ii = i + 1; ii < n; ++ii) {const T koef = u[ii][i]; if (koef == (T)0) continue; for (size_t j = i; j < n; ++j) u[ii][j] -= u[i][j] * koef;}} return res;}
-    Matrix calc_inverse() const {assert(n == m); auto u = t; for (size_t i = 0; i < n; ++i) {u[i].resize(n * 2); u[i][n + i] = 1;} for (size_t i = 0; i < n; ++i) {size_t ii = i; while (ii < n && u[ii][i] == (T)0) ++ii; if (ii >= n) return {}; assert(ii < n && "Inverse matrix doesn't exist"); if (ii != i) swap(u[i], u[ii]); T inv = T(1) / u[i][i]; for (size_t j = i; j < n * 2; ++j) u[i][j] *= inv; for (size_t ii = i + 1; ii < n; ++ii) {const T koef = u[ii][i]; if (koef == (T)0) continue; for (size_t j = i; j < n * 2; ++j) u[ii][j] -= u[i][j] * koef;}} for (size_t q = 1; q < n; ++q) {for (size_t i = 0; i < q; ++i) {const T koef = u[i][q]; if (koef == (T)0) continue; for (size_t j = q; j < n * 2; ++j) {u[i][j] -= koef * u[q][j];}}} for (size_t i = 0; i < n; ++i) {rotate(u[i].begin(), u[i].begin() + n, u[i].end()); u[i].resize(n);} return u;}
+    //For these three functions, T must be invertible
+    //O(min(n, m) ^ 2 * max(n, m))
+    size_t calc_rank() const {auto u = t; if (n > m) u = transpose_vv(u); const size_t n = u.size(); const size_t m = n ? u[0].size() : 0; size_t rk = 0; for (size_t i = 0, c = 0; i < n && c < m; ++i, ++c, ++rk) {size_t r; for (; c < m; ++c) {r = i; while (r < n && u[r][c] == ZERO) ++r; if (r != n) break;} if (c == m) break; if (r != i) swap(u[i], u[r]); normalize_row(u[i], c); for (size_t j = r + 1; j < n; ++j) {add_row(u[i], u[j], c, -u[j][c]);}} return rk;}
+    T calc_determinant() const {assert(n == m); T res = 1; auto u = t; for (size_t i = 0; i < n; ++i) {size_t r = i; while (r < n && u[r][i] == ZERO) ++r; if (r == n) return 0; if (r != i) swap(u[i], u[r]), res *= T(-1); res *= u[i][i]; normalize_row(u[i], i); for (size_t j = r + 1; j < n; ++j) {add_row(u[i], u[j], i, -u[j][i]);}} return res;}
+    Matrix calc_inverse() const {assert(n == m); auto u = t; for (size_t i = 0; i < n; ++i) {u[i].resize(n * 2); u[i][n + i] = 1;} for (size_t i = 0; i < n; ++i) {size_t r = i; while (r < n && u[r][i] == ZERO) ++r; if (r >= n) return {}; assert(r < n && "Inverse matrix doesn't exist"); if (r != i) swap(u[i], u[r]); normalize_row(u[i], i); for (size_t j = r + 1; j < n; ++j) {add_row(u[i], u[j], i, -u[j][i]);}} for (size_t i = 1; i < n; ++i) {for (size_t j = 0; j < i; ++j) {add_row(u[i], u[j], i, -u[j][i]);}} for (size_t i = 0; i < n; ++i) {rotate(u[i].begin(), u[i].begin() + n, u[i].end()); u[i].resize(n);} return u;}
 
-    Matrix get_transpose() const {vector<vector<T>> u(m, vector<T>(n)); for (size_t j = 0; j < m; ++j) for (size_t i = 0; i < n; ++i) u[j][i] = t[i][j]; return Matrix(u);}
+    Matrix get_transpose() const {return Matrix(transpose_vv(t));}
     void self_transpose() {*this = get_transpose();}
 
     friend istream& operator>>(istream& in, Matrix& rhs) {for (size_t i = 0; i < rhs.n; ++i) for (size_t j = 0; j < rhs.m; ++j) in >> rhs[i][j]; return in;}
