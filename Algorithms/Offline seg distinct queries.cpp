86c43862605e637a6d3123e39a2a92f5771d7a69
wery0 <wery0123@yandex.ru>
2024-01-05 13:36:44 +0300

    Minor changes

--- Algorithms/Offline seg distinct queries.cpp
+++ Algorithms/Offline seg distinct queries.cpp
@@ -25,24 +25,29 @@ public:
     }
 
     vector<int> process_queries(vector<pair<int, int>> queries) {
-        int z = queries.size();
-        vector<int> ans(z);
-        vector<array<int, 3>> queriess(z);
-        for (int q = 0; q < z; ++q) queriess[q] = {queries[q].first, queries[q].second, q};
+        const size_t Q = queries.size();
+        vector<int> ans(Q);
+        vector<array<int, 3>> queriess(Q);
+        for (int i = 0; i < Q; ++i) {
+            auto [l, r] = queries[i];
+            assert(min(l, r) >= 0);
+            queriess[i] = {l, r, i};
+        }
         sort(queriess.begin(), queriess.end(), [&](const auto& l, const auto& r) {
             return l[1] < r[1];
         });
         vector<int> fen(n + 1);
-        for (int r = 0, i = 0; r < n; r++) {
+        for (int r = 0, i = 0; r < n; ++r) {
             for (int p = r + 1; p <= n; p += p & -p) ++fen[p];
             if (closest_left_eq[r] != -1) {
                 for (int p = closest_left_eq[r] + 1; p <= n; p += p & -p) --fen[p];
             }
-            for (; i < z && queriess[i][1] == r; ++i) {
-                int sum = 0;
-                for (int p = queriess[i][1] + 1; p; p -= p & -p) sum += fen[p];
-                for (int p = queriess[i][0]; p; p -= p & -p) sum -= fen[p];
-                ans[queriess[i][2]] = sum;
+            for (; i < Q && queriess[i][1] == r; ++i) {
+                auto [l, r, u] = queriess[i];
+                int res = 0;
+                for (int p = r + 1; p; p -= p & -p) res += fen[p];
+                for (int p = l; p; p -= p & -p) res -= fen[p];
+                ans[u] = l > r ? 0 : res;
             }
         }
         return ans;
