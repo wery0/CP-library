c10bb0912faaa21c4a4c2f9ab483665f8d981234
wery0 <wery0123@yandex.ru>
2024-01-08 15:21:29 +0300

    Improved code

--- Data structures/Segtree/Point change/!Additional stuff.cpp
+++ Data structures/Segtree/Point change/!Additional stuff.cpp
@@ -5,17 +5,14 @@
 int first_to_right_where_sum_becomes_geq_k(size_t l, T k) {
     size_t r = n * 2 - 1;
     for (l += n; l <= r; l = (l + 1) >> 1, r = (r - 1) >> 1) {
-        if (l & 1) {
-            if (m[l].sm < k) k -= m[l].sm;
-            else {
-                size_t v = l;
-                while (v < n) {
-                    v <<= 1;
-                    if (m[v].sm < k) k -= m[v].sm, v |= 1;
-                }
-                k -= m[v].sm;
-                return v - n;
+        if (~l & 1) continue;
+        if (m[l].sm < k) k -= m[l].sm;
+        else {
+            while (l < n) {
+                l <<= 1;
+                if (m[l].sm < k) k -= m[l].sm, l ^= 1;
             }
+            return l - n;
         }
     }
     return -1;
@@ -25,18 +22,14 @@ int first_to_right_where_sum_becomes_geq_k(size_t l, T k) {
 int first_to_left_where_sum_becomes_geq_k(size_t r, T k) {
     size_t l = n;
     for (r += n; l <= r; l = (l + 1) >> 1, r = (r - 1) >> 1) {
-        if (~r & 1) {
-            if (m[r].sm < k) k -= m[r].sm;
-            else {
-                size_t v = r;
-                while (v < n) {
-                    v <<= 1;
-                    if (m[v | 1].sm < k) k -= m[v | 1].sm;
-                    else v |= 1;
-                }
-                k -= m[v].sm;
-                return v - n;
+        if (r & 1) continue;
+        if (m[r].sm < k) k -= m[r].sm;
+        else {
+            while (r < n) {
+                r = r << 1 | 1;
+                if (m[r].sm < k) k -= m[r].sm, r ^= 1;
             }
+            return r - n;
         }
     }
     return -1;
