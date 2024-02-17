d750f661019e76234a67a403c43c5726ebbcf9d0
wery0 <wery0123@yandex.ru>
2024-02-18 02:23:51 +0300

    Fixed bug, merge function made template parameter, improved code, added description

--- Data structures/Disjoint sparse table.cpp
+++ Data structures/Disjoint sparse table.cpp
@@ -1,31 +1,33 @@
-template<typename T>
+//Binary function merge() must be associative and commutative
+//<O(nlog(n)T(merge)), O(nlog(n))> preprocessing, O(T(merge)) per query
+template<typename T, auto merge = [](const T& l, const T& r) -> T{return min(l, r);}>
 class disjoint_sparse_table {
     size_t n, K;
     vector<vector<T>> m;
-
-    //Change, if need
-    inline T merge(T x, T y) const {
-        return min(x, y);
-    }
-
+ 
 public:
     disjoint_sparse_table() = default;
     template<typename I>
-    disjoint_sparse_table(I first, I last): n(last - first), K(n ? __lg(n) + 1 + (!!(n & (n - 1))) : 0), m(K - 1, vector<T>(n)) {
-        for (ssize_t i = 1; i < K; ++i) {
-            for (ssize_t l = 0, md = (1 << i) / 2, r = 1 << i; md < n; l += 1 << i, md += 1 << i, r += 1 << i) {
-                m[i - 1][md - 1] = *(first + md - 1);
-                m[i - 1][md] = *(first + md);
-                for (ssize_t j = md - 2; j >= l; --j) m[i - 1][j] = merge(m[i - 1][j + 1], *(first + j));
-                for (ssize_t j = md + 1, gr = n < r ? n : r; j < gr; ++j) m[i - 1][j] = merge(m[i - 1][j - 1], *(first + j));
+    disjoint_sparse_table(I first, I last): n(std::distance(first, last)), K(n > 1 ? __lg(n) + 1 - !(n & (n - 1)) : n), m(K, vector<T>(n)) {
+        auto get = [&](size_t i) {return *(first + i);};
+        for (size_t i = 0; i < K; ++i) {
+            std::copy(first, last, m[i].begin());
+            if (i == 0) continue;
+            const size_t step = 2 << i;
+            for (size_t l = 0, md = step / 2 + 1; md <= n; l += step, md += step) {
+                const size_t t = min(n + 1, md + step / 2);
+                for (size_t j = md - 2; j > l; --j) m[i][j - 1] = merge(m[i][j], get(j - 1));
+                for (size_t j = md + 1; j < t; ++j) m[i][j - 1] = merge(m[i][j - 2], get(j - 1));
             }
         }
     }
     template<typename T_arr>
     disjoint_sparse_table(const T_arr& arr) {*this = disjoint_sparse_table(arr.begin(), arr.end());}
-
+ 
     T query(size_t l, size_t r) const {
+        assert(l <= r && r < n);
         const int u = l == r ? 0 : __lg(l ^ r);
         return l == r ? m[0][l] : merge(m[u][l], m[u][r]);
     }
 };
+ 
