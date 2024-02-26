88b9080084b9b6950249e879e3421ef9aecbe226
wery0 <wery0123@yandex.ru>
2024-02-26 04:36:42 +0300

    Improved interface, namings, added asserts

--- Math & number theory/Mint.cpp
+++ Math & number theory/Mint.cpp
@@ -1,6 +1,13 @@
-template<typename INT, typename LL, const INT MOD>
-struct Mint {
+template<typename INT, typename LONG, const INT MOD>
+class Mint {
+    static_assert(is_integral_v<INT> && is_integral_v<LONG>);
+    static_assert(numeric_limits<INT>::max() < numeric_limits<LONG>::max() / numeric_limits<INT>::max() && "Product of two INT's must fit into LONG");
+
     INT val = 0;
+
+public:
+    using int_type = INT;
+    using long_type = LONG;
 
     constexpr Mint() = default;
     template<typename T>
@@ -11,44 +18,49 @@ struct Mint {
         val = c;
     }
 
-    template<typename I, typename L, const int M>
-    friend Mint<I, L, M> binpow(const Mint<I, L, M>& m, int64_t pw) {
-        L o = 1;
-        for (L x = m.val; pw; pw >>= 1) {
-            if (pw & 1) o = o * x % M;
-            x = x * x % M;
+    constexpr INT get_val() const {return val;}
+    static constexpr INT get_mod() {return MOD;}
+
+    Mint pow(LONG pw) const {
+        LONG o = 1;
+        for (LONG x = val; pw; pw >>= 1) {
+            if (pw & 1) o = o * x % MOD;
+            x = x * x % MOD;
         }
-        return o;
+        return Mint(o);
     }
 
-    template<typename I, typename L, const int M>
-    friend Mint<I, L, M> inv(const Mint<I, L, M>& m) {
-        return binpow(m, M - 2);
+    Mint inv() const {
+        return pow(MOD - 2);
     }
 
-    Mint operator+(const Mint& o) const {return val + o.val >= MOD ? val + o.val - MOD : val + o.val;}
-    Mint operator-(const Mint& o) const {return val < o.val ? MOD - o.val + val : val - o.val;}
-    Mint operator*(const Mint& o) const {return (LL)1 * val * o.val % MOD;}
-    Mint operator/(const Mint& o) const {return (LL)1 * val * inv(o).val % MOD;}
+    template<typename I, typename L, const int M> friend Mint<I, L, M> pow(const Mint<I, L, M>& m, LONG pw) {return m.pow(pw);}
+    template<typename I, typename L, const int M> friend Mint<I, L, M> inv(const Mint<I, L, M>& m) {return m.inv();}
+
+    Mint operator+(const Mint& rhs) const {return val + rhs.val >= MOD ? val + rhs.val - MOD : val + rhs.val;}
+    Mint operator-(const Mint& rhs) const {return val < rhs.val ? MOD - rhs.val + val : val - rhs.val;}
+    Mint operator*(const Mint& rhs) const {return (LONG)1 * val * rhs.val % MOD;}
+    Mint operator/(const Mint& rhs) const {return (LONG)1 * val * inv(rhs).val % MOD;}
     Mint operator-() const {return val ? MOD - val : val;}
 
-    void operator+=(const Mint& o) {val += o.val; val -= val < MOD ? 0 : MOD;}
-    void operator-=(const Mint& o) {val -= o.val; val += val < 0 ? MOD : 0;}
-    void operator*=(const Mint& o) {val = (LL)1 * val * o.val % MOD;}
-    void operator/=(const Mint& o) {val = (LL)1 * val * inv(o).val % MOD;}
+    void operator+=(const Mint& rhs) {val += rhs.val; val -= val < MOD ? 0 : MOD;}
+    void operator-=(const Mint& rhs) {val -= rhs.val; val += val < 0 ? MOD : 0;}
+    void operator*=(const Mint& rhs) {val = (LONG)1 * val * rhs.val % MOD;}
+    void operator/=(const Mint& rhs) {val = (LONG)1 * val * inv(rhs).val % MOD;}
 
     Mint& operator++() {val = val == MOD - 1 ? 0 : val + 1; return *this;}
     Mint operator++(int) {Mint res = *this; ++*this; return res;}
+    Mint& operator--() {val = val ? val - 1 : MOD - 1; return *this;}
+    Mint operator--(int) {Mint res = *this; --*this; return res;}
 
-    bool operator==(const Mint& o) const {return val == o.val;}
-    bool operator!=(const Mint& o) const {return val != o.val;}
-    bool operator<(const Mint& o) const {return val < o.val;}
-    bool operator>(const Mint& o) const {return val > o.val;}
+    bool operator==(const Mint& rhs) const {return val == rhs.val;}
+    bool operator!=(const Mint& rhs) const {return val != rhs.val;}
+    bool operator<(const Mint& rhs) const {return val < rhs.val;}
+    bool operator>(const Mint& rhs) const {return val > rhs.val;}
 
     explicit operator bool() const {return val != 0;}
 
-    friend istream& operator>>(istream& in, Mint& m) {return in >> m.val;}
-    friend ostream& operator<<(ostream& out, const Mint& m) {return out << m.val;}
-    friend ostream& operator<<(ostream& out, const Mint&& m) {return out << m.val;}
+    friend istream& operator>>(istream& in, Mint& rhs) {return in >> rhs.val;}
+    friend ostream& operator<<(ostream& out, const Mint& rhs) {return out << rhs.val;}
 };
 using mint = Mint<int32_t, int64_t, 998244353>;
