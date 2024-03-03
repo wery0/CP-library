44839ad4a783f24ddb951b8968775e2b529d650b
wery0 <wery0123@yandex.ru>
2024-03-03 22:13:50 +0300

    Removed dual function

--- Algorithms/Kadane's algorithm.cpp
+++ Algorithms/Kadane's algorithm.cpp
@@ -20,26 +20,3 @@ T subsegment_with_max_sum(I first, I last) {
     if (seg_borders[0] == -1) return *max_element(first, last);
     return mxs;
 }
-
-//Finds shortest subsegment with min sum
-//Sum of any subsegment must fit in T
-//If first == last - returns 0
-//If [first, last) doesn't contain positive numbers - returns min number
-template<typename I, typename T = long long>
-T subsegment_with_min_sum(I first, I last) {
-    if (first == last) return 0;
-    T cur = 0, mns = 0;
-    array<int, 2> seg_borders = {-1, -1};
-    int cur_l = -1;
-    auto cit = first;
-    for (int i = 0; cit != last; ++i, ++cit) {
-        cur += *cit;
-        if (cur < mns || (cur == mns && i - cur_l < seg_borders[1] - seg_borders[0])) {
-            mns = cur;
-            seg_borders = {cur_l + 1, i};
-        }
-        if (cur >= 0) cur = 0, cur_l = i;
-    }
-    if (seg_borders[0] == -1) return *min_element(first, last);
-    return mns;
-}
