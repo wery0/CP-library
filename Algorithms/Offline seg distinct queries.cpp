50be42ae3f1ea965b972a8cbc82b70e6f65e0486
wery0 <wery0123@yandex.ru>
2024-05-11 18:48:45 +0300

    Improved class design

--- Algorithms/Offline seg distinct queries.cpp
+++ Algorithms/Offline seg distinct queries.cpp
@@ -2,51 +2,47 @@ class offline_seg_distinct_queries {
 
     int n;
     vector<int> closest_left_eq;
-
-    template<typename I>
-    vector<int> get_closest_left_eq(I first, I last) {
-        using T = typename iterator_traits<I>::value_type;
-        const int n = last - first;
-        vector<int> ans(n, -1);
-        umap<T, int> mp;
-        auto cit = first;
-        for (int q = 0; q < n; ++q, ++cit) {
-            T cur = *cit;
-            if (mp.count(cur)) ans[q] = mp[cur];
-            mp[cur] = q;
-        }
-        return ans;
-    }
+    vector<array<int, 3>> store;
 
 public:
     template<typename T>
-    offline_seg_distinct_queries(vector<T>& arr): n(arr.size()) {
-        closest_left_eq = get_closest_left_eq(arr.begin(), arr.end());
+    offline_seg_distinct_queries(vector<T>& arr, size_t Q = 0): n(arr.size()), closest_left_eq(n, -1) {
+        store.reserve(Q);
+        umap<T, int> mp;
+        for (size_t i = 0; i < n; ++i) {
+            if (mp.count(arr[i])) closest_left_eq[i] = mp[arr[i]];
+            mp[arr[i]] = i;
+        }
     }
 
-    vector<int> process_queries(vector<pair<int, int>> queries) {
-        const size_t Q = queries.size();
+    void add_query(size_t l, size_t r) {
+        assert(l <= r && r < n);
+        using T = int;
+        store.push_back({T(l), T(r), T(store.size())});
+    }
+
+    void clear() {
+        store.clear();
+    }
+
+    //O(Qlog(n))
+    vector<int> process_queries() {
+        const size_t Q = store.size();
         vector<int> ans(Q);
-        vector<array<int, 3>> queriess(Q);
-        for (int i = 0; i < Q; ++i) {
-            auto [l, r] = queries[i];
-            assert(min(l, r) >= 0);
-            queriess[i] = {l, r, i};
-        }
-        sort(queriess.begin(), queriess.end(), [&](const auto& l, const auto& r) {
+        sort(store.begin(), store.end(), [&](const auto& l, const auto& r) {
             return l[1] < r[1];
         });
         vector<int> fen(n + 1);
-        for (int r = 0, i = 0; r < n; ++r) {
-            for (int p = r + 1; p <= n; p += p & -p) ++fen[p];
+        for (size_t r = 0, i = 0; r < n; ++r) {
+            for (size_t p = r + 1; p <= n; p += p & -p) ++fen[p];
             if (closest_left_eq[r] != -1) {
-                for (int p = closest_left_eq[r] + 1; p <= n; p += p & -p) --fen[p];
+                for (size_t p = closest_left_eq[r] + 1; p <= n; p += p & -p) --fen[p];
             }
-            for (; i < Q && queriess[i][1] == r; ++i) {
-                auto [l, r, u] = queriess[i];
+            for (; i < Q && store[i][1] == r; ++i) {
+                auto [l, r, u] = store[i];
                 int res = 0;
-                for (int p = r + 1; p; p -= p & -p) res += fen[p];
-                for (int p = l; p; p -= p & -p) res -= fen[p];
+                for (size_t p = r + 1; p; p -= p & -p) res += fen[p];
+                for (size_t p = l; p; p -= p & -p) res -= fen[p];
                 ans[u] = l > r ? 0 : res;
             }
         }
