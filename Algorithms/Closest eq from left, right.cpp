abbd33ec1d44a8287a7a542db3396fe2682b45c7
wery0 <wery0123@yandex.ru>
2024-05-11 18:38:06 +0300

    Code made more generic

--- Algorithms/Closest eq from left, right.cpp
+++ Algorithms/Closest eq from left, right.cpp
@@ -2,14 +2,13 @@
 template<typename I>
 vector<int> closest_left_eq(I first, I last) {
     using T = typename iterator_traits<I>::value_type;
-    const int n = last - first;
+    const size_t n = std::distance(first, last);
     vector<int> ans(n, -1);
     umap<T, int> mp;
-    auto cit = first;
-    for (int q = 0; q < n; ++q, ++cit) {
-        T cur = *cit;
-        if (mp.count(cur)) ans[q] = mp[cur];
-        mp[cur] = q;
+    for (size_t i = 0; i < n; ++i) {
+        T cur = *(first++);
+        if (mp.count(cur)) ans[i] = mp[cur];
+        mp[cur] = i;
     }
     return ans;
 }
@@ -18,14 +17,13 @@ vector<int> closest_left_eq(I first, I last) {
 template<typename I>
 vector<int> closest_right_eq(I first, I last) {
     using T = typename iterator_traits<I>::value_type;
-    const int n = last - first;
+    const size_t n = std::distance(first, last);
     vector<int> ans(n, n);
     umap<T, int> mp;
-    auto cit = last;
-    for (int q = n - 1; q >= 0; --q) {
+    for (size_t i = n; i;) {
         T cur = *(--last);
-        if (mp.count(cur)) ans[q] = mp[cur];
-        mp[cur] = q;
+        if (mp.count(cur)) ans[--i] = mp[cur];
+        mp[cur] = i;
     }
     return ans;
 }
