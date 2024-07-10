05957018c3aed9a332e5695ab41bd76c548ee75a
wery0 <wery0123@yandex.ru>
2024-07-11 02:26:58 +0300

    Added description, improved code

--- Graphs/Prufer code to tree.cpp
+++ Graphs/Prufer code to tree.cpp
@@ -1,13 +1,16 @@
+//Returns vector of edges of corresponding tree
 //O(n)
-vector<pair<int, int>> prufer_code_to_tree(vector<int> pcode) {
-    const int n = pcode.size() + 2;
+vector<pair<int, int>> prufer_code_to_tree(const vector<int>& pcode) {
+    const size_t n = pcode.size() + 2;
     vector<int> cnt(n);
-    for (int v : pcode) ++cnt[v];
+    for (int v : pcode) {
+        assert(0 <= v && v < n);
+        ++cnt[v];
+    }
     vector<pair<int, int>> ans(n - 1);
     int leaf = find(cnt.begin(), cnt.end(), 0) - cnt.begin();
-    for (int i = 0, ptr = leaf + 1; i < n - 2; ++i) {
-        int p = pcode[i];
-        ans[i] = {p, leaf};
+    for (size_t i = 0, ptr = leaf + 1; int p : pcode) {
+        ans[i++] = {p, leaf};
         if (--cnt[p] == 0 && p < ptr) leaf = p;
         else {
             while (cnt[ptr]) ++ptr;
