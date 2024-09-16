0e4311114aeed37c0f1a4974ed3287a7fbf4ce3f
wery0 <wery0123@yandex.ru>
2024-09-16 04:10:27 +0300

    Added .clang-format

--- Algorithms/Longest common subsequence (2 strings).cpp
+++ Algorithms/Longest common subsequence (2 strings).cpp
@@ -3,7 +3,7 @@ template<typename I>
 auto longest_common_subsequence(I f1, I l1, I f2, I l2) {
     using T = typename iterator_traits<I>::value_type;
     const size_t N = l1 - f1, M = l2 - f2;
-    vector<vector<int>> dp(N + 1, vector<int>(M + 1));
+    vector dp(N + 1, vector<int>(M + 1));
     for (size_t i = 1; i <= N; ++i) {
         for (size_t j = 1; j <= M; ++j) {
             if (*(f1 + i - 1) == *(f2 + j - 1)) dp[i][j] = dp[i - 1][j - 1] + 1;
@@ -11,7 +11,7 @@ auto longest_common_subsequence(I f1, I l1, I f2, I l2) {
         }
     }
     vector<T> ans(dp[N][M]);
-    for (size_t i = N, j = M, pos = dp[N][M]; i && j; ) {
+    for (size_t i = N, j = M, pos = dp[N][M]; i && j;) {
         if (*(f1 + i - 1) == *(f2 + j - 1)) {
             ans[--pos] = *(f1 + i - 1);
             --i, --j;
--- Algorithms/Longest common subsequence length (3 strings).cpp
+++ Algorithms/Longest common subsequence length (3 strings).cpp
@@ -4,19 +4,17 @@ int longest_common_subsequence_length_3(I f1, I l1, I f2, I l2, I f3, I l3) {
     if (l2 - f2 > l1 - f1) swap(f1, f2), swap(l1, l2);
     if (l3 - f3 > l1 - f1) swap(f1, f3), swap(l1, l3);
     const size_t N = l1 - f1, M = l2 - f2, K = l3 - f3;
-    vector<vector<vector<int>>> dp(2, vector<vector<int>>(M + 1, vector<int>(K + 1)));
-    for (size_t qq = 1; qq <= N; ++qq) {
-        const size_t q = qq & 1;
-        for (size_t w = 1; w <= M; ++w) {
-            if (*(f1 + qq - 1) == *(f2 + w - 1)) {
-                for (size_t e = 1; e <= K; ++e) {
-                    if (*(f2 + w - 1) == *(f3 + e - 1)) dp[q][w][e] = dp[q ^ 1][w - 1][e - 1] + 1;
-                    else dp[q][w][e] = max({dp[q ^ 1][w][e], dp[q][w - 1][e], dp[q][w][e - 1]});
+    vector dp(2, vector(M + 1, vector<int>(K + 1)));
+    for (size_t ii = 1; ii <= N; ++ii) {
+        const size_t i = ii & 1;
+        for (size_t j = 1; j <= M; ++j) {
+            if (*(f1 + ii - 1) == *(f2 + j - 1)) {
+                for (size_t k = 1; k <= K; ++k) {
+                    if (*(f2 + j - 1) == *(f3 + k - 1)) dp[i][j][k] = dp[i ^ 1][j - 1][k - 1] + 1;
+                    else dp[i][j][k] = max({dp[i ^ 1][j][k], dp[i][j - 1][k], dp[i][j][k - 1]});
                 }
             } else {
-                for (int e = 1; e <= K; ++e) {
-                    dp[q][w][e] = max({dp[q ^ 1][w][e], dp[q][w - 1][e], dp[q][w][e - 1]});
-                }
+                for (size_t k = 1; k <= K; ++k) { dp[i][j][k] = max({dp[i ^ 1][j][k], dp[i][j - 1][k], dp[i][j][k - 1]}); }
             }
         }
     }
