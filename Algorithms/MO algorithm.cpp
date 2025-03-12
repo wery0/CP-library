cf6e0b17a2f8a5858e108bf10e1f2489c85ae8f9
wery0 <wery0123@yandex.ru>
2025-03-12 15:19:58 +0300

    Fixed comparator bug, improved code

--- Algorithms/MO algorithm.cpp
+++ Algorithms/MO algorithm.cpp
@@ -1,7 +1,7 @@
 class MO {
 
-    int N;
-    int BLOCK_SIZE;
+    size_t N;
+    size_t BLOCK_SIZE;
 
     struct query {
         int l, r, n;
@@ -12,48 +12,43 @@ class MO {
 
 public:
     MO() = default;
+    MO(size_t N): N(N), BLOCK_SIZE(sqrt(N)) {}
 
-    MO(int N, int Q = 1): N(N) {
-        BLOCK_SIZE = sqrt(N);
-        qarr.reserve(Q);
-    }
+    void clear() {qarr.clear();}
+    void reserve(size_t Q) {qarr.reserve(Q);}
 
-    void clear() {
-        qarr.clear();
-    }
-
-    void add_query(int l, int r) {
-        assert(0 <= l && l <= r && r < N);
+    void add_query(size_t l, size_t r) {
+        assert(l <= r && r < N);
         qarr.emplace_back(l, r, qarr.size());
     }
 
-    vector<int> process_queries() {
+    template<typename T>
+    vector<T> process_queries() {
         sort(qarr.begin(), qarr.end(), [&](const query& q1, const query& q2) {
             int bl1 = q1.l / BLOCK_SIZE;
             int bl2 = q2.l / BLOCK_SIZE;
             if (bl1 != bl2) return bl1 < bl2;
-            return (bool)((q1.r < q2.r) ^ (bl1 & 1));
+            return bl1 & 1 ? q1.r > q2.r : q1.r < q2.r;
         });
-        auto addL = [&](int i) -> void {
+        auto addL = [&](int idx) -> void {
 
         };
-        auto addR = [&](int i) -> void {
+        auto addR = [&](int idx) -> void {
 
         };
-        auto remL = [&](int i) -> void {
+        auto remL = [&](int idx) -> void {
 
         };
-        auto remR = [&](int i) -> void {
+        auto remR = [&](int idx) -> void {
 
         };
         int cl = 0, cr = -1;
-        vector<int> ans(qarr.size());
+        vector<T> ans(qarr.size());
         for (const auto& [ql, qr, qn] : qarr) {
             while (cr < qr) addR(++cr);
             while (cl > ql) addL(--cl);
             while (cr > qr) remR(cr--);
             while (cl < ql) remL(cl++);
-            //Edit
             ans[qn] = ?;
         }
         return ans;
