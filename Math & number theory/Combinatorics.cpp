4f08ce91d0fe7f1848bf3bebcd561a17289b72a4
wery0 <wery0123@yandex.ru>
2025-04-06 13:50:41 +0300

    Wrapped combinatorics into struct

--- Math & number theory/Combinatorics.cpp
+++ Math & number theory/Combinatorics.cpp
@@ -1,4 +1,4 @@
-template<typename T = int64_t>
+template<typename T>
 T extended_euclid_inv(T a, T mod) {
 	static_assert(is_signed_v<T>);
 	auto gcd = [](auto&& gcd, T a, T b, T& x, T& y) {
@@ -18,28 +18,64 @@ T extended_euclid_inv(T a, T mod) {
     return x;
 }
 
-const int64_t mod = 1e9 + 7;
-const int64_t G = 3e5 + 5;
-vector<int64_t> fc, ifc;
+template<typename T, const T mod>
+struct comb_mod {
+    static_assert(is_integral_v<T>);
+    static_assert(mod > 0);
+    static_assert(numeric_limits<T>::max() / mod > mod && "Product of two numbers < mod should fit into T!");
 
-int64_t powmod(int64_t x, int64_t k) {
-    if (k < 0) return 0;
-    int64_t o = 1;
-    for (; k; k >>= 1) {
-        if (k & 1) o = o * x % mod;
-        x = x * x % mod;
+    size_t N;
+    vector<T> fc, ifc;
+
+    comb_mod() = default;
+    comb_mod(size_t N): N(N), fc(N, 1), ifc(N, 1) {
+        assert(N > 1);
+        for (size_t i = 1; i < N; ++i) fc[i] = fc[i - 1] * i % mod;
+        ifc[N - 1] = inv(fc[N - 1]);
+        for (size_t i = N - 2; i; --i) ifc[i] = ifc[i + 1] * (i + 1);
     }
-    return o;
-}
 
-int64_t inv(int64_t x) {return powmod(x, mod - 2);}
+    T inv(T x) {
+        return binpow(x, mod - 2);
+    }
 
-int64_t C(int64_t n, int64_t k) {return n < 0 || k < 0 || k > n ? 0 : fc[n] * ifc[k] % mod * ifc[n - k] % mod;}
+    T binpow(T x, int64_t k) {
+        assert(k >= 0);
+        T res = 1;
+        for (; k; k >>= 1) {
+            if (k & 1) res = res * x % mod;
+            x = x * x % mod;
+        }
+        return res;
+    }
 
-int main() {
-    fast;
-    fc.resize(G, 1);
-    for (int i = 1; i < G; ++i) fc[i] = fc[i - 1] * i % mod;
-    ifc.resize(G, inv(fc[G - 1]));
-    for (int i = G - 2; i >= 0; --i) ifc[i] = ifc[i + 1] * (i + 1) % mod;
-}
+    T A(int64_t n, int64_t k) {assert(n < N); return n < 0 || k < 0 || k > n ? 0 : fc[n] * ifc[n - k] % mod;}
+    T C(int64_t n, int64_t k) {assert(n < N); return n < 0 || k < 0 || k > n ? 0 : fc[n] * ifc[k] % mod * ifc[n - k] % mod;}
+};
+
+template<typename T>
+struct comb {
+    size_t N;
+    vector<T> fc, ifc;
+
+    comb() = default;
+    comb(size_t N): N(N), fc(N, 1), ifc(N, 1) {
+        assert(N > 1);
+        for (int i = 1; i < N; ++i) fc[i] = fc[i - 1] * i;
+        ifc[N - 1] = inv(fc[N - 1]);
+        for (int i = N - 2; i; --i) ifc[i] = ifc[i + 1] * (i + 1);
+    }
+
+    T binpow(T x, int64_t k) {
+        assert(k >= 0);
+        T res = 1;
+        for (; k; k >>= 1) {
+            if (k & 1) res *= x;
+            x *= x;
+        }
+        return res;
+    }
+
+    T A(int64_t n, int64_t k) {assert(n < N); return n < 0 || k < 0 || k > n ? 0 : fc[n] * ifc[n - k];}
+    T C(int64_t n, int64_t k) {assert(n < N); return n < 0 || k < 0 || k > n ? 0 : fc[n] * ifc[k] * ifc[n - k];}
+};
