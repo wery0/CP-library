c8dad3c19772d47bded521e729ba69d05b3601ba
wery0 <wery0123@yandex.ru>
2025-05-22 14:28:06 +0300

    Move extended_euclid_inv to a separate file

--- Math & number theory/Combinatorics.cpp
+++ Math & number theory/Combinatorics.cpp
@@ -1,23 +1,3 @@
-template<typename T>
-T extended_euclid_inv(T a, T mod) {
-	static_assert(is_signed_v<T>);
-	auto gcd = [](auto&& gcd, T a, T b, T& x, T& y) {
-		if (!a) {
-	        x = 0, y = 1;
-	        return b;
-	    }
-	    T g = gcd(gcd, b % a, a, x, y);
-	    T nx = y - x * (b / a), ny = x;
-	    x = nx, y = ny;
-	    return g;
-	};
-    T x, y, g = gcd(gcd, a, mod, x, y);
-    if (g != 1) return -1;
-    if (x < 0) x += (-x + mod - 1) / mod * mod;
-    x %= mod;
-    return x;
-}
-
 template<typename T, const T mod>
 struct comb_mod {
     static_assert(is_integral_v<T>);
