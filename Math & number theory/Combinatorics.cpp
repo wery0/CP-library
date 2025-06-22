987f3d09231f5f26854d6894a042b0c197616a65
wery0 <wery0123@yandex.ru>
2025-06-22 11:01:08 +0300

    Added C_bigN_smallK()

--- Math & number theory/Combinatorics.cpp
+++ Math & number theory/Combinatorics.cpp
@@ -1,38 +1,3 @@
-template<typename T, const T mod>
-struct comb_mod {
-    static_assert(is_integral_v<T>);
-    static_assert(mod > 0);
-    static_assert(numeric_limits<T>::max() / mod > mod && "Product of two numbers < mod should fit into T!");
-
-    size_t N;
-    vector<T> fc, ifc;
-
-    comb_mod() = default;
-    comb_mod(size_t N): N(N), fc(N, 1), ifc(N, 1) {
-        assert(N > 1);
-        for (size_t i = 1; i < N; ++i) fc[i] = fc[i - 1] * i % mod;
-        ifc[N - 1] = inv(fc[N - 1]);
-        for (size_t i = N - 2; i; --i) ifc[i] = ifc[i + 1] * (i + 1);
-    }
-
-    T inv(T x) {
-        return binpow(x, mod - 2);
-    }
-
-    T binpow(T x, int64_t k) {
-        assert(k >= 0);
-        T res = 1;
-        for (; k; k >>= 1) {
-            if (k & 1) res = res * x % mod;
-            x = x * x % mod;
-        }
-        return res;
-    }
-
-    T A(int64_t n, int64_t k) {assert(n < N); return n < 0 || k < 0 || k > n ? 0 : fc[n] * ifc[n - k] % mod;}
-    T C(int64_t n, int64_t k) {assert(n < N); return n < 0 || k < 0 || k > n ? 0 : fc[n] * ifc[k] % mod * ifc[n - k] % mod;}
-};
-
 template<typename T>
 struct comb {
     size_t N;
@@ -58,4 +23,5 @@ struct comb {
 
     T A(int64_t n, int64_t k) {assert(n < N); return n < 0 || k < 0 || k > n ? 0 : fc[n] * ifc[n - k];}
     T C(int64_t n, int64_t k) {assert(n < N); return n < 0 || k < 0 || k > n ? 0 : fc[n] * ifc[k] * ifc[n - k];}
+    T C_bigN_smallK(int64_t n, int64_t k) {if (n < 0 || k < 0 || k > n) return 0; k = min(k, n - k); assert(k < N); T res = 1, x = n; for (int i = 0; i < k; ++i, --x) res *= x; return res * ifc[k];}
 };
