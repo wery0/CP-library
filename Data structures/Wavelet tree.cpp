454ce991f4bdaab63d91c855b2cc08e0201ad22e
wery0 <wery0123@yandex.ru>
2025-07-23 14:19:10 +0300

    Added Wavelet tree

---
+++ Data structures/Wavelet tree.cpp
@@ -0,0 +1,135 @@
+template<typename T, bool do_compress>
+class wavelet_tree {
+    static_assert(is_integral_v<T>);
+    size_t n, lg;
+    vector<T> keys;
+    T mne, mxe;
+    using bitvec = bit_vector<uint32_t, uint32_t>; 
+    vector<bitvec> bvs;
+
+public:
+    wavelet_tree() = default;
+    //O(nlog(n)) time and O(n) memory for preparation
+    template<typename I>
+    wavelet_tree(I first, I last): n(std::distance(first, last)) {
+        if (!n) return;
+        vector<T> m(first, last);
+        if (do_compress) {
+            vector<int> idx(n);
+            iota(idx.begin(), idx.end(), 0);
+            sort(idx.begin(), idx.end(), [&m](int l, int r) {return m[l] < m[r];});
+            keys.reserve(n);
+            for (int i : idx) {
+                if (keys.empty() || keys.back() != m[i]) keys.emplace_back(m[i]);
+                m[i] = keys.size() - 1;
+            }
+            keys.shrink_to_fit();
+        }
+        const auto [mni, mxi] = minmax_element(m.begin(), m.end());
+        mne = *mni, mxe = *mxi;
+        size_t D = mxe - mne + 1;
+        lg = __lg(D) + (D & (D - 1) ? 2 : 1);
+        bvs.resize(lg, bitvec(n));
+        function<void(int, int, int, T, T)> build = [&](int layer, int l, int r, T vl, T vr) {
+            if (l > r || vl == vr) return;
+            T vm = vl + (vr - vl) / 2;
+            for (int i = l; i <= r; ++i) if (m[i] > vm) bvs[layer].set(i);
+            int p = stable_partition(m.begin() + l, m.begin() + r + 1, [vm](T x) {return x <= vm;}) - m.begin();
+            build(layer + 1, l, p - 1, vl, vm);
+            build(layer + 1, p, r, vm + 1, vr);
+        };
+        build(0, 0, n - 1, mne, mxe);
+        for (auto& bv : bvs) bv.prepare();
+    }
+
+    //O(log(# distinct)) if do_compress == true, O(log(mxe - mne)) otherwise
+    T seg_kth_ordered_statistics(int ql, int qr, int k) {
+        assert(ql <= qr && qr < n);
+        assert(k <= qr - ql);
+        int l = 0, r = n - 1;
+        T vl = mne, vr = mxe;
+        for (int layer = 0; vl < vr; ++layer) {
+            T vm = vl + (vr - vl) / 2;
+            int c0 = bvs[layer].seg0(l, r);
+            int cq0 = bvs[layer].seg0(ql, qr);
+            int cq1 = qr - ql + 1 - cq0;
+            if (k < cq0) {
+                ql = l + (ql ? bvs[layer].seg0(l, ql - 1) : 0);
+                qr = ql + cq0 - 1;
+                r = l + c0 - 1;
+                vr = vm;
+            } else {
+                k -= cq0;
+                ql = l + c0 + (ql ? bvs[layer].seg1(l, ql - 1) : 0);
+                qr = ql + cq1 - 1;
+                l = l + c0;
+                vl = vm + 1;
+            }
+        }
+        return do_compress ? keys[vl] : vl;
+    }
+
+    //O(log(# distinct)) if do_compress == true, O(log(mxe - mne)) otherwise
+    int seg_count_leq(int ql, int qr, T x) {
+        assert(ql <= qr && qr < n);
+        if (do_compress) {
+            auto it = upper_bound(keys.begin(), keys.end(), x);
+            if (it == keys.begin()) return 0;
+            x = it - keys.begin() - 1;
+        }
+        int l = 0, r = n - 1;
+        T vl = mne, vr = mxe;
+        int res = 0;
+        for (int layer = 0; vl < vr && ql <= qr; ++layer) {
+            T vm = vl + (vr - vl) / 2;
+            int c0 = bvs[layer].seg0(l, r);
+            int cq0 = bvs[layer].seg0(ql, qr);
+            int cq1 = qr - ql + 1 - cq0;
+            if (x <= vm) {
+                ql = l + (ql ? bvs[layer].seg0(l, ql - 1) : 0);
+                qr = ql + cq0 - 1;
+                r = l + c0 - 1;
+                vr = vm;
+            } else {
+                res += cq0;
+                ql = l + c0 + (ql ? bvs[layer].seg1(l, ql - 1) : 0);
+                qr = ql + cq1 - 1;
+                l = l + c0;
+                vl = vm + 1;
+            }
+        }
+        res += vl == vr && vl <= x && ql <= qr ? qr - ql + 1 : 0;
+        return res;
+    }
+
+    //O(log(# distinct)) if do_compress == true, O(log(mxe - mne)) otherwise
+    int seg_count_seg(int ql, int qr, T x, T y) {
+        assert(ql <= qr && qr < n);
+        if (x > y) return 0;
+        if (do_compress) {
+            auto ix = lower_bound(keys.begin(), keys.end(), x);
+            if (ix == keys.begin()) return seg_count_leq(ql, qr, y);
+            x = ix - keys.begin();
+            auto iy = upper_bound(keys.begin(), keys.end(), y);
+            y = iy - keys.begin() - 1;            
+        }
+        int res = 0;
+        auto dfs = [&](auto&& dfs, int layer, int l, int r, int ql, int qr, T vl, T vr) {
+            if (vr < x || y < vl || ql > qr || l > r) return;
+            if (x <= vl && vr <= y) {
+                res += qr - ql + 1;
+                return;
+            }
+            T vm = vl + (vr - vl) / 2;
+            int c0 = bvs[layer].seg0(l, r);
+            int cq0 = bvs[layer].seg0(ql, qr);
+            int cq1 = qr - ql + 1 - cq0;
+            int lfql = l + (ql ? bvs[layer].seg0(l, ql - 1) : 0), lfqr = lfql + cq0 - 1;
+            dfs(dfs, layer + 1, l, l + c0 - 1, lfql, lfqr, vl, vm);
+            int rgql = l + c0 + (ql ? bvs[layer].seg1(l, ql - 1) : 0), rgqr = rgql + cq1 - 1;
+            dfs(dfs, layer + 1, l + c0, r, rgql, rgqr, vm + 1, vr);
+        };
+        dfs(dfs, 0, 0, n - 1, ql, qr, mne, mxe);
+        return res;
+    }
+};
