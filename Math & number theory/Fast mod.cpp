a2e2130706ba9e8def3aa75fb1f8afc4a8fb2992
wery0 <wery0123@yandex.ru>
2025-11-17 08:16:13 +0300

    Speed up quot_rem functions, added constness, inlined

--- Math & number theory/Fast mod.cpp
+++ Math & number theory/Fast mod.cpp
@@ -1,40 +1,50 @@
 //Use for constant modulo that is unknown in compile time. Up to x5 speed up.
-template<typename T = uint64_t, typename H = __uint128_t>
+template<typename T = uint64_t>
 class fast_mod {
+    using H =
+    typename conditional<is_same_v<T, uint8_t>, uint16_t,
+        typename conditional<is_same_v<T, uint16_t>, uint32_t,
+            typename conditional<is_same_v<T, uint32_t>, uint64_t,
+                typename conditional<is_same_v<T, uint64_t>, __uint128_t, int>::type
+            >::type
+        >::type
+    >::type;
+
     static constexpr int szT = sizeof(T) * 8;
-    static constexpr int szH = sizeof(H) * 8;
     static_assert(is_unsigned_v<T>);
     static_assert(is_unsigned_v<H> || is_same_v<H, __uint128_t>);
-    static_assert(szH == szT * 2);
+    static_assert(sizeof(H) * 8 == szT * 2);
 
     T m, b;
 
 public:
     fast_mod() = default;
-    fast_mod(T m): m(m), b(T(-1) / m) {assert(m);}
+    constexpr fast_mod(T m): m(m), b(T(-1) / m) {assert(m);}
 
     // x / m + (0 or 1)
-    T quot(T x) {return (H(b) * x) >> szT;}
+    T quot(T x) const {return (H(b) * x) >> szT;}
 
     // x / m
-    T honest_quot(T x) {T q = (H(b) * x) >> szT; return x - q * m < m ? q : q + 1;}
+    T honest_quot(T x) const {T q = (H(b) * x) >> szT; return x - q * m < m ? q : q + 1;}
 
     // x % m + (0 or m)
-    T rem(T x) {return x - T((H(b) * x) >> szT) * m;}
+    T rem(T x) const {return x - T((H(b) * x) >> szT) * m;}
 
     // x % m
-    T honest_rem(T x) {x -= T((H(b) * x) >> szT) * m; return x < m ? x : x - m;}
+    T honest_rem(T x) const {x -= T((H(b) * x) >> szT) * m; return x < m ? x : x - m;}
 
     // {x / m + (0 or 1), x % m + (0 or m)}
-    pair<T, T> quot_rem(T x) {  
+    __attribute__((always_inline))
+    array<T, 2> quot_rem(T x) const {
         T q = (H(b) * x) >> szT;
         return {q, x - q * m};
     }
 
     // {x / m, x % m}
-    pair<T, T> honest_quot_rem(T x) {  
+    __attribute__((always_inline))
+    array<T, 2> honest_quot_rem(T x) const {
         T q = (H(b) * x) >> szT;
-        q += x - q * m < m ? 0 : 1;
-        return {q, x - q * m};
+        x -= q * m;
+        return x < m ? array<T, 2>{q, x} : array<T, 2>{q + 1, x - m};
     }
 };
