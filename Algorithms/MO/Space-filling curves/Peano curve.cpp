8c18640a4ab15a3e2e1db0fdb7401c3a5dcf246c
wery0 <wery0123@yandex.ru>
2025-11-27 15:54:06 +0300

    Generalized Peano curve (encoding & decoding) to arbitrary dimension

--- Algorithms/MO/Space-filling curves/Peano curve.cpp
+++ Algorithms/MO/Space-filling curves/Peano curve.cpp
@@ -1,43 +1,54 @@
-//Maps (x, y) -> position in peano curve
-//O(log(max(x, y)))
-template<typename T = uint64_t>
-T peano_encode(T x, T y) {
+//Maps K-dimensional point -> position in K-dimensional peano curve
+//Position can be up to pow(3, ceil(log_3(max coordinat + 1))) ^ K
+//<O(K * log(max coordinat)), O(K * log(max coordinat))>
+template<typename T = uint64_t, const size_t K>
+T peano_encode(array<T, K> p) {
     static_assert(is_unsigned_v<T>);
-    T pw = 0;
-    for (T s = 1; s <= max(x, y); ++pw) s *= 3;
-    vector<T> f(pw), g(pw);
-    for (int i = 0; i < pw; ++i) {
-        f[pw - 1 - i] = x % 3;
-        g[pw - 1 - i] = y % 3;
-        x /= 3;
-        y /= 3;
+    T pw = 0, mx = *max_element(p.begin(), p.end());
+    for (T s = 1; s <= mx; ++pw) s *= 3;
+    array<vector<int>, K> f;
+    for (auto& i : f) i.resize(pw);
+    for (int i = 0; i < K; ++i) {
+        for (int j = 0; j < pw; ++j) {
+            f[i][pw - 1 - j] = p[i] % 3;
+            p[i] /= 3;
+        }
     }
-    T p = 0;
-    array<T, 2> sm = {0, 0};
-    for (int i = 0; i < pw * 2; ++i) {
-        T r = (i & 1 ? g : f)[i / 2];
-        if (sm[~i & 1] & 1) r = 2 - r;
-        sm[i & 1] += r;
-        p = p * 3 + r;
+    T pos = 0, ssm = 0;
+    array<T, K> sm{0};
+    for (int i = 0; i < pw * K; ++i) {
+        T r = f[i % K][i / K];
+        if ((ssm - sm[i % K]) & 1) r = 2 - r;
+        sm[i % K] += r;
+        ssm += r;
+        pos = pos * 3 + r;
     }
-    return p;
+    return pos;
 }
 
-//Maps position in peano curve -> (x, y)
-//O(log(p))
-template<typename T = uint64_t>
-array<T, 2> peano_decode(T p) {
-    static_assert(is_unsigned_v<T>);
-    T pw = 0;
-    for (T s = 1; s * s <= p; ++pw) s *= 3;
-    vector<T> m(pw * 2);
-    for (T i = 0; i < pw * 2; ++i, p /= 3) m[pw * 2 - 1 - i] = p % 3;
-    T x = 0, y = 0;
-    for (T i = 0, s0 = 0, s1 = 0; i < pw; ++i) {
-        s0 += m[i * 2];
-        x = x * 3 + (s1 & 1 ? 2 - m[i * 2] : m[i * 2]);
-        y = y * 3 + (s0 & 1 ? 2 - m[i * 2 + 1] : m[i * 2 + 1]);
-        s1 += m[i * 2 + 1];
+//Maps position in K-dimensional peano curve -> K-dimensional point
+//Position can be up to pow(3, ceil(log_3(max coordinat + 1))) ^ K
+//<O(K * log(max coordinat)), O(K)>
+template<typename T = uint64_t, const size_t K>
+array<T, K> peano_decode(T pos) {
+    T pw = 0, u = pos, x = 1, ssm = 0;
+    array<T, K> sm{0}, p{0};
+    for (T u = pos; u; ++pw) {
+        for (int i = 0; i < K; ++i) u /= 3;
     }
-    return {x, y};
+    for (int i = pw * K - 1; i >= 0; --i) {
+        T r = u % 3; u /= 3;
+        ssm += r;
+        sm[i % K] += r;
+    }
+    for (int i = pw - 1; i >= 0; --i, x *= 3) {
+        for (int j = K - 1; j >= 0; --j) {
+            T r = pos % 3; pos /= 3;
+            ssm -= r;
+            sm[j] -= r;
+            if ((ssm - sm[j]) & 1) r = 2 - r;
+            p[j] += r * x;
+        }
+    }
+    return p;
 }
