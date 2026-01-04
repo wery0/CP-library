243a5d65b104caec8f84df4acc9823e64fcbc80d
wery0 <wery0123@yandex.ru>
2026-01-04 23:33:55 +1000

    Moved get_flow_on_edges() into separate file

--- Flows/Ford-Fulkerson.cpp
+++ Flows/Ford-Fulkerson.cpp
@@ -77,15 +77,4 @@ public:
         flow_calculated = true;
         return ans;
     }
-
-    //For every added edge returns the flow going through it.
-    //If the flow on edge x->y is negative, then the flow goes from y to x (could only happen with undirected edges)
-    vector<tuple<int, int, T_flow>> get_flow_on_edges() const {
-        vector<tuple<int, int, T_flow>> res(store.size() / 2);
-        for (size_t i = 0; i < store.size(); i += 2) {
-            const auto& e = store[i];
-            res[i / 2] = {store[i ^ 1].to, e.to, e.flow};
-        }
-        return res;
-    }
 };
