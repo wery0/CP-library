92cd133f33f261fd1f2372038a9975e0e4e8a6e2
wery0 <wery0123@yandex.ru>
2026-01-21 04:52:18 +1000

    Improved iteration over consecutive palindromic numbers O(log(cur)) -> O(1), simplified implementation

--- Data structures/Palindrome numbers enumerator.cpp
+++ Data structures/Palindrome numbers enumerator.cpp
@@ -4,8 +4,7 @@ class palindrome_numbers_enumerator {
     static_assert(is_integral_v<T>);
     static_assert(is_signed_v<T>);
 
-    T st = 1, q1 = 0, q2 = 1;
-    T cur = 0, num = 0;
+    T cur = 0, num = 0, st = 1, h = 0, df = 1;
     int is_odd = 1;
 
     static T construct(T val, int is_odd) {
@@ -19,19 +18,28 @@ class palindrome_numbers_enumerator {
         return equal(s.begin(), s.begin() + s.size() / 2, s.rbegin());
     }
 
+    static T str_to_num(const string& s) {
+        return stoll(s);
+    }
+
 public:
     palindrome_numbers_enumerator(T x = 0) {set_cur(x);}
 
     void set_prev() {
-        --(is_odd ? q1 : q2);
-        --num;
-        if ((is_odd ? q1 : q2) < st && q1 != 0) {
-            if (!is_odd) ++q2, --q1;
-            else ++q1, --q2;
-            if (is_odd) st /= 10;
+        assert(cur > 0);
+        --h, --num;
+        if (h < st && cur != 1) {
+            is_odd ? (df += df / 10, st /= 10) : (df /= 11, h = h * 10 + 9);
             is_odd ^= 1;
+            cur -= 2;
+        } else {
+            int c0 = 0;
+            for (T u = h + 1; u % 10 == 0; u /= 10) ++c0;
+            T dd = df;
+            if (is_odd && c0) --c0, dd += df / 10;
+            while (c0--) dd /= 10;
+            cur -= dd;
         }
-        cur = construct(is_odd ? q1 : q2, is_odd);
     }
 
     void set_cur(T x) {
@@ -40,30 +48,31 @@ public:
         assert(is_palindrome(s));
         cur = x, is_odd = n & 1, st = 1;
         for (size_t i = 0; i < (n - 1) / 2; ++i) st *= 10;
-        if (is_odd) {
-            q1 = stoll(s.substr(0, (n + 1) / 2));
-            q2 = st;
-        } else {
-            q1 = st * 10;
-            q2 = stoll(s.substr(0, (n + 1) / 2));
-        }
-        num = q1 + q2 - is_odd;
+        h = str_to_num(s.substr(0, (n + 1) / 2));
+        df = is_odd ? st : st * 11;
+        num = h + (is_odd ? st : st * 10) - 1;
     }
 
     void set_next() {
-        ++(is_odd ? q1 : q2);
-        ++num;
-        if ((is_odd ? q1 : q2) >= st * 10) {
+        ++h, ++num;
+        if (h >= st * 10) {
+            is_odd ? (df *= 11, h /= 10) : (df -= df / 11, st *= 10);
             is_odd ^= 1;
-            if (q2 != st) st = st ? st * 10 : 1;
+            cur += 2;
+        } else {
+            int c0 = 0;
+            for (T u = h; u % 10 == 0; u /= 10) ++c0;
+            T dd = df;
+            if (is_odd && c0) --c0, dd += df / 10;
+            while (c0--) dd /= 10;
+            cur += dd;
         }
-        cur = construct(is_odd ? q1 : q2, is_odd);
     }
 
     T get_cur() const {return cur;}
 
     //O(log(k))
-    static T get_kth_palindrome_number(T k) {
+    static T calc_kth_palindrome_number(T k) {
         string s = to_string(k);
         T pw10 = 1;
         while (pw10 * 10 <= k) pw10 *= 10;
@@ -79,27 +88,29 @@ public:
     }
 
     //O(log(x))
-    static T get_lower_bound_palindrome(T x) {
+    static T calc_lower_bound_palindrome(T x) {
+        assert(x >= 0);
         string s = to_string(x);
         if (is_palindrome(s)) return x;
         const size_t n = s.size();
         string l = s.substr(0, (n + 1) / 2);
-        T c = construct(stoll(l), n & 1);
-        return c >= x ? c : construct(stoll(l) + 1, n & 1);
+        T c = construct(str_to_num(l), n & 1);
+        return c >= x ? c : construct(str_to_num(l) + 1, n & 1);
     }
 
     //O(log(x))
-    static T get_inverse_lower_bound_palindrome(T x) {
+    static T calc_inverse_lower_bound_palindrome(T x) {
+        assert(x >= 0);
         string s = to_string(x);
         if (is_palindrome(s)) return x;
         const size_t n = s.size();
-        if (count(s.begin(), s.end(), '0') == n - 1) return x - 1;
+        if (s[0] == '1' && all_of(s.begin() + 1, s.end(), [](char c) {return c == '0';})) return x - 1;
         string l = s.substr(0, (n + 1) / 2);
-        T c = construct(stoll(l), n & 1);
-        return c <= x ? c : construct(stoll(l) - 1, n & 1);
+        T c = construct(str_to_num(l), n & 1);
+        return c <= x ? c : construct(str_to_num(l) - 1, n & 1);
     }
 
     //O(log(x))
-    static T get_upper_bound_palindrome(T x) {return get_lower_bound_palindrome(x + 1);}
-    static T get_inverse_upper_bound_palindrome(T x) {return get_inverse_lower_bound_palindrome(x - 1);}
+    static T calc_upper_bound_palindrome(T x) {return get_lower_bound_palindrome(x + 1);}
+    static T calc_inverse_upper_bound_palindrome(T x) {return get_inverse_lower_bound_palindrome(x - 1);}
 };
