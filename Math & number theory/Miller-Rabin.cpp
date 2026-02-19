66f2a877c35457f6d16355f0d61854a1e842c39a
wery0 <wery0123@yandex.ru>
2026-02-20 01:53:08 +1000

    Miller-Rabin made template

--- Math & number theory/Miller-Rabin.cpp
+++ Math & number theory/Miller-Rabin.cpp
@@ -1,34 +1,29 @@
 namespace MillerRabin {
-    //Works for <= 63 bit modulo
-    //Change this function, if you need another way to multiply big numbers.
-    uint64_t big_prod_mod(const uint64_t x, const uint64_t y, const uint64_t m) {
-        uint64_t c = (long double)x * y / m;
-        int64_t ans = int64_t(x * y - c * m) % int64_t(m);
-        return ans < 0 ? ans + m : ans;
-    }
-
-    uint64_t binpow(uint64_t x, uint64_t k, const uint64_t mod) {
-        uint64_t o = 1;
-        for (; k; k >>= 1) {
-            if (k & 1) o = big_prod_mod(o, x, mod);
-            x = big_prod_mod(x, x, mod);
-        }
-        return o;
-    }
-
-    bool is_prime(const uint64_t p) {
-        if (p < 64) {return 2891462833508853932ull >> p & 1;}
-        static const vector<uint64_t> A32 = {2, 7, 61};  //enough for all p < 4759123141
-        static const vector<uint64_t> A64 = {2, 325, 9375, 28178, 450775, 9780504};
-        uint64_t k = __builtin_ctzll(p - 1), d = (p - 1) >> k;
-        for (uint64_t a : (p < 4759123141ull ? A32 : A64)) {
-            if (a >= p) break;
-            uint64_t c = binpow(a, d, p);
+    /*
+    Complexity: O(ITER * log(n))
+    Accuracy:
+      - If n is prime, always determines that
+      - If n is composite, determines that with >= 1 - 4^{-ITER} probability (on practice probability is much better)
+    */
+    template<typename T>
+    bool is_prime(const T n, int ITER = 5) {
+        assert(numeric_limits<T>::max() / n > n && "n^2 should fit into T!");
+        if (n < 64) {return 2891462833508853932ll >> n & 1;}
+        if (n % 2 == 0) return 0;
+        T k = __builtin_ctzll(n - 1), d = (n - 1) >> k;
+        static mt19937 rnd(std::chrono::steady_clock::now().time_since_epoch().count());
+        static uniform_int_distribution<T> gen(0, numeric_limits<T>::max());
+        for (int i = 0; i < ITER; ++i) {
+            T b = 2 + gen(rnd) % (n - 2), c = 1;
+            for (T dd = d, pb = b; dd; dd >>= 1) {
+                if (dd & 1) c = c * pb % n;
+                pb = pb * pb % n;
+            }
             if (c == 1) continue;
             int fl = 0;
-            for (uint64_t q = 0; q < k && !fl; ++q) {
-                fl |= c == p - 1;
-                c = big_prod_mod(c, c, p);
+            for (T q = 0; q < k && !fl; ++q) {
+                fl |= c == n - 1;
+                c = c * c % n;
             }
             if (!fl) return 0;
         }
