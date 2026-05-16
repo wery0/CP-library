565f8b9e35d8ed3eac64367b42c7a7ac81672ddb
wery0 <wery0123@yandex.ru>
2026-05-16 06:49:12 +0300

    Suppored non-prime modulos in discrete log

--- Math & number theory/Discrete log.cpp
+++ Math & number theory/Discrete log.cpp
@@ -1,37 +1,28 @@
-//Solves equation a ^ x = b (mod p), p is prime
-//If all_solutions == false, returns the smallest if it exists
-//If all_solutions == true, returns all solutions in range [0; p)
-//O(sqrt(p))
-template<typename T>
-vector<T> discrete_log(T a, T b, T p, bool all_solutions) {
-    auto binpow = [&](T x, T k) {  //Be careful with overflow
-        T o = 1;
-        for (; k; k >>= 1) {
-            if (k & 1) o = o * x % p;
-            x = x * x % p;
-        }
-        return o;
-    };
-    a %= p, b %= p;
-    T block = sqrtl(p);
-    unordered_map<T, vector<T>> mp(block);
-    for (T i = 0, pw = 1; i < block; ++i) {
-        mp[pw].push_back(i);
-        pw = pw * a % p;
+//Implementation of Shanks'es baby-step giant-step algorithm for solving equation a ^ x = b (mod m)
+//Returns minimal x if it exists, -1 otherwise
+//Complexity: <O(sqrt(m)), O(sqrt(m))>
+template<typename T, typename T2>
+T discrete_log_bsgs(T a, T b, T m) {
+    static_assert(is_integral_v<T> && is_signed_v<T>);
+    static_assert(is_integral_v<T2> && is_signed_v<T2>);
+    static_assert(sizeof(T) * 2 == sizeof(T2));
+    auto mulmod = [&](T x, T y) {return T2(x) * y % m;};
+    assert(0 <= a && a < m);
+    assert(0 <= b && b < m);
+    T k = 1, add = 0, g;
+    while ((g = gcd(a, m)) > 1) {
+        if (b == k) return add;
+        if (b % g) return -1;
+        b /= g, m /= g, ++add;
+        k = mulmod(k, a / g);
     }
-    const T inv_a = binpow(a, p - 2);
-    const T iab = binpow(inv_a, block);
-    vector<T> res;
-    for (T k = 0, ipw = 1; k <= block; ++k, ipw = ipw * iab % p) {
-        if (!all_solutions && res.size() == 1) break;
-        T need = b * ipw % p;
-        if (mp.count(need)) {
-            for (T r : mp[need]) {
-                if (k * block + r >= p) break;
-                res.push_back(k * block + r);
-                if (!all_solutions) break;
-            }
-        }
+    T n = sqrtl(m) + 1, an = 1;
+    for (T k = n, aa = a; k; k >>= 1, aa = mulmod(aa, aa)) if (k & 1) an = mulmod(an, aa);
+    unordered_map<T, T> mp(n * 2);
+    for (T q = 0, cur = b; q <= n; ++q, cur = mulmod(cur, a)) mp[cur] = q;
+    for (T p = 1, cur = k; p <= n; ++p) {
+        cur = mulmod(cur, an);
+        if (mp.count(cur)) return n * p - mp[cur] + add;
     }
-    return res;
+    return -1;
 }
